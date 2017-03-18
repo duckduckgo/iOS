08c3fcc6347da7265ea59a3d4c8ca0146c87a247
Mia Alexiou <mia.alexiou@subsymbolicsoftware.com>
2017-03-18 13:00:55 +0000

    Show infinity symbol for large numbers in tab icon

--- DuckDuckGo/TabIconMaker.swift
+++ DuckDuckGo/TabIconMaker.swift
@@ -14,32 +14,32 @@ class TabIconMaker {
     func icon(forTabs count: Int) -> UIImage {
         
         let image = #imageLiteral(resourceName: "Tabs")
-        let text = "\(count)"
+        let text = count < 100 ? "\(count)" : "~"
 
         UIGraphicsBeginImageContextWithOptions(image.size, false, UIScreen.main.scale)
         image.draw(in: CGRect(origin: .zero, size: image.size))
-        text.draw(in: CGRect(origin: point(forCount: count), size: image.size), withAttributes: attributes(forCount: count))
+        text.draw(in: CGRect(origin: point(forText: text), size: image.size), withAttributes: attributes(forText: text))
         
         let icon = UIGraphicsGetImageFromCurrentImageContext()!
         UIGraphicsEndImageContext()
         return icon
     }
     
-    private func point(forCount count: Int) -> CGPoint {
-        if isSingleDigit(count) {
+    private func point(forText text: String) -> CGPoint {
+        if isSingleChar(text) {
             return CGPoint(x: 10.1, y: 3)
         }
         return CGPoint(x: 7.9, y: 4.5)
     }
     
-    private func attributes(forCount count: Int) -> [String : NSObject] {
-        let size = (isSingleDigit(count)) ? 10 : 8
-        let weight = (isSingleDigit(count)) ? 5 : 3
+    private func attributes(forText text: String) -> [String : NSObject] {
+        let size = (isSingleChar(text)) ? 10 : 8
+        let weight = (isSingleChar(text)) ? 5 : 3
         let font = UIFont.systemFont(ofSize: CGFloat(size), weight: CGFloat(weight))
         return [ NSFontAttributeName: font, NSForegroundColorAttributeName: UIColor.white ]
     }
     
-    private func isSingleDigit(_ number: Int) -> Bool {
-        return number < 10
+    private func isSingleChar(_ text: String) -> Bool {
+        return text.characters.count == 1
     }
 }

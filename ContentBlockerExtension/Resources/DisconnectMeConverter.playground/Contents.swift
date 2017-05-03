2de9b36617d5897dfef1d5c7811520d0a96ac162
Mia Alexiou <mia.alexiou@subsymbolicsoftware.com>
2017-05-03 13:24:58 +0100

    Add domain exclusions to blockerList

--- ContentBlockerExtension/Resources/DisconnectMeConverter.playground/Contents.swift
+++ ContentBlockerExtension/Resources/DisconnectMeConverter.playground/Contents.swift
@@ -8,32 +8,38 @@
 
 import UIKit
 
+struct TrackerEntry {
+    let domain: String
+    let url: String
+}
+
 func run() {
     guard let jsonData = loadInputFile() else { return }
-    let trackersUrls = extractUrls(jsonData: jsonData)
-    let blockListJson = generateBlockListJson(urls: trackersUrls)
+    let trackers = extractTrackers(jsonData: jsonData)
+    let blockListJson = generateBlockListJson(trackers: trackers)
     saveToFile(text: blockListJson)
 }
 
 func loadInputFile() -> Data? {
-    guard let path = Bundle.main.path(forResource: "trackers", ofType: "json") else {
-        return nil
-    }
+    guard let path = Bundle.main.path(forResource: "trackers", ofType: "json") else { return nil }
     return FileManager.default.contents(atPath: path)
 }
 
-func extractUrls(jsonData: Data) -> Array<String> {
-    print("EXTRACTING URLS...")
+func extractTrackers(jsonData: Data) -> Array<TrackerEntry> {
+    print("EXTRACTING TRACKERS...")
     let json = JSON(data: jsonData)
     let categories = json["categories"]
     
-    var list = Array<String>()
+    var list = Array<TrackerEntry>()
     for (_, elements) in categories {
         for element in elements.arrayValue {
-            guard let urls = element.first?.1.first?.1.arrayObject else { continue }
-            for url in urls {
-                print(url)
-                list.append("\(url)")
+            guard let baseUrl = element.first?.1.first?.0 else { continue }
+            guard let trackers = element.first?.1.first?.1.arrayObject else { continue }
+            let domain = extractDomain(fromUrl: baseUrl)
+            for tracker in trackers {
+                let entry = TrackerEntry(domain: domain, url: "\(tracker)")
+                list.append(entry)
+                print(entry)
             }
         }
     }
@@ -41,12 +47,17 @@ func extractUrls(jsonData: Data) -> Array<String> {
     return list
 }
 
-func generateBlockListJson(urls: Array<String>) -> String {
+func extractDomain(fromUrl url: String) -> String {
+    let host = URL(string: url)?.host ?? url
+    return host.replacingOccurrences(of: "www.", with: "")
+}
+
+func generateBlockListJson(trackers: Array<TrackerEntry>) -> String {
     print("GENERATING JSON... ", terminator:"")
     var output = "[\n"
-    for (index, url) in urls.enumerated() {
-        let entry = blockedEntry(url: url)
-        let suffix = last(list: urls, index: index) ? "\n" : ",\n"
+    for (index, tracker) in trackers.enumerated() {
+        let entry = blockedEntry(tracker: tracker)
+        let suffix = last(list: trackers, index: index) ? "\n" : ",\n"
         output.append(entry + suffix)
     }
     output.append("]")
@@ -58,8 +69,10 @@ func last(list: Array<Any>, index: Int) -> Bool {
     return index == list.count-1
 }
 
-func blockedEntry(url: String) -> String {
-    return "{ \"action\":  { \"type\": \"block\" }, \"trigger\": { \"load-type\": [\"third-party\"], \"url-filter\": \"\(url)\"}}"
+func blockedEntry(tracker: TrackerEntry) -> String {
+    let domain = "[\"*\(tracker.domain)\"]"
+    let url = "\"\(tracker.url)\""
+    return "{ \"action\":  { \"type\": \"block\" }, \"trigger\": { \"load-type\": [\"third-party\"], \"url-filter\": \(url), \"unless-domain\": \(domain)}}"
 }
 
 func saveToFile(text: String) {

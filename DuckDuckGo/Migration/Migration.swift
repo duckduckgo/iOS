dd2425c18f8a5a293c025d6712342ed2365ea756
Chris Brind <c.brind@waracle.com>
2017-07-29 14:43:15 +0100

    migrate stories and searches and integrate with main app during onboarding

--- DuckDuckGo/Migration/Migration.swift
+++ DuckDuckGo/Migration/Migration.swift
@@ -12,38 +12,60 @@ import Core
 
 class Migration {
     
+    public static let oldBookmarksKey = "bookmarks"
+    
     private var container: PersistenceContainer
     
     init(container: PersistenceContainer = PersistenceContainer(name: "Stories")) {
         self.container = container
     }
     
-    func start(queue: DispatchQueue = DispatchQueue.global(qos: .background), completion: @escaping (_ stories: Int) -> ()) {
+    func start(queue: DispatchQueue = DispatchQueue.global(qos: .background), completion: @escaping (_ stories: Int, _ bookmarks: Int) -> ()) {
         queue.async {
             let bookmarks = BookmarksManager()
+            let bookmarksMigrated = self.migrateBookmarks(into: bookmarks)
             let storiesMigrated = self.migrateStories(into: bookmarks)
-            completion(storiesMigrated)
+            completion(storiesMigrated, bookmarksMigrated)
         }
+    }
+    
+    private func migrateBookmarks(into bookmarks: BookmarksManager) -> Int {
+        
+        let defaults = UserDefaults.standard
+        guard let oldBookmarks = defaults.array(forKey: Migration.oldBookmarksKey) else {
+            return 0
+        }
+        
+        var bookmarkCount = 0
+        for bookmarkDict in oldBookmarks {
+            
+            guard let bookmark = bookmarkDict as? [ String: String? ] else { continue }
+            guard let title = bookmark["title"] else { continue }
+            guard let urlString = bookmark["url"] else { continue }
+            guard let url = URL(string: urlString!) else { continue }
+            
+            bookmarks.save(bookmark: Link(title: title, url: url, favicon: nil))
+            bookmarkCount += 1
+        }
+        
+        return bookmarkCount
     }
     
     private func migrateStories(into bookmarks: BookmarksManager) -> Int {
         let savedStories = self.container.savedStories()
-        let storiesMigrated = savedStories.count
+        
+        var storyCount = 0
         for story in savedStories {
             
-            guard let articleURLString = story.articleURLString else { continue }
-            guard let articleURL = URL(string: articleURLString) else { continue }
+            guard let urlString = story.urlString else { continue }
+            guard let url = URL(string: urlString) else { continue }
             
-            var imageURL: URL?
-            if let url = story.imageURLString {
-                imageURL = URL(string: url)
-            }
-            
-            bookmarks.save(bookmark: Link(title: story.title, url: articleURL, favicon: imageURL))
+            bookmarks.save(bookmark: Link(title: story.title, url: url, favicon: nil))
+            storyCount += 1
         }
         
         self.container.clear()
-        return storiesMigrated;
+        return storyCount;
     }
     
 }

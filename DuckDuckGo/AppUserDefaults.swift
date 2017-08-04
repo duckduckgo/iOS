740ec20eb3eb46ab60990e20afc5f2a91cd38593
Chris Brind <brindy@brindy.org.uk>
2017-08-04 19:57:21 +0100

    create app wide user defaults

---
+++ DuckDuckGo/AppUserDefaults.swift
@@ -0,0 +1,52 @@
+//
+//  InfoBundle.swift
+//  DuckDuckGo
+//
+//  Copyright © 2017 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+import Foundation
+
+class AppUserDefaults {
+    
+    private let groupName: String
+    
+    private struct Keys {
+        static let autocompleteDisabledKey = "com.duckduckgo.app.autocompleteDisabledKey"
+    }
+    
+    private var userDefaults: UserDefaults? {
+        return UserDefaults(suiteName: groupName)
+    }
+    
+    init(groupName: String = "group.com.duckduckgo.app") {
+        self.groupName = groupName
+    }
+
+    var autocompleteDisabled: Bool {
+        
+        get {
+            return userDefaults?.bool(forKey: Keys.autocompleteDisabledKey) ?? false
+        }
+        
+        set {
+            userDefaults?.setValue(newValue, forKey: Keys.autocompleteDisabledKey)
+        }
+        
+    }
+        
+}
+

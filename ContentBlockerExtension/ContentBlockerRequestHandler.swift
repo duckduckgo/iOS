82922e96758024b99a54c16fafa34d0f4da52e61
Mia Alexiou <mia.alexiou@subsymbolicsoftware.com>
2017-08-07 22:51:49 +0200

    Disable the safari content blocker extension

--- ContentBlockerExtension/ContentBlockerRequestHandler.swift
+++ ContentBlockerExtension/ContentBlockerRequestHandler.swift
@@ -22,6 +22,19 @@ import UIKit
 import MobileCoreServices
 import Core
 
+// This component was disabled however will be reenabled again soon
+// after release.
+//
+// If reenabling consider adding whitelist support and whitelisting:
+//   - any url in the ContentBlockerConfigurationStore whitelist. Be careful 
+//     here, we're not talking about the tracker being on the whitellist but
+//     the page the tracker is on e.g facebook might be on the whiletlist so
+//     we wouldn't block any trackers while on facebook.
+//   - third party trackers whose tracker.parent matches the current url. For
+//     example abs-0.twimg.com is a third-party url when on twitter.com however
+//     we KNOW from the disconnect list that its parent is twitter so want to
+//     treat it as a first-party url and not block it when on twitter.
+//
 class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
     
     private lazy var parser = AppleContentBlockerParser()

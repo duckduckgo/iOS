7bc4e852188772d303bbc258f48f188a30b4882c
Mia Alexiou <mia@duckduckgo.com>
2017-09-20 23:24:41 +0100

    Fix address bar overlap timing issue on iOS 11

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -44,8 +44,8 @@ class TabViewController: WebViewController {
         webEventsDelegate = self
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         resetNavigationBar()
     }
     

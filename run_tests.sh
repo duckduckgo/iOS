713a54a23a19c7091da4a2fa5852ec9853d698ce
Mia Alexiou <mia@duckduckgo.com>
2017-10-09 14:29:51 +0100

    Add integration tests to test script. Run scripts with a fresh install.

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,5 @@
 carthage bootstrap --platform iOS --cache-builds;
+xcrun simctl uninstall booted com.duckduckgo.mobile.ios;
 xcodebuild test -project DuckDuckGo.xcodeproj -scheme DuckDuckGo CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 6s';
+xcrun simctl uninstall booted com.duckduckgo.mobile.ios;
+xcodebuild test -project DuckDuckGo.xcodeproj -scheme IntegrationTests CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 6s';

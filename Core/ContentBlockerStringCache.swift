d17a1aa4d71f097567ca41ad7b8e30b57fe37f99
Chris Brind <brindy@duckduckgo.com>
2017-12-14 18:27:16 +0000

    use decent key for version in user defaults

--- Core/ContentBlockerStringCache.swift
+++ Core/ContentBlockerStringCache.swift
@@ -24,7 +24,7 @@ public class ContentBlockerStringCache {
     struct Constants {
         // bump the cache version if you know the cache should be invalidated on the next release
         static let cacheVersion = 1
-        static let cacheVersionKey = "cacheVersionKey"
+        static let cacheVersionKey = "com.duckduckgo.contentblockerstringcache.version"
     }
 
     private var cacheDir: URL {

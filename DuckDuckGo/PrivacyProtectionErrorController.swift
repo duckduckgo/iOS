127cce94fabe298942394446959a260ef5dd8f91
Chris Brind <brindy@duckduckgo.com>
2018-04-04 20:51:25 +0100

    use table view for the error privacy info and use logic to resize the footer appropriately.

--- DuckDuckGo/PrivacyProtectionErrorController.swift
+++ DuckDuckGo/PrivacyProtectionErrorController.swift
@@ -28,12 +28,15 @@ protocol PrivacyProtectionErrorDelegate: class {
 
 }
 
-class PrivacyProtectionErrorController: UIViewController {
+class PrivacyProtectionErrorController: UITableViewController {
 
     @IBOutlet weak var errorLabel: UILabel!
     @IBOutlet weak var button: UIButton!
     @IBOutlet weak var activity: UIActivityIndicatorView!
+    @IBOutlet weak var buttonCell: UITableViewCell!
 
+    weak var footer: PrivacyProtectionFooterController!
+    
     var errorText: String?
 
     weak var delegate: PrivacyProtectionErrorDelegate?
@@ -42,17 +45,56 @@ class PrivacyProtectionErrorController: UIViewController {
         button.layer.cornerRadius = 5
         errorLabel.text = errorText
         resetTryAgain()
+        buttonCell.isHidden = !canRetry()
     }
 
-    func resetTryAgain() {
-        button?.isHidden = !(delegate?.canTryAgain(controller: self) ?? false)
-        activity?.isHidden = true
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateFooterHeight()
     }
-
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        coordinator.animate(alongsideTransition: nil) { context in
+            self.updateFooterHeight()
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let footer = segue.destination as? PrivacyProtectionFooterController {
+            self.footer = footer
+        }
+    }
+    
     @IBAction func onTapTryAgain() {
         activity.isHidden = false
         button.isHidden = true
         delegate?.tryAgain(controller: self)
+    }
+
+    func resetTryAgain() {
+        button?.isHidden = !canRetry()
+        activity?.isHidden = true
+    }
+    
+    private func canRetry() -> Bool {
+        return (delegate?.canTryAgain(controller: self) ?? false)
+    }
+    
+    private func updateFooterHeight() {
+        guard let footerView = tableView.tableFooterView else { return }
+        
+        tableView.tableFooterView = nil
+        
+        let frameHeight = tableView.frame.size.height
+        let contentHeight = tableView.contentSize.height
+        
+        let minSize = footer.preferredContentSize.height
+        let height = max(minSize, frameHeight - contentHeight)
+        
+        let frame = CGRect(x: 0, y: 0, width: tableView.frame.width, height: height)
+        footerView.frame = frame
+        tableView.tableFooterView = footerView
     }
 
 }

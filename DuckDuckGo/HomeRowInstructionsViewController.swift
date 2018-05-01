778dd4442c00bf79a9b889edd4b6ef7d9d4bcd6e
Chris Brind <brindy@duckduckgo.com>
2018-05-01 18:19:30 +0100

    show home row video

--- DuckDuckGo/HomeRowInstructionsViewController.swift
+++ DuckDuckGo/HomeRowInstructionsViewController.swift
@@ -7,20 +7,34 @@
 //
 
 import UIKit
+import AVKit
 
 class HomeRowInstructionsViewController: UIViewController {
     
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var videoContainerView: UIView!
     @IBOutlet weak var button: UIButton!
+    
+    weak var layer: AVPlayerLayer?
+    weak var player: AVPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.blur(style: .dark)
         
-        for view in [containerView, button] {
-            view?.layer.cornerRadius = 5
-            view?.layer.masksToBounds = true
-        }
+        applyCorners()
+        addVideo()
+    }
+    
+    @IBAction func playVideo() {
+        guard let player = player else { return }
+        player.seek(to: CMTime(seconds: 0.0, preferredTimescale: player.currentTime().timescale))
+        player.play()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        startVideo()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -31,6 +45,25 @@ class HomeRowInstructionsViewController: UIViewController {
  
     @IBAction func dismiss() {
         dismiss(animated: true)
+    }
+    
+    private func applyCorners() {
+        for view in [containerView, button] {
+            view?.layer.cornerRadius = 5
+            view?.layer.masksToBounds = true
+        }
+    }
+    
+    private func addVideo() {
+        let movieURL = Bundle.main.url(forResource: "home-row-instructions", withExtension: "mp4")!
+        player = AVPlayer(url: movieURL)
+        layer = AVPlayerLayer(player: player)
+        videoContainerView.layer.addSublayer(layer!)
+        layer?.frame = videoContainerView.bounds
+    }
+    
+    private func startVideo() {
+        player?.play()
     }
     
 }

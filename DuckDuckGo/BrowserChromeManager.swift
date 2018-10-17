d56631ce8972cce013596857683f678d788f710a
bwaresiak <b.waresiak@gmail.com>
2018-10-17 22:39:26 +0200

    Improve scrolling behaviour (#369)
    * Change bar hiding logic to match Safari
    
    * Decrease treshold to make bar hiding feel more responsive
    
    * Do not refresh rating view on every layout call
    
    * Tweak method signature to make the code a bit more clear
    
    * Fix for iPhone X issue
    
    * Removed unnecessary method

--- DuckDuckGo/BrowserChromeManager.swift
+++ DuckDuckGo/BrowserChromeManager.swift
@@ -33,68 +33,94 @@ protocol BrowserChromeDelegate: class {
 class BrowserChromeManager: NSObject, UIScrollViewDelegate {
 
     struct Constants {
-
-        static let threshold: CGFloat = 60
-
+        static let threshold: CGFloat = 30
     }
 
     weak var delegate: BrowserChromeDelegate?
 
-    var dragging = false
     var hidden = false
-    var lastOffset: CGPoint?
-    var cumulative: CGFloat = 0
-
+    var dragging = false
+    var draggingStartPosY: CGFloat = 0
+    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         guard dragging else { return }
+        guard canHideBars(for: scrollView) else { return }
+        
+        let isInBottomBounceArea = scrollView.contentOffset.y > scrollView.contentOffsetYAtBottom
+        guard isInBottomBounceArea == false else { return }
 
-        if let lastOffset = lastOffset {
-
-            let ydiff = lastOffset.y - scrollView.contentOffset.y
-
-            if ydiff == 0 || (cumulative < 0 && ydiff > 0) || (cumulative > 0 && ydiff < 0) {
-                cumulative = 0
-            }
-
-            cumulative += ydiff
-
-            if abs(cumulative) > Constants.threshold {
-                updateBars(ydiff < 0)
-            }
-
+        if scrollView.contentOffset.y - draggingStartPosY > Constants.threshold {
+            updateBars(shouldHide: true)
         }
-
-        lastOffset = scrollView.contentOffset
     }
 
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
         dragging = true
+        draggingStartPosY = scrollView.contentOffset.y
+    }
+
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        guard canHideBars(for: scrollView) else { return }
+        
+        let isInBottomBounceArea = scrollView.contentOffset.y > scrollView.contentOffsetYAtBottom
+        let startedFromVeryBottom = abs(draggingStartPosY - scrollView.contentOffsetYAtBottom) < 1
+        
+        if isInBottomBounceArea && startedFromVeryBottom {
+            updateBars(shouldHide: false)
+            // Fix for iPhone X issue: reaching bottom of the web page and then revealing
+            // bars by executing "bottom bounce" gesture, caused web view to re layout and
+            // cover bottom of the web page.
+            DispatchQueue.main.async { [weak scrollView] in
+                guard let scrollView = scrollView else { return }
+                scrollView.setContentOffset(CGPoint(x: 0, y: scrollView.contentOffsetYAtBottom),
+                                            animated: true)
+            }
+        } else if velocity.y < 0 {
+            updateBars(shouldHide: false)
+        } else if velocity.y > 0 {
+            updateBars(shouldHide: true)
+        }
     }
 
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
         dragging = false
-        cumulative = 0
     }
 
     func scrollViewShouldScrollToTop(_ scrollView: UIScrollView) -> Bool {
         if hidden {
-            updateBars(false)
+            updateBars(shouldHide: false)
             return false
         }
 
         return true
     }
+    
+    /// Bars should not be hidden in case ScrollView content is smaller than viewport.
+    private func canHideBars(for scrollView: UIScrollView) -> Bool {
+        return scrollView.bounds.height < scrollView.contentSize.height
+    }
 
-    private func updateBars(_ shouldHide: Bool) {
+    private func updateBars(shouldHide: Bool) {
         guard shouldHide != hidden else { return }
         hidden = shouldHide
         delegate?.setBarsHidden(shouldHide, animated: true)
     }
 
     func reset() {
-        updateBars(false)
-        cumulative = 0
-        lastOffset = nil
+        updateBars(shouldHide: false)
+        draggingStartPosY = 0
     }
+}
 
+fileprivate extension UIScrollView {
+    
+    /// Calculate Y-axis content offset corresponding to very bottom of the scroll area
+    var contentOffsetYAtBottom: CGFloat {
+        let yOffset = contentSize.height - bounds.height - contentInset.top + contentInset.bottom
+        if #available(iOS 11.0, *) {
+            return yOffset - safeAreaInsets.top + safeAreaInsets.bottom
+        } else {
+            return yOffset
+        }
+    }
 }

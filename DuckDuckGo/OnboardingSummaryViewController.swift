f75bec0f26f83f687f14a93d6078a6c097302b87
Chris Brind <brindy@duckduckgo.com>
2019-04-11 15:24:27 +0100

    offset the summary so it aligns better with the theme selection.

--- DuckDuckGo/OnboardingSummaryViewController.swift
+++ DuckDuckGo/OnboardingSummaryViewController.swift
@@ -11,10 +11,12 @@ import UIKit
 class OnboardingSummaryViewController: OnboardingContentViewController {
     
     @IBOutlet var bulletsStack: UIStackView!
+    @IBOutlet var offsetY: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bulletsStack.spacing = isSmall ? 8 : 12
+        offsetY.constant = isSmall ? 0 : -27
         self.canContinue = true
     }
     

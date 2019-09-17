baaca8acc6aab40c4da909c12ca4b9a86ff95cbe
bwaresiak <bartek@duckduckgo.com>
2019-09-17 16:26:31 +0200

    Reveal all components of top bar along with status bar view (#485)

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -418,7 +418,7 @@ class TabViewController: UIViewController {
     }
 
     private func resetNavigationBar() {
-        chromeDelegate?.setBarsHidden(false, animated: false)
+        chromeDelegate?.setNavigationBarHidden(false)
     }
 
     @IBAction func onBottomOfScreenTapped(_ sender: UITapGestureRecognizer) {

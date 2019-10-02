c85ce1f6cbf073cedda647f26de242e921d2c93f
Igor Mandrigin <mandrigin@users.noreply.github.com>
2019-10-02 15:51:52 +0200

    Make simulator in `./run-tests.sh` configurable. (#487)
    * Make simulator in `./run-tests.sh` configurable.
    
    * Make iPhone 8 simulator a default for tests.

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+SIM_NAME=${DDG_SIMULATOR:-'iPhone 8'}
+SCHEME=${DDG_TEST_SCHEME:-'DuckDuckGo'}
+
 carthage bootstrap --platform iOS --cache-builds;
 xcrun simctl uninstall booted com.duckduckgo.mobile.ios;
-xcodebuild test -quiet -project DuckDuckGo.xcodeproj -scheme DuckDuckGo CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination 'platform=iOS Simulator,name=iPhone 6s';
+xcodebuild test -quiet -project DuckDuckGo.xcodeproj -scheme $SCHEME CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO -destination "platform=iOS Simulator,name=$SIM_NAME";

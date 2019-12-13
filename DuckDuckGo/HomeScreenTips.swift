39f4280000a24d08033e4808de7aff9b35af193f
Bartek Waresiak <bartek@duckduckgo.com>
2019-12-13 01:04:52 +0100

    Fix Home Screen tips tests

--- DuckDuckGo/HomeScreenTips.swift
+++ DuckDuckGo/HomeScreenTips.swift
@@ -52,10 +52,11 @@ class HomeScreenTips {
         self.storage = storage
     }
     
-    func trigger(variantManager: VariantManager = DefaultVariantManager()) {
+    func trigger(variantManager: VariantManager = DefaultVariantManager(),
+                 ctaStorage: HomeRowCTAStorage = UserDefaultsHomeRowCTAStorage()) {
         guard storage.isEnabled else { return }
         guard tutorialSettings.hasSeenOnboarding else { return }
-        guard UserDefaultsHomeRowCTAStorage().dismissed else { return }
+        guard ctaStorage.dismissed else { return }
         guard let tip = Tip(rawValue: storage.nextHomeScreenTip) else { return }
         
         let completion: (Bool) -> Void = { shown in

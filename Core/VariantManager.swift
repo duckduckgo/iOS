e8eecb2b424f44e5153ef081f973e989d2f6bb6b
bwaresiak <bartek@duckduckgo.com>
2020-01-28 11:43:35 +0100

    Disable SERP variants (#563)

--- Core/VariantManager.swift
+++ Core/VariantManager.swift
@@ -36,9 +36,9 @@ public struct Variant {
     
     public static let defaultVariants: [Variant] = [
         // SERP testing
-        Variant(name: "sc", weight: 1, features: []),
-        Variant(name: "sd", weight: 1, features: []),
-        Variant(name: "se", weight: 1, features: []),
+        Variant(name: "sc", weight: doNotAllocate, features: []),
+        Variant(name: "sd", weight: doNotAllocate, features: []),
+        Variant(name: "se", weight: doNotAllocate, features: []),
         
         Variant(name: "mg", weight: 1, features: []),
         Variant(name: "mt", weight: 1, features: [.onboardingCTA]),

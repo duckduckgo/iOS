b8dfb0f0d71bf3205b0c4660e1469891a9f86477
Bartek Waresiak <bartek@duckduckgo.com>
2020-02-13 14:28:26 +0100

    Pick AppIcon from onboarding

--- DuckDuckGo/OnboardingAppIconViewController.swift
+++ DuckDuckGo/OnboardingAppIconViewController.swift
@@ -17,19 +17,37 @@
 //  limitations under the License.
 //
 
-import Foundation
+import UIKit
 
 class OnboardingAppIconViewController: OnboardingContentViewController {
     
-    @IBOutlet weak var videoContainerView: VideoContainerView!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    let dataSource = AppIconDataSource()
+    let worker = AppIconWorker(context: .onboarding)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.dataSource = dataSource
+        collectionView.reloadData()
+        
+        initSelection()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
+    private func initSelection() {
+        let icon = AppIconManager.shared.appIcon
+        let index = dataSource.appIcons.firstIndex(of: icon) ?? 0
+        let indexPath = IndexPath(row: index, section: 0)
+        collectionView.selectItem(at: indexPath, animated: false, scrollPosition: .top)
+    }
+    
+    private var selectedIcon: AppIcon {
+        guard let selection = collectionView.indexPathsForSelectedItems?.last else {
+            return AppIconManager.shared.appIcon
+        }
+        
+        return dataSource.appIcons[selection.row]
     }
     
     override var header: String {
@@ -45,6 +63,15 @@ class OnboardingAppIconViewController: OnboardingContentViewController {
     }
     
     override func onContinuePressed(navigationHandler: @escaping () -> Void) {
-        navigationHandler()
+        
+        let currentIcon = AppIconManager.shared.appIcon
+        
+        if currentIcon != selectedIcon {
+            worker.changeAppIcon(selectedIcon) { _ in
+                navigationHandler()
+            }
+        } else {
+            navigationHandler()
+        }
     }
 }

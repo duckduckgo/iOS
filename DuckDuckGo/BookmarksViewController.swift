4ee72d33165f4339487434480fa5e5c2c3e43cda
Chris Brind <brindy@duckduckgo.com>
2020-02-25 23:40:16 +0000

    merge the bookmarks data source

--- DuckDuckGo/BookmarksViewController.swift
+++ DuckDuckGo/BookmarksViewController.swift
@@ -30,7 +30,7 @@ class BookmarksViewController: UITableViewController {
     private lazy var appSettings = AppDependencyProvider.shared.appSettings
 
     fileprivate lazy var dataSource: BookmarksDataSource = {
-        return BookmarksAndFavoritesDataSource()
+        return BookmarksDataSource()
     }()
     
     override func viewDidLoad() {

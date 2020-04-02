e5d0243f7f062f4c0c0c7af97129d686d5de75ad
Chris Brind <brindy@duckduckgo.com>
2020-04-02 12:13:00 +0100

    move the privacy overlay to resign active and remove it on become active

--- DuckDuckGo/AppDelegate.swift
+++ DuckDuckGo/AppDelegate.swift
@@ -102,6 +102,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         guard !testing else { return }
         
+        if privacyStore.authenticationEnabled {
+            beginAuthentication()
+        } else {
+            removeOverlay()
+        }
+
         StatisticsLoader.shared.load {
             StatisticsLoader.shared.refreshAppRetentionAtb()
             Pixel.fire(pixel: .appLaunch)
@@ -130,18 +136,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        if privacyStore.authenticationEnabled {
-            beginAuthentication()
-        } else {
-            removeOverlay()
-        }
-        
         autoClear?.applicationWillMoveToForeground()
         showKeyboardIfSettingOn = true
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
+    func applicationWillResignActive(_ application: UIApplication) {
         displayOverlay()
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
         autoClear?.applicationDidEnterBackground()
     }
 

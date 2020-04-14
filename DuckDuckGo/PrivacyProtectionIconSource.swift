20c1b3205d98365557d17278358e42faac94c4af
Bartek Waresiak <bartek@duckduckgo.com>
2020-04-14 13:08:50 +0200

    Use UIGraphicsImageRenderer

--- DuckDuckGo/PrivacyProtectionIconSource.swift
+++ DuckDuckGo/PrivacyProtectionIconSource.swift
@@ -32,36 +32,35 @@ class PrivacyProtectionIconSource {
         }
         
         let imageRect = CGRect(x: 0, y: 0, width: Constants.iconWidth, height: Constants.iconHeight)
-        
-        UIGraphicsBeginImageContextWithOptions(imageRect.size, false, 0.0)
-        guard let context = UIGraphicsGetCurrentContext() else { return nil }
-        
-        let networkSymbol: String
-        if let firstCharacter = networkName.first {
-            networkSymbol = String(firstCharacter).uppercased()
-        } else {
-            networkSymbol = "?"
+
+        let renderer = UIGraphicsImageRenderer(size: imageRect.size)
+        let icon = renderer.image { imageContext in
+            let context = imageContext.cgContext
+            
+            let networkSymbol: String
+            if let firstCharacter = networkName.first {
+                networkSymbol = String(firstCharacter).uppercased()
+            } else {
+                networkSymbol = "?"
+            }
+            
+            context.setFillColor(UIColor.white.cgColor)
+            context.fillEllipse(in: imageRect)
+            
+            let label = UILabel(frame: imageRect)
+            label.font = UIFont.semiBoldAppFont(ofSize: 17)
+            label.textColor = UIColor.black
+            label.textAlignment = .center
+            label.text = networkSymbol
+            label.sizeToFit()
+            
+            context.translateBy(x: (imageRect.width - label.bounds.width) / 2,
+                                y: (imageRect.height - label.font.ascender) / 2 - (label.font.ascender - label.font.capHeight) / 2)
+            
+            context.setBlendMode(.destinationOut)
+            label.layer.draw(in: context)
         }
         
-        context.setFillColor(UIColor.white.cgColor)
-        context.fillEllipse(in: imageRect)
-        
-        let label = UILabel(frame: imageRect)
-        label.font = UIFont.semiBoldAppFont(ofSize: 17)
-        label.textColor = UIColor.black
-        label.textAlignment = .center
-        label.text = networkSymbol
-        label.sizeToFit()
-        
-        context.translateBy(x: (imageRect.width - label.bounds.width) / 2,
-                             y: (imageRect.height - label.font.ascender) / 2 - (label.font.ascender - label.font.capHeight) / 2)
-        
-        context.setBlendMode(.destinationOut)
-        label.layer.draw(in: context)
-        
-        let icon = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        
-        return icon?.withRenderingMode(.alwaysTemplate)
+        return icon.withRenderingMode(.alwaysTemplate)
     }
 }

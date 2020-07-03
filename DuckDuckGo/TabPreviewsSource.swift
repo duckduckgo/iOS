6e3318443aa9e29f72e6864ec49df027a59fa611
Bartek Waresiak <bartek@duckduckgo.com>
2020-07-03 13:06:24 +0200

    Move Previews dir location from Caches to Application Support

--- DuckDuckGo/TabPreviewsSource.swift
+++ DuckDuckGo/TabPreviewsSource.swift
@@ -21,10 +21,15 @@ import UIKit
 
 class TabPreviewsSource {
     
+    struct Constants {
+        static let previewsDirectoryName = "Previews"
+    }
+    
     private var cache = [String: UIImage]()
     
     func prepare() {
         ensurePreviewStoreDirectoryExists()
+        migratePreviewStoreDirectoryFromCache()
     }
     
     func update(preview: UIImage, forTab tab: Tab) {
@@ -66,18 +71,57 @@ class TabPreviewsSource {
     }
     
     private var previewStoreDir: URL? {
+        guard var cachesDirURL = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first else { return nil }
+        cachesDirURL.appendPathComponent(Constants.previewsDirectoryName, isDirectory: true)
+        return cachesDirURL
+    }
+    
+    private var legacyPreviewStoreDir: URL? {
         guard var cachesDirURL = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first else { return nil }
-        cachesDirURL.appendPathComponent("Previews", isDirectory: true)
+        cachesDirURL.appendPathComponent(Constants.previewsDirectoryName, isDirectory: true)
         return cachesDirURL
     }
     
     private func ensurePreviewStoreDirectoryExists() {
-        guard let url = previewStoreDir else { return }
-        if !FileManager.default.fileExists(atPath: url.absoluteString, isDirectory: nil) {
+        guard var url = previewStoreDir else { return }
+        
+        // Create Application Support Dir if needed.
+        let parentDirURL = url.deletingLastPathComponent()
+        if !FileManager.default.fileExists(atPath: parentDirURL.path, isDirectory: nil) {
+            try? FileManager.default.createDirectory(at: parentDirURL,
+                                                     withIntermediateDirectories: false,
+                                                     attributes: nil)
+        }
+        
+        if !FileManager.default.fileExists(atPath: url.path, isDirectory: nil) {
+            
+            // Exclude Previews Dir from backup
+            var resourceValues = URLResourceValues()
+            resourceValues.isExcludedFromBackup = true
+            try? url.setResourceValues(resourceValues)
+            
             try? FileManager.default.createDirectory(at: url,
                                                      withIntermediateDirectories: false,
                                                      attributes: nil)
         }
+    }
+    
+    private func migratePreviewStoreDirectoryFromCache() {
+        guard let source = legacyPreviewStoreDir,
+            let destination = previewStoreDir else { return }
+        
+        let contents = (try? FileManager.default.contentsOfDirectory(at: source,
+                                                                    includingPropertiesForKeys: nil,
+                                                                    options: .skipsSubdirectoryDescendants)) ?? []
+        let previews = contents.filter { $0.lastPathComponent.hasSuffix(".png") }
+        
+        for preview in previews {
+            let desitnationURL = destination.appendingPathComponent(preview.lastPathComponent)
+            do { try FileManager.default.moveItem(at: preview, to: desitnationURL) }
+            catch { print("\(error)") }
+        }
+        
+        try? FileManager.default.removeItem(at: source)
     }
     
     private func previewLocation(for tab: Tab) -> URL? {

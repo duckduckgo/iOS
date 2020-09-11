5eebb0c46d4c314ffa31288c1102b27f44583a04
Bartek Waresiak <bartek@duckduckgo.com>
2020-09-11 15:12:56 +0200

    Use NSTextAttachment to display privacy grade improvement

--- DuckDuckGo/PrivacyProtectionHeaderCell.swift
+++ DuckDuckGo/PrivacyProtectionHeaderCell.swift
@@ -60,32 +60,59 @@ class PrivacyProtectionHeaderConfigurator {
         if !protecting {
             cell.gradeLabel.setAttributedTextString(UserText.privacyProtectionProtectionDisabled)
         } else if differentGrades(in: siteRating) {
-            // SITE rating siteRating
-            cell.gradeLabel.setAttributedTextString(UserText.privacyProtectionEnhanced)
+            cell.gradeLabel.attributedText = makeEnhancedProtectionLabel(siteRating, fromText: cell.gradeLabel.attributedText!)
         } else {
             cell.gradeLabel.setAttributedTextString(UserText.privacyProtectionProtectionDisabled)
         }
     }
     
-    private static func makeEnhancedProtectionLabel(_ siteRating: SiteRating, fromText: NSAttributedString) {
+    private static func makeEnhancedProtectionLabel(_ siteRating: SiteRating, fromText: NSAttributedString) -> NSAttributedString? {
         let siteGradeImages = siteRating.siteGradeImages()
-        // create an NSMutableAttributedString that we'll append everything to
-        let fullString = NSMutableAttributedString(attributedString: fromText)
+        
+        let string = UserText.privacyProtectionEnhanced
+        
+        let regex = try? NSRegularExpression(pattern: "\\$([1-9])")
+        guard let match = regex?.matches(in: string, options: [], range: NSRange(location: 0, length: string.count)),
+            match.count >= 2 else { return nil }
+        
+        let firstRange = Range(match[0].range, in: string)!
+        let secondRange = Range(match[1].range, in: string)!
+        
+        let firstPart = String(string[string.startIndex..<firstRange.lowerBound])
+        let firstGrade = String(string[firstRange])
+        let secondPart = String(string[firstRange.upperBound..<secondRange.lowerBound])
+        let lastPart = String(string[secondRange.upperBound..<string.endIndex])
+        
+        let fullString = NSMutableAttributedString(string: firstPart)
+        fullString.setAttributes(fromText.attributes(at: 0, effectiveRange: nil),
+                                 range: NSRangeFromString(firstPart))
         
         let image1Attachment = NSTextAttachment()
         image1Attachment.image = siteGradeImages.from
+        image1Attachment.bounds = CGRect(x: 0.0, y: -8,
+                                         width: siteGradeImages.from.size.width,
+                                         height: siteGradeImages.from.size.height)
         let image1String = NSAttributedString(attachment: image1Attachment)
-        fullString.append(image1String)
-        fullString.append(NSAttributedString(string: " to "))
-        
+
         let image2Attachment = NSTextAttachment()
         image2Attachment.image = siteGradeImages.to
+        image2Attachment.bounds = CGRect(x: 0.0, y: -8,
+                                         width: siteGradeImages.to.size.width,
+                                         height: siteGradeImages.to.size.height)
         let image2String = NSAttributedString(attachment: image2Attachment)
-        fullString.append(image2String)
-        fullString.append(NSAttributedString(string: "|"))
         
-        // draw the result in a label
-        //        label.attributedText = fullString
+        if firstGrade == "$1" {
+            fullString.append(image1String)
+            fullString.append(NSAttributedString(string: secondPart))
+            fullString.append(image2String)
+        } else {
+            fullString.append(image2String)
+            fullString.append(NSAttributedString(string: secondPart))
+            fullString.append(image1String)
+        }
+        fullString.append(NSAttributedString(string: lastPart))
+   
+        return fullString
     }
     
     private static func differentGrades(in siteRating: SiteRating) -> Bool {

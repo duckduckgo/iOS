dc1ae53892317dbd7c28cc6c8cfad21566262945
Bartek Waresiak <bartek@duckduckgo.com>
2020-09-26 01:30:07 +0200

    Import localizations

--- loc_import.sh
+++ loc_import.sh
@@ -8,8 +8,11 @@ for dir in $1/*; do
     targetLocale=$(echo $locale | cut -f1 -d-)
     if [ "$locale" != "$targetLoc" ]
     then
-      echo "Changing locale to $targetLocale"
-      sed -i "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" $dir/en.xliff
+      echo "Changing locale from $locale to $targetLocale"
+      echo "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/"
+      echo "$dir/en.xliff"
+      sed -i '.bak' "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" "$dir/en.xliff"
+      rm "$dir/en.xliff.bak"
     fi
 
     echo "Importing $dir/en.xliff..."

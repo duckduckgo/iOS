449b04815635174e686ae0d8b467fc4e903a63bf
Bartek Waresiak <bartek@duckduckgo.com>
2020-09-28 09:05:35 +0200

    Fix update script, create en localization for Widgets

--- loc_update.sh
+++ loc_update.sh
@@ -5,5 +5,5 @@ declare -a targets=("DuckDuckGo" "Widgets")
 
 for dir in "${targets[@]}"; do
   echo Processing $dir
-  find $dir/ -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o DuckDuckGo/en.lproj
+  find $dir/ -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o $dir/en.lproj
 done

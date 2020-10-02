17c079eb4c555356077b66b0cf02d97c205ee69f
Brad Slayter <slayterdevelopment@gmail.com>
2020-10-02 09:24:47 -0500

    Copy tweaks

--- DuckDuckGo/UserText.swift
+++ DuckDuckGo/UserText.swift
@@ -310,9 +310,9 @@ public struct UserText {
     public static let defaultBrowserHomeMessageTopText = NSLocalizedString("home.message.topText", value: "NEW IN IOS 14", comment: "")
     public static let defaultBrowserHomeMessageButtonText = NSLocalizedString("home.message.buttonText", value: "Set as Default Browser", comment: "")
     
-    public static let doNotSellInfoText = NSLocalizedString("donotsell.info.headertext", value: "Your data shouldn't be for sale. At DuckDuckGo, we agree. Activate the \"Global Privacy Control\" (GPC) setting and we'll signal to websites your preference to:\n\n• Not sell your personal data\n• Limit sharing your personal data to other companies",
+    public static let doNotSellInfoText = NSLocalizedString("donotsell.info.headertext", value: "Your data shouldn't be for sale. At DuckDuckGo, we agree. Activate the \"Global Privacy Control\" (GPC) setting and we'll signal to websites your preference to:\n\n• Not sell your personal data\n• Limit sharing of your personal data to other companies",
                         comment: "")
-    public static let doNotSellDisclaimerBold = NSLocalizedString("donotsell.disclaimer.footertext", value: "Since Global Privacy Control (GPC) is a new standard, most websites won't recognize it yet, but we're woking hard to ensure it becomes accepted worldwide. ", comment: "")
+    public static let doNotSellDisclaimerBold = NSLocalizedString("donotsell.disclaimer.footertext", value: "Since Global Privacy Control (GPC) is a new standard, most websites won't recognize it yet, but we're working hard to ensure it becomes accepted worldwide. ", comment: "")
     public static let doNotSellDisclaimerSuffix = NSLocalizedString("donotsell.disclaimer.suffix", value: " However, websites are only required to act on the signal to the extent applicable laws compel them to do so. ", comment: "")
     public static let doNotSellLearnMore = NSLocalizedString("donotsell.disclaimer.learnmore", value: "Learn More", comment: "")
 }

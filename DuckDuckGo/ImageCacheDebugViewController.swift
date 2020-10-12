e23a9045679308d1e9a003d44f2b60b56a49e6ee
Christopher Brind <brindy@duckduckgo.com>
2020-10-12 16:34:34 +0100

    improve favorites (#734)

--- DuckDuckGo/ImageCacheDebugViewController.swift
+++ DuckDuckGo/ImageCacheDebugViewController.swift
@@ -23,10 +23,18 @@ import UIKit
 class ImageCacheDebugViewController: UITableViewController {
 
     private let titles = [
-        "Favorites (Bookmark Cache)",
-        "Bookmarks (Bookmark Cache)",
-        "Tabs (Tabs Cache)"
+        Sections.favorites: "Favorites (Bookmark Cache)",
+        Sections.bookmarks: "Bookmarks (Bookmark Cache)",
+        Sections.tabs: "Tabs (Tabs Cache)"
     ]
+
+    enum Sections: Int, CaseIterable {
+
+        case favorites
+        case bookmarks
+        case tabs
+
+    }
 
     let imageNotFound = UIImage(systemName: "exclamationmark.circle")
     let imageError = UIImage(systemName: "exclamationmark.triangle")
@@ -34,32 +42,33 @@ class ImageCacheDebugViewController: UITableViewController {
     let tabsModel = TabsModel.get() ?? TabsModel(desktop: false)
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 3
+        return Sections.allCases.count
     }
 
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        guard let section = Sections(rawValue: section) else { return nil }
         return titles[section]
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        switch indexPath.section {
+        switch Sections(rawValue: indexPath.section) {
 
-        case 0:
+        case .favorites:
             cell.textLabel?.text = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
             cell.imageView?.loadFavicon(forDomain: bookmarksManager.favorite(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
                 cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
                 cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
             }
 
-        case 1:
+        case .bookmarks:
             cell.textLabel?.text = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
             cell.imageView?.loadFavicon(forDomain: bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
                 cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
                 cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
             }
 
-        case 2:
+        case .tabs:
             if let link = tabsModel.get(tabAt: indexPath.row).link {
                 cell.textLabel?.text = link.url.host
                 cell.imageView?.loadFavicon(forDomain: tabsModel.get(tabAt: indexPath.row).link?.url.host, usingCache: .tabs) {
@@ -79,12 +88,36 @@ class ImageCacheDebugViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        switch section {
-        case 0: return bookmarksManager.favoritesCount
-        case 1: return bookmarksManager.bookmarksCount
-        case 2: return tabsModel.count
+        switch Sections(rawValue: section) {
+        case .favorites: return bookmarksManager.favoritesCount
+        case .bookmarks: return bookmarksManager.bookmarksCount
+        case .tabs: return tabsModel.count
         default: return 0
         }
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+        let host: String?
+        switch Sections(rawValue: indexPath.section) {
+        case .favorites: host = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
+        case .bookmarks: host = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
+        case .tabs: host = tabsModel.get(tabAt: indexPath.row).link?.url.host
+        default: host = nil
+        }
+        guard let domain = host, let cell = tableView.cellForRow(at: indexPath) else { return }
+        share(image: cell.imageView?.image, forDomain: domain, fromView: cell)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    private func share(image: UIImage?, forDomain domain: String, fromView view: UIView) {
+        guard let image = image else { return }
+        let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        controller.title = domain
+        if let popover = controller.popoverPresentationController {
+            popover.sourceView = view
+        }
+        present(controller, animated: true)
     }
 
     private func describe(_ image: UIImage?) -> String {

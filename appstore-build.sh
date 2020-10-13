a207badd1269e222a6d6e044d616e9242cbdb927
Chris Brind <brindy@duckduckgo.com>
2020-10-13 14:20:33 +0100

    remove build script and use bitrise uids for profile names

--- appstore-build.sh
+++
@@ -1,5 +0,0 @@
-
-xcodebuild -allowProvisioningUpdates -scheme DuckDuckGo clean archive -configuration release -sdk iphoneos -archivePath ./DuckDuckGo.xcarchive
-xcodebuild -allowProvisioningUpdates -exportArchive -archivePath ./DuckDuckGo.xcarchive -exportOptionsPlist ./appStoreExportOptions.plist -exportPath .
-
-ls -la .

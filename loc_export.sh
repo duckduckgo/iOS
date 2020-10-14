da99036a36ed322c02bc3425cc744dfc52623de3
Bartek Waresiak <bartek@duckduckgo.com>
2020-10-14 13:40:59 +0200

    Directly point to xliff file when exporting

--- loc_export.sh
+++ loc_export.sh
@@ -4,5 +4,6 @@ echo "Updating..."
 ./loc_update.sh
 
 echo "Exporting..."
+rm -r loc
 xcodebuild -exportLocalizations -project DuckDuckGo.xcodeproj -localizationPath loc -exportLanguage en
-open loc
+open "loc/en.xcloc/Localized Contents"

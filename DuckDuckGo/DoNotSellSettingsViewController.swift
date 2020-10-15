42d7a4515460b6e482c3b5873aac5c5823f2389b
bwaresiak <bartek@duckduckgo.com>
2020-10-15 16:54:24 +0200

    Translations (#727)
    * Scripts to import and update localization files
    
    * Update Localizable.strings when building
    
    * Update Localizable.strings file
    
    * Initial setup for new languages
    
    * Import localizations
    
    * Script to quickly export xliff file
    
    * Remove subtitle from Launch Screen
    
    * Fix update script, create en localization for Widgets
    
    * Fix duplicated Norwegian translation issue
    
    * Add missing English resources
    
    * Localize stringsdict file
    
    * Import stringsdict
    
    * Fix import script
    
    * Remove duplicate of Norwegian language
    
    * Add hint to placeholder string
    
    * Translate welcome message
    
    * Add comment to translatable string
    
    * Fix glitch on fullscreen dax dialogs
    
    * Replace DoNotSell title with GPC to simplofy translation effort
    
    * Update strigns in storyboard based on UserText
    
    * Improvements to Settings autolayout
    
    * Fix layout for GPC
    
    * Import latest localizations
    
    * Fix GPC screen layout
    
    * Fix untranslatable strings
    
    * Tweak GPC code, default to system language on start, fix onboarding sizing
    
    * Make placeholders attributed
    
    * Fix Tracker Networks layout
    
    * Convert Localizable.strings UTF-16LE to UTF-8
    
    * Directly point to xliff file when exporting
    
    * Update translations
    
    * Update stringsdict file
    
    * Fix welcome message leading and trailing constraints

--- DuckDuckGo/DoNotSellSettingsViewController.swift
+++ DuckDuckGo/DoNotSellSettingsViewController.swift
@@ -39,7 +39,6 @@ class DoNotSellSettingsViewController: UITableViewController {
         
         doNotSellToggle.isOn = appSettings.sendDoNotSell
         
-        infoTextView.text = UserText.doNotSellInfoText
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineHeightMultiple = 1.16
         infoTextView.attributedText = NSAttributedString(string: UserText.doNotSellInfoText,
@@ -53,6 +52,26 @@ class DoNotSellSettingsViewController: UITableViewController {
         disclaimerTextView.backgroundColor = .clear
         
         applyTheme(ThemeManager.shared.currentTheme)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        if let header = tableView.tableHeaderView {
+            let newSize = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+            header.frame.size.height = newSize.height
+            DispatchQueue.main.async {
+                self.tableView.tableHeaderView = header
+            }
+        }
+        
+        if let footer = tableView.tableFooterView {
+            let newSize = footer.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+            footer.frame.size.height = newSize.height
+            DispatchQueue.main.async {
+                self.tableView.tableFooterView = footer
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {

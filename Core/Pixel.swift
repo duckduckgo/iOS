2544b603d5759c7c979a933d3da957a619e1cf78
Sam Symons <sam@samsymons.com>
2020-11-09 16:00:06 -0800

    Remove notification opt in/out pixels.
    These weren’t used anywhere before this change.

--- Core/Pixel.swift
+++ Core/Pixel.swift
@@ -137,9 +137,6 @@ public enum PixelName: String {
     case feedbackNegativePerformanceVideo = "mfbs_negative_performance_video"
     case feedbackNegativePerformanceOther = "mfbs_negative_performance_other"
     
-    case notificationOptIn = "m_ne"
-    case notificationOptOut = "m_nd"
-    
     case brokenSiteReported = "m_bsr"
     
     case brokenSiteReport = "epbf"

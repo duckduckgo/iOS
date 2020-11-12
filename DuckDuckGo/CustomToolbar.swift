1356a8082fab14722693a1b79b1e02c0519c86a3
Chris Brind <brindy@duckduckgo.com>
2020-11-12 10:31:53 +0000

    try checking hit test area programatically for custom views

---
+++ DuckDuckGo/CustomToolbar.swift
@@ -0,0 +1,39 @@
+//
+//  CustomToolbar.swift
+//  DuckDuckGo
+//
+//  Copyright © 2020 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+class CustomToolbar: UIToolbar {
+
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        print(#function, event)
+
+        guard let touch = event?.allTouches?.first else { return super.hitTest(point, with: event) }
+
+        let item = items?.first(where: {
+            guard let customView = $0.customView else { return false }
+            let location = touch.location(in: customView)
+            print($0, location)
+            return false
+        })
+
+        return item?.customView ?? super.hitTest(point, with: event)
+    }
+
+}

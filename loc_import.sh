82a4b1a6550f0759b1e452ec4b676544f7c3152b
Chris Brind <brindy@duckduckgo.com>
2020-11-17 13:10:11 +0000

    imported import script and imported translations

--- loc_import.sh
+++ loc_import.sh
@@ -1,25 +1,47 @@
 #!/bin/sh
 
+if [ $# -ne 2 ]; then
+    echo 
+    echo "USAGE: ./loc_import.sh <base import directory> <xliff name>"
+    echo
+    echo "e.g. ./loc_import.sh ~/Downloads/my-job-name-2/ en-2.xliff"
+    echo
+    exit 1
+fi
+
 for dir in $1/*; do
-  echo
+  echo processing $dir
   locale=`basename "$dir"`
   targetLocale=$(echo $locale | cut -f1 -d-)
 
-  if test -f "$dir/en.xliff"; then
+  if test -f "$dir/$2"; then
     echo "Processing $locale xliff"
 
     if [ "$locale" != "$targetLocale" ];
     then
       echo "Changing locale from '$locale' to '$targetLocale'"
-      sed -i '.bak' "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" "$dir/en.xliff"
-      rm "$dir/en.xliff.bak"
+      sed -i '.bak' "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" "$dir/$2"
+      rm "$dir/$2.bak"
     fi
 
-    echo "Importing $dir/en.xliff..."
-    xcodebuild -importLocalizations -project DuckDuckGo.xcodeproj -localizationPath "$dir/en.xliff"
+    echo "Importing $dir/$2 ..."
+    xcodebuild -importLocalizations -project DuckDuckGo.xcodeproj -localizationPath "$dir/$2"
+
+    if [ $? -ne 0 ]; then
+       echo "ERROR: Failed to import $dir/$2"
+       echo 
+       echo "Check translation folder and files then try again."
+       echo 
+       exit 1
+    fi 
+
   elif test -f "$dir/Localizable.stringsdict"; then
     echo "Processing $locale stringsdict"
     cp "$dir/Localizable.stringsdict" "DuckDuckGo/$targetLocale.lproj/"
+  else
+    echo "ERROR: $2 or Localizable.stringsdict not found in $dir"
+    echo
+    exit 1
   fi
 
 done

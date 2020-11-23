e71ce4e60ca2cfa9998df20bd4b9682b8fbc9b4a
Bartek Waresiak <bartek@duckduckgo.com>
2020-11-23 15:35:06 +0100

    Make second parameter optional when importing

--- loc_import.sh
+++ loc_import.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 
+xliffName=$2
+
 if [ $# -ne 2 ]; then
-    echo 
-    echo "USAGE: ./loc_import.sh <base import directory> <xliff name>"
-    echo
-    echo "e.g. ./loc_import.sh ~/Downloads/my-job-name-2/ en-2.xliff"
-    echo
-    exit 1
+    xliffName="en.xliff"
 fi
 
 for dir in $1/*; do
@@ -14,32 +11,32 @@ for dir in $1/*; do
   locale=`basename "$dir"`
   targetLocale=$(echo $locale | cut -f1 -d-)
 
-  if test -f "$dir/$2"; then
+  if test -f "$dir/$xliffName"; then
     echo "Processing $locale xliff"
 
     if [ "$locale" != "$targetLocale" ];
     then
       echo "Changing locale from '$locale' to '$targetLocale'"
-      sed -i '.bak' "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" "$dir/$2"
-      rm "$dir/$2.bak"
+      sed -i '.bak' "s/target-language=\"$locale\"/target-language=\"$targetLocale\"/" "$dir/$xliffName"
+      rm "$dir/$xliffName.bak"
     fi
 
-    echo "Importing $dir/$2 ..."
-    xcodebuild -importLocalizations -project DuckDuckGo.xcodeproj -localizationPath "$dir/$2"
+    echo "Importing $dir/$xliffName ..."
+    xcodebuild -importLocalizations -project DuckDuckGo.xcodeproj -localizationPath "$dir/$xliffName"
 
     if [ $? -ne 0 ]; then
-       echo "ERROR: Failed to import $dir/$2"
-       echo 
+       echo "ERROR: Failed to import $dir/$xliffName"
+       echo
        echo "Check translation folder and files then try again."
-       echo 
+       echo
        exit 1
-    fi 
+    fi
 
   elif test -f "$dir/Localizable.stringsdict"; then
     echo "Processing $locale stringsdict"
     cp "$dir/Localizable.stringsdict" "DuckDuckGo/$targetLocale.lproj/"
   else
-    echo "ERROR: $2 or Localizable.stringsdict not found in $dir"
+    echo "ERROR: $xliffName or Localizable.stringsdict not found in $dir"
     echo
     exit 1
   fi

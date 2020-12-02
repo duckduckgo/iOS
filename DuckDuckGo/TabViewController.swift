14fdf8aa6ed1e0abab3b5b9764818844dcf6204f
Brad Slayter <slayterdevelopment@gmail.com>
2020-12-02 07:26:26 -0600

    Remove GPC header injection

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -31,8 +31,6 @@ class TabViewController: UIViewController {
         static let frameLoadInterruptedErrorCode = 102
         
         static let trackerNetworksAnimationDelay: TimeInterval = 0.7
-        
-        static let secGPCHeader = "Sec-GPC"
     }
     
     @IBOutlet private(set) weak var error: UIView!
@@ -1005,40 +1003,10 @@ extension TabViewController: WKNavigationDelegate {
         detectedNewNavigation()
         checkLoginDetectionAfterNavigation()
     }
-    
-    private func requestForDoNotSell(basedOn incomingRequest: URLRequest) -> URLRequest? {
-        var request = incomingRequest
-        // Add Do Not sell header if needed
-        if appSettings.sendDoNotSell {
-            if let headers = request.allHTTPHeaderFields,
-               headers.firstIndex(where: { $0.key == Constants.secGPCHeader }) == nil {
-                request.addValue("1", forHTTPHeaderField: Constants.secGPCHeader)
-                return request
-            }
-        } else {
-            // Check if DN$ header is still there and remove it
-            if let headers = request.allHTTPHeaderFields, headers.firstIndex(where: { $0.key == Constants.secGPCHeader }) != nil {
-                request.setValue(nil, forHTTPHeaderField: Constants.secGPCHeader)
-                return request
-            }
-        }
-        
-        return nil
-    }
             
     func webView(_ webView: WKWebView,
                  decidePolicyFor navigationAction: WKNavigationAction,
                  decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-
-        if navigationAction.isTargetingMainFrame(),
-           !(navigationAction.request.url?.isCustomURLScheme() ?? false),
-           lastCommittedURL != navigationAction.request.url,
-           navigationAction.navigationType != .backForward,
-           let request = requestForDoNotSell(basedOn: navigationAction.request) {
-            decisionHandler(.cancel)
-            load(urlRequest: request)
-            return
-        }
 
         if navigationAction.navigationType == .linkActivated,
            let url = navigationAction.request.url,

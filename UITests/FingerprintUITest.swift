518a17c73b138eb20b9e8c3f87eb0f1c02ee83b1
Brad Slayter <slayterdevelopment@gmail.com>
2020-12-09 08:11:51 -0600

    Clean up test

--- UITests/FingerprintUITest.swift
+++ UITests/FingerprintUITest.swift
@@ -24,15 +24,20 @@ import XCTest
 class FingerprintUITest: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        let app = XCUIApplication()
+
+        // Add a bookmark to edit to a bookmarklet later
+        app/*@START_MENU_TOKEN@*/.searchFields["searchEntry"]/*[[".searchFields[\"Search or enter address\"]",".searchFields[\"searchEntry\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app
+            .searchFields["searchEntry"]
+            .typeText("https://duckduckgo.com\n")
+        app.buttons["Browsing Menu"].tap()
+        app.sheets.scrollViews.otherElements.buttons["Add to Bookmarks"].tap()
+        app.toolbars["Toolbar"].buttons["Bookmarks"].tap()
     }
 
     override func tearDownWithError() throws {
@@ -47,15 +52,6 @@ class FingerprintUITest: XCTestCase {
 
     func test() throws {
         let app = XCUIApplication()
-
-        // Add a bookmark to edit to a bookmarklet later
-        app/*@START_MENU_TOKEN@*/.searchFields["searchEntry"]/*[[".searchFields[\"Search or enter address\"]",".searchFields[\"searchEntry\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app
-            .searchFields["searchEntry"]
-            .typeText("https://duckduckgo.com\n")
-        app.buttons["Browsing Menu"].tap()
-        app.sheets.scrollViews.otherElements.buttons["Add to Bookmarks"].tap()
-        app.toolbars["Toolbar"].buttons["Bookmarks"].tap()
         
         // Edit bookmark into bookmarklet to verify fingerprinting test
         let bookmarksNavigationBar = app.navigationBars["Bookmarks"]

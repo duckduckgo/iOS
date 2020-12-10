ac0c9dcef78c335338886668a602fb866a304868
Brad Slayter <slayterdevelopment@gmail.com>
2020-12-10 07:56:49 -0600

    UI test target

--- FingerprintingUITests/FingerprintUITest.swift
+++ FingerprintingUITests/FingerprintUITest.swift
@@ -31,7 +31,7 @@ class FingerprintUITest: XCTestCase {
         let app = XCUIApplication()
 
         // Add a bookmark to edit to a bookmarklet later
-        app/*@START_MENU_TOKEN@*/.searchFields["searchEntry"]/*[[".searchFields[\"Search or enter address\"]",".searchFields[\"searchEntry\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.searchFields["searchEntry"].tap()
         app
             .searchFields["searchEntry"]
             .typeText("https://duckduckgo.com\n")
@@ -45,8 +45,8 @@ class FingerprintUITest: XCTestCase {
         let app = XCUIApplication()
         app.toolbars["Toolbar"].buttons["Bookmarks"].tap()
         let tablesQuery = app.tables
-        tablesQuery/*@START_MENU_TOKEN@*/.staticTexts["DuckDuckGo — Privacy, simplified."]/*[[".cells.staticTexts[\"DuckDuckGo — Privacy, simplified.\"]",".staticTexts[\"DuckDuckGo — Privacy, simplified.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
-        tablesQuery/*@START_MENU_TOKEN@*/.buttons["Delete"]/*[[".cells.buttons[\"Delete\"]",".buttons[\"Delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        tablesQuery.staticTexts["DuckDuckGo — Privacy, simplified."].swipeLeft()
+        tablesQuery.buttons["Delete"].tap()
         app.navigationBars["Bookmarks"].buttons["Done"].tap()
     }
 
@@ -56,23 +56,23 @@ class FingerprintUITest: XCTestCase {
         // Edit bookmark into bookmarklet to verify fingerprinting test
         let bookmarksNavigationBar = app.navigationBars["Bookmarks"]
         bookmarksNavigationBar.buttons["Edit"].tap()
-        app.tables/*@START_MENU_TOKEN@*/.staticTexts["DuckDuckGo — Privacy, simplified."]/*[[".cells.staticTexts[\"DuckDuckGo — Privacy, simplified.\"]",".staticTexts[\"DuckDuckGo — Privacy, simplified.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        app.alerts["Edit Bookmark"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["www.example.com"]/*[[".cells.textFields[\"www.example.com\"]",".textFields[\"www.example.com\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap(withNumberOfTaps: 3, numberOfTouches: 1)
-        app.alerts["Edit Bookmark"].scrollViews.otherElements.collectionViews/*@START_MENU_TOKEN@*/.textFields["www.example.com"]/*[[".cells.textFields[\"www.example.com\"]",".textFields[\"www.example.com\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        app.tables.staticTexts["DuckDuckGo — Privacy, simplified."].tap()
+        app.alerts["Edit Bookmark"].scrollViews.otherElements.collectionViews.textFields["www.example.com"].tap(withNumberOfTaps: 3, numberOfTouches: 1)
+        app.alerts["Edit Bookmark"].scrollViews.otherElements.collectionViews.textFields["www.example.com"]
             .typeText("javascript:(function(){const values = {'screen.availTop': 0,'screen.availLeft': 0,'screen.availWidth': screen.width,'screen.availHeight': screen.height,'screen.colorDepth': 24,'screen.pixelDepth': 24,'window.screenY': 0,'window.screenLeft': 0,'navigator.webkitTemporaryStorage': undefined,'navigator.webkitPersistentStorage': undefined,'navigator.doNotTrack': undefined};var passed = true;var reason = null;for (const test of results.results) {if (values[test.id] !== undefined) {if (values[test.id] !== test.value) {console.log(test.id, values[test.id]);reason = test.id;passed = false;break;}}}var elem = document.createElement('p');elem.innerHTML = (passed) ? 'TEST PASSED' : 'TEST FAILED: ' + reason;document.body.insertBefore(elem, document.body.childNodes[0]);}());")
         app.alerts["Edit Bookmark"].scrollViews.otherElements.buttons["Save"].tap()
         bookmarksNavigationBar.buttons["Done"].tap()
         bookmarksNavigationBar.buttons["Done"].tap()
         
         // Clear all tabs and data
-        app.toolbars["Toolbar"]/*@START_MENU_TOKEN@*/.buttons["Fire"]/*[[".buttons[\"Close all tabs and clear data\"]",".buttons[\"Fire\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.toolbars["Toolbar"].buttons["Fire"].tap()
         app.sheets.scrollViews.otherElements.buttons["Close Tabs and Clear Data"].tap()
         
         sleep(2)
         
         // Go to fingerprinting test page
         app
-            /*@START_MENU_TOKEN@*/.searchFields["searchEntry"]/*[[".searchFields[\"Search or enter address\"]",".searchFields[\"searchEntry\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+            .searchFields["searchEntry"]
             .tap()
         app
             .searchFields["searchEntry"]
@@ -82,7 +82,7 @@ class FingerprintUITest: XCTestCase {
         
         // Run the new bookmarklet
         app.toolbars["Toolbar"].buttons["Bookmarks"].tap()
-        app.tables/*@START_MENU_TOKEN@*/.staticTexts["DuckDuckGo — Privacy, simplified."]/*[[".cells.staticTexts[\"DuckDuckGo — Privacy, simplified.\"]",".staticTexts[\"DuckDuckGo — Privacy, simplified.\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        app.tables.staticTexts["DuckDuckGo — Privacy, simplified."].tap()
         
         // Verify the test passed
         XCTAssertTrue(webview.staticTexts["TEST PASSED"].waitForExistence(timeout: 25), "Test not run")

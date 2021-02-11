d382b0b907eddbb33097ed6395ebf6f8425cfe20
Elle Sullivan <esullivan@duckduckgo.com>
2021-02-11 12:29:12 +0000

    Fix dax dialogs check failing if there's a redirect

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -940,7 +940,7 @@ extension TabViewController: WKNavigationDelegate {
         // For the email beta, we don't want to show dax dialogs after onboarding, because we instead take the
         // user to the email sign up page.
         // THIS SHOULD BE REMOVED BEFORE THE EMAIL FEATURE IS OUT OF BETA
-        guard url != appUrls.emailLandingPage else { return }
+        guard url?.host != appUrls.emailLandingPage.host else { return }
         
         guard !isLinkPreview else { return }
 

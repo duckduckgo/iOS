f2447f2f7b7920b8f8b939d91a8a1586a38c56e0
Bartek Waresiak <bartek@duckduckgo.com>
2021-03-18 08:59:32 +0100

    Theme tweaks and animation updates

---
+++ DuckDuckGo/ActionMessageView.swift
@@ -0,0 +1,58 @@
+//
+//  ActionMessageView.swift
+//  DuckDuckGo
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+
+extension ActionMessageView: NibLoading {}
+
+class ActionMessageView: UIView {
+    
+    
+    
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var actionButton: UIButton!
+    
+    private var action: () -> Void = {}
+    
+    static func loadFromXib() -> ActionMessageView {
+        return ActionMessageView.load(nibName: "ActionMessageView")
+    }
+    
+    static func present(message: String, actionTitle: String, onAction: @escaping () -> Void) {
+        guard let window = UIApplication.shared.keyWindow else { return }
+        
+        let messageView = loadFromXib()
+        
+        messageView.message.setAttributedTextString(message)
+        messageView.actionButton.setTitle(actionTitle, for: .normal)
+        messageView.action = onAction
+        
+        window.addSubview(messageView)
+        
+        window.safeAreaLayoutGuide.bottomAnchor.constraint(equalTo: messageView.bottomAnchor, constant: 40).isActive = true
+        messageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 10
+    }
+    
+}

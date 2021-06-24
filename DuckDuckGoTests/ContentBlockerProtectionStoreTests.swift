4243947a897dcdcd1c9e1f2dd62b11fd1dc3b888
Chris Brind <brindy@duckduckgo.com>
2021-06-24 18:12:11 +0100

    add unit test for checking temporary protected domains

---
+++ DuckDuckGoTests/ContentBlockerProtectionStoreTests.swift
@@ -0,0 +1,39 @@
+//
+//  ContentBlockerProtectionStoreTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import Core
+@testable import DuckDuckGo
+
+class ContentBlockerProtectionStoreTests: XCTestCase {
+
+    func testWhenCheckingDomainsAreProtected_ThenUsesPersistedUnprotectedDomainList() {
+        let domains = [
+            "domain1.com",
+            "domain2.com",
+            "domain3.com"
+        ].joined(separator: "\n").data(using: .utf8)!
+        _ = StorageCacheProvider().current.fileStore.persist(domains, forConfiguration: .temporaryUnprotectedSites)
+        let protection = ContentBlockerProtectionUserDefaults()
+        XCTAssertFalse(protection.isTempUnprotected(domain: "main1.com"))
+        XCTAssertFalse(protection.isTempUnprotected(domain: "notdomain1.com"))
+        XCTAssertTrue(protection.isTempUnprotected(domain: "domain1.com"))
+    }
+
+}

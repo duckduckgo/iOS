f2905acd7e2f768c5d1ae2f77bde152ce59dd8f8
Sam Symons <sam@samsymons.com>
2021-06-23 22:25:15 -0700

    Update waitlist copy.

--- DuckDuckGo/UserText.swift
+++ DuckDuckGo/UserText.swift
@@ -358,7 +358,7 @@ public struct UserText {
     public static let emailWaitlistJoinedWithNotificationSummary = NSLocalizedString("email.waitlist.joined.notification", value: "We’ll send you a notification when\nEmail Protection is ready for you. Learn more.", comment: "Description text for the email waitlist")
     public static let emailWaitlistJoinedWithoutNotificationSummary = NSLocalizedString("email.waitlist.joined.no-notification", value: "Your invite will show up here when we’re ready for you. Want to get a notification when it arrives? Learn more about Email Protection.", comment: "Description text for the email waitlist")
 
-    public static let emailWaitlistJoinWaitlist = NSLocalizedString("email.waitlist.join", value: "Join the Waitlist", comment: "Action button text for the email waitlist")
+    public static let emailWaitlistJoinWaitlist = NSLocalizedString("email.waitlist.join", value: "Join the Private Waitlist", comment: "Action button text for the email waitlist")
     public static let emailWaitlistGetStarted = NSLocalizedString("email.waitlist.get-started", value: "Get Started", comment: "Action button text for the email waitlist")
 
     public static let emailWaitlistHaveInviteCode = NSLocalizedString("email.waitlist.have-invite-code", value: "I have an Invite Code", comment: "Invite code button text for the email waitlist")

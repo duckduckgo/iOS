bfd8d41a264657a5a06eeb905f4831b4a702a237
Elle Sullivan <esullivan@duckduckgo.com>
2021-07-19 16:58:57 +0100

    Fix site rating not showing on some back/forward navigation

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -1003,6 +1003,7 @@ extension TabViewController: WKNavigationDelegate {
         hideProgressIndicator()
         webpageDidFailToLoad()
         checkForReloadOnError()
+        scheduleTrackerNetworksAnimation(collapsing: true)
     }
 
     private func webpageDidFailToLoad() {

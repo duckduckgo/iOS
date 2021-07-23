74ef3fd91ea9b5084df302e60d4af5cd460164f5
Elle Sullivan <esullivan@duckduckgo.com>
2021-07-23 16:38:16 +0100

    Revert "Remove tests since feature is disabled"
    This reverts commit de0572caa0168356b2df9ce14f15546a8336f801.

--- DuckDuckGoTests/DaxDialogFireEducationTests.swift
+++ DuckDuckGoTests/DaxDialogFireEducationTests.swift
@@ -17,7 +17,6 @@
 //  limitations under the License.
 //
 
-/* Commented out since feature is disabled
 import XCTest
 @testable import DuckDuckGo
 @testable import Core
@@ -78,6 +77,7 @@ class DaxDialogFireEducationTests: XCTestCase {
 
     func testWhenEachVersionOfTrackersMessageIsShownThenFormattedCorrectlyAndNotShownAgain() {
 
+        // swiftlint:disable line_length
         let testCases = [
             (urls: [ URLs.google ], expected: DaxDialogs.BrowsingSpec.withOneTracker.format(args: "Google"), line: #line),
             (urls: [ URLs.google, URLs.amazon ], expected: DaxDialogs.BrowsingSpec.withMutipleTrackers.format(args: 0, "Google", "Amazon.com"), line: #line),
@@ -86,6 +86,7 @@ class DaxDialogFireEducationTests: XCTestCase {
             (urls: [ URLs.facebook, URLs.google, URLs.amazon ], expected: DaxDialogs.BrowsingSpec.withMutipleTrackers.format(args: 1, "Google", "Facebook"), line: #line),
             (urls: [ URLs.facebook, URLs.google, URLs.amazon, URLs.tracker ], expected: DaxDialogs.BrowsingSpec.withMutipleTrackers.format(args: 2, "Google", "Facebook"), line: #line)
         ]
+        // swiftlint:enable line_length
 
         testCases.forEach { testCase in
             
@@ -272,4 +273,3 @@ class DaxDialogFireEducationTests: XCTestCase {
                                       blocked: true)
     }
 }
-*/

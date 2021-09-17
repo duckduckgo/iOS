f84500e3b171d4b326a499ebb5deeeb69a2de2ed
Elle Sullivan <esullivan@duckduckgo.com>
2021-09-17 16:59:17 +0100

    Add bookmark details cell

---
+++ DuckDuckGo/BookmarkDetailsCell.swift
@@ -0,0 +1,51 @@
+//
+//  BookmarkDetailsCell.swift
+//  DuckDuckGo
+//
+//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import UIKit
+import Core
+
+class BookmarkDetailsCell: UITableViewCell {
+
+    static let reuseIdentifier = "BookmarkDetailsCell"
+    
+    var title: String? {
+        get {
+            titleTextField.text
+        }
+        set {
+            titleTextField.text = newValue
+        }
+    }
+    
+    var urlString: String? {
+        get {
+            urlTextField.text
+        }
+        set {
+            urlTextField.text = newValue
+        }
+    }
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var urlTextField: UITextField!
+    @IBOutlet weak var faviconImageView: UIImageView!
+    
+    //TODO set favicon
+}
+

31431b73722fe3397df7ad7ff808b0c69246fec1
Elle Sullivan <esullivan@duckduckgo.com>
2021-10-18 12:29:06 +0100

    push tmp code

--- DuckDuckGo/ImageCacheDebugViewController.swift
+++ DuckDuckGo/ImageCacheDebugViewController.swift
@@ -65,19 +65,20 @@ class ImageCacheDebugViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         switch Sections(rawValue: indexPath.section) {
 
-        case .favorites:
-            cell.textLabel?.text = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
-            cell.imageView?.loadFavicon(forDomain: bookmarksManager.favorite(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
-                cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
-                cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
-            }
-
-        case .bookmarks:
-            cell.textLabel?.text = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
-            cell.imageView?.loadFavicon(forDomain: bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
-                cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
-                cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
-            }
+        //TODO
+//        case .favorites:
+//            cell.textLabel?.text = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
+//            cell.imageView?.loadFavicon(forDomain: bookmarksManager.favorite(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
+//                cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
+//                cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
+//            }
+//
+//        case .bookmarks:
+//            cell.textLabel?.text = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
+//            cell.imageView?.loadFavicon(forDomain: bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host, usingCache: .bookmarks) {
+//                cell.imageView?.image = $1 ? self.imageNotFound : $0 ?? self.imageError
+//                cell.detailTextLabel?.text = self.describe($1 ? nil : $0)
+//            }
 
         case .tabs:
             if let link = tabsModel.get(tabAt: indexPath.row).link {
@@ -100,8 +101,8 @@ class ImageCacheDebugViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch Sections(rawValue: section) {
-        case .favorites: return bookmarksManager.favoritesCount
-        case .bookmarks: return bookmarksManager.bookmarksCount
+        case .favorites: return 0 //TODO bookmarksManager.favoritesCount
+        case .bookmarks: return 0 //TODO what do here? count could be inefficient
         case .tabs: return tabsModel.count
         default: return 0
         }
@@ -109,16 +110,19 @@ class ImageCacheDebugViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
 
-        let host: String?
-        switch Sections(rawValue: indexPath.section) {
-        case .favorites: host = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
-        case .bookmarks: host = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
-        case .tabs: host = tabsModel.get(tabAt: indexPath.row).link?.url.host
-        default: host = nil
-        }
-        guard let domain = host, let cell = tableView.cellForRow(at: indexPath) else { return }
-        share(image: cell.imageView?.image, forDomain: domain, fromView: cell)
-        tableView.deselectRow(at: indexPath, animated: true)
+        //TODO what is this even supposed to do?
+        //and how do we translate it to new method? Flat struture?
+        //yeah...that's probably it, we'll need a way of getting just bookmarks flat
+//        let host: String?
+//        switch Sections(rawValue: indexPath.section) {
+//        case .favorites: host = bookmarksManager.favorite(atIndex: indexPath.row)?.url.host
+//        case .bookmarks: host = bookmarksManager.bookmark(atIndex: indexPath.row)?.url.host
+//        case .tabs: host = tabsModel.get(tabAt: indexPath.row).link?.url.host
+//        default: host = nil
+//        }
+//        guard let domain = host, let cell = tableView.cellForRow(at: indexPath) else { return }
+//        share(image: cell.imageView?.image, forDomain: domain, fromView: cell)
+//        tableView.deselectRow(at: indexPath, animated: true)
     }
 
     private func share(image: UIImage?, forDomain domain: String, fromView view: UIView) {

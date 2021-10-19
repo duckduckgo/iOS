713b21506761b49329172f02aec1c784a9c1406c
Michal Smaga <msmaga@duckduckgo.com>
2021-10-19 12:07:55 +0200

    Test JS for fetching font styles on page

--- Core/TextSizeUserScript.swift
+++ Core/TextSizeUserScript.swift
@@ -27,30 +27,66 @@ public class TextSizeUserScript: NSObject, UserScript {
 
     public var source: String = """
 (function() {
-    window.print = function() {
-        // webkit.messageHandlers.printHandler.postMessage({});
-        document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust='100%';
-    };
+
+    function styleInPage(css, verbose){
+        if(typeof getComputedStyle== "undefined")
+        getComputedStyle= function(elem){
+            return elem.currentStyle;
+        }
+        var who, hoo, values= [], val,
+        nodes= document.body.getElementsByTagName('*'),
+        L= nodes.length;
+        for(var i= 0; i<L; i++){
+            who= nodes[i];
+            if(who.style){
+                hoo= '#'+(who.id || who.nodeName+'('+i+')');
+                val= who.style.fontFamily || getComputedStyle(who, '')[css];
+                if(val){
+                    if(verbose) values.push([hoo, val]);
+                    else if(values.indexOf(val)== -1) values.push(val);
+                }
+                val_before = getComputedStyle(who, ':before')[css];
+                if(val_before){
+                    if(verbose) values.push([hoo, val_before]);
+                    else if(values.indexOf(val_before)== -1) values.push(val_before);
+                }
+                val_after= getComputedStyle(who, ':after')[css];
+                if(val_after){
+                    if(verbose) values.push([hoo, val_after]);
+                    else if(values.indexOf(val_after)== -1) values.push(val_after);
+                }
+            }
+        }
+        return values;
+    }
+
+    window.addEventListener("DOMContentLoaded", function(event) {
+                            webkit.messageHandlers.log.postMessage("fontFamily:");
+                            var resultFontFamily = styleInPage('fontFamily');
+                            webkit.messageHandlers.log.postMessage(JSON.stringify(resultFontFamily));
+    }, false)
+
 }) ();
 """
 
     public var injectionTime: WKUserScriptInjectionTime = .atDocumentStart
     public var forMainFrameOnly: Bool = false
-//    public var messageNames: [String] = ["printHandler"]
-//
-//    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-//        delegate?.printingUserScriptDidRequestPrintController(self)
-//    }
-    
-    public func adjustTextSize(_ percentage: Int) {
+    public var messageNames: [String] = []
+
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         
     }
-
 }
 
-extension WKWebView {
+public extension WKWebView {
     
-    public func adjustTextSize(_ percentage: Int) {
-                
+    func adjustTextSize(_ percentage: Int) {
+        let jsString = "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust='\(percentage)%'"
+        evaluateJavaScript(jsString, completionHandler: nil)
+    }
+    
+    func doNotAdjustTextSize() {
+        let jsString = "document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust='none'"
+        evaluateJavaScript(jsString, completionHandler: nil)
     }
 }

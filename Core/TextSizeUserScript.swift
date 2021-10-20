23cdc6170a2dbaf83480eb23d1b946f767621943
Michal Smaga <msmaga@duckduckgo.com>
2021-10-20 13:59:54 +0200

    Refine JS for TextSizeUserScript

--- Core/TextSizeUserScript.swift
+++ Core/TextSizeUserScript.swift
@@ -23,59 +23,37 @@ import WebKit
 
 public class TextSizeUserScript: NSObject, UserScript {
 
+    public var textSizeAdjustment: Float = 1.0
+    
     public weak var delegate: PrintingUserScriptDelegate?
 
-    public var source: String = """
-(function() {
+    public var source: String {
+        let percentage = Int(textSizeAdjustment * 100)
+        return """
+        (function() {
 
-    function styleInPage(css, verbose){
-        if(typeof getComputedStyle== "undefined")
-        getComputedStyle= function(elem){
-            return elem.currentStyle;
-        }
-        var who, hoo, values= [], val,
-        nodes= document.body.getElementsByTagName('*'),
-        L= nodes.length;
-        for(var i= 0; i<L; i++){
-            who= nodes[i];
-            if(who.style){
-                hoo= '#'+(who.id || who.nodeName+'('+i+')');
-                val= who.style.fontFamily || getComputedStyle(who, '')[css];
-                if(val){
-                    if(verbose) values.push([hoo, val]);
-                    else if(values.indexOf(val)== -1) values.push(val);
-                }
-                val_before = getComputedStyle(who, ':before')[css];
-                if(val_before){
-                    if(verbose) values.push([hoo, val_before]);
-                    else if(values.indexOf(val_before)== -1) values.push(val_before);
-                }
-                val_after= getComputedStyle(who, ':after')[css];
-                if(val_after){
-                    if(verbose) values.push([hoo, val_after]);
-                    else if(values.indexOf(val_after)== -1) values.push(val_after);
-                }
-            }
-        }
-        return values;
+            document.addEventListener("DOMContentLoaded", function(event) {
+                webkit.messageHandlers.log.postMessage(" -- TextSizeUserScript - event DOMContentLoaded");
+                document.getElementsByTagName('body')[0].style.webkitTextSizeAdjust='\(percentage)%'
+        
+                /*
+                var styles = `html {-webkit-text-size-adjust:\(percentage)%}`
+                var styleSheet = document.createElement("style")
+                styleSheet.type = "text/css"
+                styleSheet.innerText = styles
+                document.head.appendChild(styleSheet)
+                */
+            }, false)
+
+        }) ();
+        """
     }
-
-    window.addEventListener("DOMContentLoaded", function(event) {
-                            webkit.messageHandlers.log.postMessage("fontFamily:");
-                            var resultFontFamily = styleInPage('fontFamily');
-                            webkit.messageHandlers.log.postMessage(JSON.stringify(resultFontFamily));
-    }, false)
-
-}) ();
-"""
 
     public var injectionTime: WKUserScriptInjectionTime = .atDocumentStart
     public var forMainFrameOnly: Bool = false
     public var messageNames: [String] = []
 
-    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        
-    }
+    public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) { }
 }
 
 public extension WKWebView {

77044ba100a5f1fad9cdeafab1ae53f03185ffdd
Fernando Bunn <bunn@idevzilla.com>
2021-11-18 16:47:23 +0000

    Use correct var when sending state to searchField function

--- DuckDuckGo/OmniBar.swift
+++ DuckDuckGo/OmniBar.swift
@@ -216,7 +216,7 @@ class OmniBar: UIView {
         if state.showSiteRating {
             searchFieldContainer.revealSiteRatingView()
         } else {
-            searchFieldContainer.hideSiteRatingView(newState)
+            searchFieldContainer.hideSiteRatingView(state)
         }
 
         setVisibility(searchLoupe, hidden: !state.showSearchLoupe)

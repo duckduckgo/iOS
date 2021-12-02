b8b49c623d67a7d55f614e96fc66255a0601ea60
Michal Smaga <msmaga@duckduckgo.com>
2021-12-02 21:36:32 +0100

    Remove unnecessary repetition for the source preparation

--- Core/TextSizeUserScript.swift
+++ Core/TextSizeUserScript.swift
@@ -26,33 +26,29 @@ public class TextSizeUserScript: NSObject, UserScript {
     public static let knownDynamicTypeExceptions: [String] = ["wikipedia.org"]
     public var textSizeAdjustmentInPercents: Int = 100
     
-    public var source: String {
-        let dynamicTypeScalePercentage = UIFontMetrics.default.scaledValue(for: 100)
-        
-        return Self.loadJS("textsize", from: Bundle.core, withReplacements: [
-            "$KNOWN_DYNAMIC_TYPE_EXCEPTIONS$": TextSizeUserScript.knownDynamicTypeExceptions.joined(separator: "\n"),
-            "$TEXT_SIZE_ADJUSTMENT_IN_PERCENTS$": "\(textSizeAdjustmentInPercents)",
-            "$DYNAMIC_TYPE_SCALE_PERCENTAGE$": "\(dynamicTypeScalePercentage)"
-        ])
-    }
+    public var source: String { TextSizeUserScript.makeSource(for: textSizeAdjustmentInPercents) }
 
     public var injectionTime: WKUserScriptInjectionTime = .atDocumentStart
     public var forMainFrameOnly: Bool = false
     public var messageNames: [String] = []
 
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) { }
+    
+    fileprivate static func makeSource(for textSizeAdjustmentInPercents: Int) -> String {
+        let dynamicTypeScalePercentage = UIFontMetrics.default.scaledValue(for: 100)
+        
+        return loadJS("textsize", from: Bundle.core, withReplacements: [
+            "$KNOWN_DYNAMIC_TYPE_EXCEPTIONS$": knownDynamicTypeExceptions.joined(separator: "\n"),
+            "$TEXT_SIZE_ADJUSTMENT_IN_PERCENTS$": "\(textSizeAdjustmentInPercents)",
+            "$DYNAMIC_TYPE_SCALE_PERCENTAGE$": "\(dynamicTypeScalePercentage)"
+        ])
+    }
 }
 
 public extension WKWebView {
     
     func adjustTextSize(_ percentage: Int) {
-        let dynamicTypeScalePercentage = UIFontMetrics.default.scaledValue(for: 100)
-        let jsString = TextSizeUserScript.loadJS("textsize", from: Bundle.core, withReplacements: [
-            "$KNOWN_DYNAMIC_TYPE_EXCEPTIONS$": TextSizeUserScript.knownDynamicTypeExceptions.joined(separator: "\n"),
-            "$TEXT_SIZE_ADJUSTMENT_IN_PERCENTS$": "\(percentage)",
-            "$DYNAMIC_TYPE_SCALE_PERCENTAGE$": "\(dynamicTypeScalePercentage)"
-        ])
-        
+        let jsString = TextSizeUserScript.makeSource(for: percentage)
         evaluateJavaScript(jsString, completionHandler: nil)
     }
 }

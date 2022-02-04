1945c9c6c8e9e4ace1496303488ac8280ca40bdc
Jacek Łyp <jlyp@duckduckgo.com>
2022-02-04 12:38:21 +0100

    Only reset lastUpgradedURL if loading new URL comes from the user action

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -371,7 +371,14 @@ class TabViewController: UIViewController {
     }
     
     public func load(url: URL) {
-        lastUpgradedURL = nil
+        load(url: url, didUpgradeUrl: false)
+    }
+    
+    private func load(url: URL, didUpgradeUrl: Bool) {
+        if !didUpgradeUrl {
+            lastUpgradedURL = nil
+        }
+        
         if !url.isBookmarklet() {
             self.url = url
         }
@@ -1314,7 +1321,7 @@ extension TabViewController: WKNavigationDelegate {
             if isUpgradable, let upgradedUrl = self?.upgradeUrl(url, navigationAction: navigationAction) {
                 NetworkLeaderboard.shared.incrementHttpsUpgrades()
                 self?.lastUpgradedURL = upgradedUrl
-                self?.load(url: upgradedUrl)
+                self?.load(url: upgradedUrl, didUpgradeUrl: true)
                 completion(.cancel)
                 return
             }

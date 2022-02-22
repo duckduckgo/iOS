5e33e911cf6f5d61a3a8d4b56532d5fb9b06792f
Brad Slayter <SlayterDev@users.noreply.github.com>
2022-02-22 07:55:52 -0600

    Use config for autofill (#1064)
    * Use config for autofill
    
    * lint
    
    * Update BSK and embedded config
    
    * Re-add configuration data tests

---
+++ DuckDuckGoTests/AppPrivacyConfigurationTests.swift
@@ -0,0 +1,33 @@
+//
+//  AppPrivacyConfigurationTests.swift
+//
+//  Copyright © 2022 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import Core
+
+class AppPrivacyConfigurationTests: XCTestCase {
+
+    func testWhenEmbeddedDataIsUpdatedThenUpdateSHAAndEtag() {
+
+        let data = AppPrivacyConfigurationDataProvider.loadEmbeddedAsData()
+
+        XCTAssertEqual(data.sha256,
+                       AppPrivacyConfigurationDataProvider.Constants.embeddedConfigurationSHA,
+                       "Error: please update SHA and ETag when changing embedded config")
+    }
+
+}

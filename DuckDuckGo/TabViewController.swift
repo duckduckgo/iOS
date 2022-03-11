a20c3f202b0b4ed9f2edf6cbc3d66683000514e7
bwaresiak <bartek@duckduckgo.com>
2022-03-11 13:32:20 +0100

    Improve local storage & cookie clearing by stopping all operations in open tabs (#1084)

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -185,6 +185,8 @@ class TabViewController: UIViewController {
     private var canDisplayJavaScriptAlert: Bool {
         return !shouldBlockJSAlert && presentedViewController == nil && delegate?.tabCheckIfItsBeingCurrentlyPresented(self) ?? false
     }
+    
+    private var onWebsiteLoaded: (() -> Void)?
 
     static func loadFromStoryboard(model: Tab) -> TabViewController {
         let storyboard = UIStoryboard(name: "Tab", bundle: nil)
@@ -409,8 +411,10 @@ class TabViewController: UIViewController {
         }
     }
     
-    func stopLoading() {
+    func prepareForDataClearing(completion: @escaping () -> Void) {
         webView.stopLoading()
+        webView.load(URLRequest(url: URL(string: "about:blank")!))
+        onWebsiteLoaded = completion
     }
     
     private func load(urlRequest: URLRequest) {
@@ -998,6 +1002,11 @@ extension TabViewController: WKNavigationDelegate {
     
     private func onWebpageDidFinishLoading() {
         os_log("webpageLoading finished", log: generalLog, type: .debug)
+        
+        if let callback = onWebsiteLoaded {
+            callback()
+            onWebsiteLoaded = nil
+        }
         
         siteRating?.finishedLoading = true
         updateSiteRating()

7d6183e13048847532bc5a069a9017274891009f
Sam Symons <sam@samsymons.com>
2022-03-24 12:24:41 -0600

    Fix an issue with Safe Areas.

--- DuckDuckGo/MenuControllerView.swift
+++ DuckDuckGo/MenuControllerView.swift
@@ -106,6 +106,8 @@ struct MenuControllerView<Content: View>: UIViewControllerRepresentable {
             
             preferredContentSize = view.intrinsicContentSize
             view.backgroundColor = .clear
+            
+            disableSafeArea()
         }
         
         override var canBecomeFirstResponder: Bool {
@@ -114,6 +116,33 @@ struct MenuControllerView<Content: View>: UIViewControllerRepresentable {
         
         @objc func menuItemAction(_ sender: Any) {
             self.action?()
+        }
+        
+        // Work around an issue with Safe Areas inside UIHostingController: https://defagos.github.io/swiftui_collection_part3/
+        func disableSafeArea() {
+            guard let viewClass = object_getClass(view) else { return }
+            
+            let viewSubclassName = String(cString: class_getName(viewClass)).appending("_IgnoreSafeArea")
+            if let viewSubclass = NSClassFromString(viewSubclassName) {
+                object_setClass(view, viewSubclass)
+            } else {
+                guard let viewClassNameUtf8 = (viewSubclassName as NSString).utf8String else { return }
+                guard let viewSubclass = objc_allocateClassPair(viewClass, viewClassNameUtf8, 0) else { return }
+                
+                if let method = class_getInstanceMethod(UIView.self, #selector(getter: UIView.safeAreaInsets)) {
+                    let safeAreaInsets: @convention(block) (AnyObject) -> UIEdgeInsets = { _ in
+                        return .zero
+                    }
+
+                    class_addMethod(viewSubclass,
+                                    #selector(getter: UIView.safeAreaInsets),
+                                    imp_implementationWithBlock(safeAreaInsets),
+                                    method_getTypeEncoding(method))
+                }
+                
+                objc_registerClassPair(viewSubclass)
+                object_setClass(view, viewSubclass)
+            }
         }
     }
 

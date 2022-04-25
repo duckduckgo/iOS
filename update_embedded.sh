0f0e3830e7525d13b65489f40ab0fd6c0e4a75f5
Bartek Waresiak <bartek@duckduckgo.com>
2022-04-25 15:05:09 +0200

    Tweak script

--- update_embedded.sh
+++ update_embedded.sh
@@ -7,35 +7,45 @@ rm -f "$TEMP_FILENAME"
 rm -f "$TEMP_ETAG_FILENAME"
 
 # parameters: url, provider file path, data file path
-function performUpdate() {
+performUpdate() {
   FILE_URL=$1
   PROVIDER_PATH=$2
   DATA_PATH=$3
-  echo "Processing: ${FILE_URL}"
+  printf "Processing: %s\n" "${FILE_URL}"
 
-  OLD_ETAG=$(cat ${PROVIDER_PATH} | grep 'public static let embeddedDataETag' | awk -F '\\\\"' '{print $2}')
-  OLD_SHA=$(cat ${PROVIDER_PATH} | grep 'public static let embeddedDataSHA' | awk -F '"' '{print $2}')
+  if test ! -f "$DATA_PATH"; then
+    printf "Error: Missing %s\n" "${DATA_PATH}"
+    exit 1
+  fi
 
-  echo "Existing ETag: ${OLD_ETAG}"
-  echo "Existing SHA256: ${OLD_SHA}"
+  if test ! -f "$PROVIDER_PATH"; then
+    printf "Error: Missing %s\n" "${PROVIDER_PATH}"
+    exit 1
+  fi
 
-  curl -o $TEMP_FILENAME -H "If-None-Match: \"${OLD_ETAG}\"" --etag-save $TEMP_ETAG_FILENAME "${FILE_URL}"
+  OLD_ETAG=$(grep 'public static let embeddedDataETag' "${PROVIDER_PATH}" | awk -F '\\\\"' '{print $2}')
+  OLD_SHA=$(grep 'public static let embeddedDataSHA' "${PROVIDER_PATH}" | awk -F '"' '{print $2}')
+
+  printf "Existing ETag: %s\n" "${OLD_ETAG}"
+  printf "Existing SHA256: %s\n" "${OLD_SHA}"
+
+  curl -o "$TEMP_FILENAME" -H "If-None-Match: \"${OLD_ETAG}\"" --etag-save "$TEMP_ETAG_FILENAME" "${FILE_URL}"
 
   if test -f $TEMP_FILENAME; then
-    NEW_ETAG=$(cat $TEMP_ETAG_FILENAME | awk -F '"' '{print $2}')
-    NEW_SHA=$(shasum -a 256 $TEMP_FILENAME | awk -F ' ' '{print $1}')
+    NEW_ETAG=$(< "$TEMP_ETAG_FILENAME" awk -F '"' '{print $2}')
+    NEW_SHA=$(shasum -a 256 "$TEMP_FILENAME" | awk -F ' ' '{print $1}')
 
-    echo "New ETag: $NEW_ETAG"
-    echo "New SHA256: $NEW_SHA"
+    printf "New ETag: %s\n" "$NEW_ETAG"
+    printf "New SHA256: %s\n" "$NEW_SHA"
 
-    sed -i '' "s/$OLD_ETAG/$NEW_ETAG/g" ${PROVIDER_PATH}
-    sed -i '' "s/$OLD_SHA/$NEW_SHA/g" ${PROVIDER_PATH}
+    sed -i '' "s/$OLD_ETAG/$NEW_ETAG/g" "${PROVIDER_PATH}"
+    sed -i '' "s/$OLD_SHA/$NEW_SHA/g" "${PROVIDER_PATH}"
 
-    cp $TEMP_FILENAME $DATA_PATH
+    cp "$TEMP_FILENAME" "$DATA_PATH"
 
-    echo "Files updated\n"
+    printf 'Files updated\n\n'
   else
-    echo "Nothing to update\n"
+    printf 'Nothing to update\n\n'
   fi
 
   rm -f "$TEMP_FILENAME"

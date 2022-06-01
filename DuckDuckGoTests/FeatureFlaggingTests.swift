4edca4748bfc1f3bb1a854371f15334cd9d75116
Elle Sullivan <esullivan@duckduckgo.com>
2022-06-01 17:43:10 +0100

    Revise FeatureFlaggingTests to not use MockHTTPURLResponse

--- DuckDuckGoTests/FeatureFlaggingTests.swift
+++ DuckDuckGoTests/FeatureFlaggingTests.swift
@@ -27,49 +27,50 @@ class DefaultFeatureFlaggerTests: XCTestCase {
     
     func testShouldMarkUserAsInternalWhenURLAndStatusCodeCorrectThenReturnsTrue() {
         let featureFlagger = DefaultFeatureFlagger()
-        let response = MockHTTPURLResponse(statusCode: correctStatusCode)
+        let response = HTTPURLResponse(url: correctURL, statusCode: correctStatusCode, httpVersion: nil, headerFields: nil)
+        //let response = MockHTTPURLResponse(statusCode: correctStatusCode)
         let result = featureFlagger.shouldMarkUserAsInternal(forUrl: correctURL, response: response)
         XCTAssertTrue(result)
     }
 
     func testShouldMarkUserAsInternalWhenURLIsCorrectAndStatusCodeisIncorrectThenReturnsFalse() {
         let featureFlagger = DefaultFeatureFlagger()
-        let response = MockHTTPURLResponse(statusCode: 300)
+        let response = HTTPURLResponse(url: correctURL, statusCode: 300, httpVersion: nil, headerFields: nil)
         let result = featureFlagger.shouldMarkUserAsInternal(forUrl: correctURL, response: response)
         XCTAssertFalse(result)
     }
-    
+
     func testShouldMarkUserAsInternalWhenURLIsIncorrectButSubdomainIsCorrectAndStatusCodeIsCorrectThenReturnsFalse() {
         let featureFlagger = DefaultFeatureFlagger()
         let url = URL(string: "login.fishtown.com")!
-        let response = MockHTTPURLResponse(statusCode: 200)
+        let response = HTTPURLResponse(url: url, statusCode: correctStatusCode, httpVersion: nil, headerFields: nil)
         let result = featureFlagger.shouldMarkUserAsInternal(forUrl: url, response: response)
         XCTAssertFalse(result)
     }
-    
+
     func testShouldMarkUserAsInternalWhenURLIsIncorrectButdomainIsCorrectAndStatusCodeIsCorrectThenReturnsFalse() {
         let featureFlagger = DefaultFeatureFlagger()
         let url = URL(string: "sso.duckduckgo.com")!
-        let response = MockHTTPURLResponse(statusCode: 200)
+        let response = HTTPURLResponse(url: url, statusCode: correctStatusCode, httpVersion: nil, headerFields: nil)
         let result = featureFlagger.shouldMarkUserAsInternal(forUrl: url, response: response)
         XCTAssertFalse(result)
     }
 }
 
-class MockHTTPURLResponse: HTTPURLResponse {
-    
-    let mockStatusCode: Int
-    
-    override var statusCode: Int {
-        return mockStatusCode
-    }
-    
-    init(statusCode: Int) {
-        self.mockStatusCode = statusCode
-        super.init()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-}
+//class MockHTTPURLResponse: HTTPURLResponse {
+//
+//    let mockStatusCode: Int
+//
+//    override var statusCode: Int {
+//        return mockStatusCode
+//    }
+//
+//    init?(statusCode: Int) {
+//        self.mockStatusCode = statusCode
+//        super.init(url: <#T##URL#>, statusCode: <#T##Int#>, httpVersion: <#T##String?#>, headerFields: <#T##[String : String]?#>)
+//    }
+//
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}

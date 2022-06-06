bf946379de29382d188d2979f274e679963dd742
Elle Sullivan <THISISDINOSAUR@users.noreply.github.com>
2022-06-06 14:27:26 +0100

    internal feature flagging (#1173)
    Task/Issue URL:
    Tech Design URL: https://app.asana.com/0/481882893211075/1202291296379036/f
    CC:
    
    Description:
    Adds very basic feature flagging for internal users. A user is permanently marked as internal if they successfully authenticate through login.duckduckgo.com. isInternalUser also always returns true for debug builds
    
    Also makes the debug menu only appear if the user is internal
    
    * Add basic internal feature flagging
    
    * Refactor feature flagging and add tests
    
    * Make debug menu dependant on feature flagging
    
    * Remove unneeded return value in markUserAsInternalIfNeeded
    
    * Revise FeatureFlaggingTests to not use MockHTTPURLResponse
    
    * Remove unused mock
    
    * Possibly fix linting error
    
    * Remove response requirement from shouldMarkUserAsInternal
    
    * Fix unit tests

---
+++ Core/FeatureFlagger.swift
@@ -0,0 +1,81 @@
+//
+//  FeatureFlagger.swift
+//  DuckDuckGo
+//
+//  Copyright © 2022 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+public enum Feature: String {
+    case debugMenu
+}
+
+public protocol FeatureFlagger {
+    func isFeatureOn(_ feature: Feature) -> Bool
+}
+
+public protocol FeatureFlaggerInternalUserDecider {
+    
+    var isInternalUser: Bool { get }
+    
+    func markUserAsInternalIfNeeded(forUrl url: URL?, response: HTTPURLResponse?)
+}
+
+public class DefaultFeatureFlagger: FeatureFlagger {
+    
+    public init() { }
+    
+    public func isFeatureOn(_ feature: Feature) -> Bool {
+        switch feature {
+        case .debugMenu:
+            return isInternalUser
+        }
+    }
+    
+    @UserDefaultsWrapper(key: .featureFlaggingDidVerifyInternalUser, defaultValue: false)
+    private var didVerifyInternalUser: Bool
+}
+
+extension DefaultFeatureFlagger: FeatureFlaggerInternalUserDecider {
+    
+    public var isInternalUser: Bool {
+        if isDebugBuild {
+            return true
+        }
+        return didVerifyInternalUser
+    }
+    
+    private static let internalUserVerificationURLHost = "login.duckduckgo.com"
+    
+    public func markUserAsInternalIfNeeded(forUrl url: URL?, response: HTTPURLResponse?) {
+        if isInternalUser { // If we're already an internal user, we don't need to do anything
+            return
+        }
+        
+        didVerifyInternalUser = shouldMarkUserAsInternal(forUrl: url, statusCode: response?.statusCode)
+    }
+    
+    func shouldMarkUserAsInternal(forUrl url: URL?, statusCode: Int?) -> Bool {
+        if let statusCode = statusCode,
+           statusCode == 200,
+           let url = url,
+           url.host == DefaultFeatureFlagger.internalUserVerificationURLHost {
+            
+            return true
+        }
+        return false
+    }
+}

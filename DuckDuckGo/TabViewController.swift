3841d4c84f066aa2e2f9afcb4af20a4f673c0b88
Fernando Bunn <bunn@idevzilla.com>
2022-06-17 21:37:30 +0100

    Open settings and push autofill details
    https://app.asana.com/0/0/1202452016531858/f

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -1596,10 +1596,13 @@ extension TabViewController: WKNavigationDelegate {
     
     @available(iOS 14.0, *)
     private func showLoginDetails(with account: SecureVaultModels.WebsiteAccount) {
-        let autofill = AutofillLoginDetailsViewController(account: account, authenticator: AutofillLoginListAuthenticator())
-        let navigationController = UINavigationController(rootViewController: autofill)
-        autofill.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(dismissLoginDetails))
-        self.present(navigationController, animated: true)
+        if let navController = SettingsViewController.loadFromStoryboard() as? UINavigationController,
+           let settingsController = navController.topViewController as? SettingsViewController {
+            settingsController.loadViewIfNeeded()
+            
+            settingsController.showAutofillAccountDetails(account, animated: false)
+            self.present(navController, animated: true)
+        }
     }
     
     @objc private func dismissLoginDetails() {

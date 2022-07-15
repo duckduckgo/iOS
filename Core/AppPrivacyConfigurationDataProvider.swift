9c5936ab696f81fa1115516f86e9daf8b1efa8c7
amddg44 <amallon@duckduckgo.com>
2022-07-15 15:44:06 +0200

    Update embedded files

--- Core/AppPrivacyConfigurationDataProvider.swift
+++ Core/AppPrivacyConfigurationDataProvider.swift
@@ -22,8 +22,8 @@ import BrowserServicesKit
 final class AppPrivacyConfigurationDataProvider: EmbeddedDataProvider {
 
     public struct Constants {
-        public static let embeddedDataETag = "\"38152e617015df7f9fd90b529d898d16\""
-        public static let embeddedDataSHA = "e388843f4b4f3076afa4577b2dfce5b2bab2a48f2d9eb8101f145e4a7896d989"
+        public static let embeddedDataETag = "\"4a4eefe8bfa9c412a31f62af47361da3\""
+        public static let embeddedDataSHA = "6ff47918c96b858caaaa963c6ece092c5c870f0ce148afa5dd2d995166eceff5"
     }
 
     var embeddedDataEtag: String {

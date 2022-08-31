120101c7ca00094dd1e73adc628e0068fe73108f
Lorenzo Mattei <lmattei@duckduckgo.com>
2022-08-31 19:10:05 +0200

    Add Danger

---
+++ danger.ts
@@ -0,0 +1,8 @@
+import {warn, danger} from "danger";
+
+export default async () => {  
+    // Warn when there is a big PR
+    if (danger.github.pr.additions + danger.github.pr.deletions > 5) {
+        warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
+    }
+};

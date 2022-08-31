20062725683f9c25b03eb34066ba4a65132812e4
Lorenzo Mattei <lmattei@duckduckgo.com>
2022-08-31 23:38:47 +0200

    Fix typo

--- dangerfile.ts
+++ dangerfile.ts
@@ -3,6 +3,6 @@ import {warn, danger} from "danger";
 export default async () => {  
     // Warn when there is a big PR
     if (danger.github.pr.additions + danger.github.pr.deletions > 5) {
-        warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
+        warn("PR has more than 5 lines of code changing. Consider splitting into smaller PRs if possible.");
     }
 };

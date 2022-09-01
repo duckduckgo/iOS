ea4dbad8fb368eca204b1d7c5e03943e406a2253
Lorenzo Mattei <lmattei@duckduckgo.com>
2022-09-01 23:07:41 +0200

    Add rule to remind to add the internal task

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,4 +5,11 @@ export default async () => {
     if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
         warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
     }
+
+    // Warn when link to internal task is missing
+    for (let bodyLine of danger.github.pr.body.toLowerCase().split(/\n/)) {
+        if (bodyLine.includes("task/issue url:") && (!bodyLine.includes("app.asana.com"))) {
+            warn("Please, don't forget to add a link to the internal task");
+        }
+    }
 };

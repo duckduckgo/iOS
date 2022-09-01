88e8779588d42cb2fa26061b1435ea7ebad3ea62
Lorenzo Mattei <lmattei@duckduckgo.com>
2022-09-01 22:50:11 +0200

    Update PR size rule

--- dangerfile.ts
+++ dangerfile.ts
@@ -2,7 +2,7 @@ import {warn, danger} from "danger";
 
 export default async () => {  
     // Warn when there is a big PR
-    if (danger.github.pr.additions + danger.github.pr.deletions > 5) {
-        warn("PR has more than 5 lines of code changing. Consider splitting into smaller PRs if possible.");
+    if (danger.github.pr.additions + danger.github.pr.deletions > 500) {
+        warn("PR has more than 500 lines of code changing. Consider splitting into smaller PRs if possible.");
     }
 };

80571b6ecfeb30d30ef02413c60f02c79c27d7c8
Elle Sullivan <esullivan@duckduckgo.com>
2022-09-05 16:01:24 +0100

    Add app version to secure vault error pixels

--- DuckDuckGo/SecureVaultErrorReporter.swift
+++ DuckDuckGo/SecureVaultErrorReporter.swift
@@ -30,7 +30,9 @@ final class SecureVaultErrorReporter: SecureVaultErrorReporting {
         guard !ProcessInfo().arguments.contains("testing") else { return }
 #endif
         let isBackgrounded = UIApplication.shared.applicationState == .background
-        let pixelParams = [PixelParameters.isBackgrounded: isBackgrounded ? "true" : "false"]
+        // including the appVersion for debugging purposes, it should be removed before the feature is public
+        let pixelParams = [PixelParameters.isBackgrounded: isBackgrounded ? "true" : "false",
+                           PixelParameters.appVersion: AppVersion.shared.versionAndBuildNumber]
         switch error {
         case .initFailed(let error):
             Pixel.fire(pixel: .secureVaultInitFailedError, error: error, withAdditionalParameters: pixelParams)

44d0be8ba14fc4a1430e85952156e0e4e8f8cc98
amddg44 <amallon@duckduckgo.com>
2022-09-21 17:41:42 +0200

    Autofill text update

--- DuckDuckGo/UserText.swift
+++ DuckDuckGo/UserText.swift
@@ -645,7 +645,7 @@ public struct UserText {
     public static let autofillLoginDetailsDefaultTitle = "Login" // Title for autofill login details
     public static let autofillLoginDetailsEditTitle = "Edit Login" // Title when editing autofill login details
     public static let autofillLoginDetailsNewTitle = "Add Login" // Title when adding new autofill login
-    public static let autofillLoginDetailsDeleteButton = "Delete" // Delete button when deleting an autofill login
+    public static let autofillLoginDetailsDeleteButton = "Delete Login" // Delete button when deleting an autofill login
     public static let autofillLoginDetailsDeleteConfirmationTitle = "Are you sure you want to delete this Login?" // Title of confirmation alert when deleting an autofill login
     public static let autofillLoginDetailsDeleteConfirmationButtonTitle = "Delete Login" // Autofill alert button confirming delete autofill login
 

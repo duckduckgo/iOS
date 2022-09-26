5b286490cea472e426613c5b72a76e65d122d73a
Michal Smaga <miasma13@gmail.com>
2022-09-26 16:32:41 +0200

    Fix typo

--- DuckDuckGo/SourcesForSwiftPackage/Model/ServerTrust.swift
+++ DuckDuckGo/SourcesForSwiftPackage/Model/ServerTrust.swift
@@ -1,7 +1,7 @@
 //
 //  ServerTrust.swift
 //
-//  Copyright © 2021 DuckDuckGo. All rights reserved.
+//  Copyright © 2022 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.

65469c70be70ba7e9071fd7388cc52fe836e920a
Jacek Łyp <jlyp@duckduckgo.com>
2023-01-16 13:51:50 +0100

    Fix lint issues

--- prepare_release.sh
+++ prepare_release.sh
@@ -1,30 +1,36 @@
 #!/bin/sh
 
+set -e
+
 if [ -z "$1" ]; then
     echo Usage:\ \ \ ./prepare_release.sh \<VERSION\>
     echo Example: ./prepare_release.sh 7.77.1
-    echo Current version: `cat Configuration/Version.xcconfig | cut -d' ' -f3`
+    echo Current version: "$(cut -d' ' -f3 < Configuration/Version.xcconfig)"
     exit 1
 fi
 
 git stash
 
-git show-branch release/$1 &>/dev/null && echo "Error: Branch release/$1 already exists." && exit 1
-git show-branch release/$1-changes &>/dev/null && echo "Error: Branch release/$1-changes already exists." && exit 1
+git show-branch release/"$1" >/dev/null 2>&1 && echo "Error: Branch release/$1 already exists." && exit 1
+git show-branch release/"$1"-changes >/dev/null 2>&1 && echo "Error: Branch release/$1-changes already exists." && exit 1
 
 # Git flow start release
 
 git checkout develop
 git pull
-git checkout -b release/$1
-git checkout -b release/$1-changes
+git checkout -b release/"$1"
+git checkout -b release/"$1"-changes
 
-# Commit updated version number
+# Update version number
 
-./set_version.sh $1
+./set_version.sh "$1"
 git add Configuration/Version.xcconfig
 git add DuckDuckGo/Settings.bundle/Root.plist
-git commit -m "Update version number"
+
+fastlane increment_build_number_for_version version:"$1"
+git add DuckDuckGo.xcodeproj/project.pbxproj
+
+git commit -m "Update version and build numbers"
 
 # Commit updated embedded files
 
@@ -37,9 +43,9 @@ git commit -m "Update embedded files"
 
 # Create a PR against release branch # make sure GH is set up
 
-git push origin release/$1
-git push origin release/$1-changes
-gh pr create --title "Release $1 [TEST]" --base release/$1 --body "" --assignee @me
+git push origin release/"$1"
+git push origin release/"$1"-changes
+gh pr create --title "Release $1 [TEST]" --base release/"$1" --body "" --assignee @me
+gh pr view --web
 
-#build number
 #tag and push tag on merge

fc2d8f0344ef90b13ee204349ef7a60096545b7f
Diego Rey Mendez <diego@reymendez.com>
2023-01-16 13:16:20 +0100

    Adds the build number to the set_version.sh script

--- set_version.sh
+++ set_version.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 if [ -z "$1" ]; then
-   echo Usage: ./set_version.sh NUMBER
-   echo Example: ./set_version.sh 7.100.1
-   echo Current version: `cat Configuration/Version.xcconfig | cut -d' ' -f3`
+   echo Usage: "./set_version.sh <version> <buildnumber>"
+   echo Example: ./set_version.sh 7.100.1 0
+   echo Current version: `sed -n 1p Configuration/Version.xcconfig | cut -d' ' -f3`
+   echo Current build: `sed -n 2p Configuration/Version.xcconfig | cut -d' ' -f3`
    exit 1
 fi
 
-echo "MARKETING_VERSION = $1\n" > Configuration/Version.xcconfig
+echo "MARKETING_VERSION = $1" > Configuration/Version.xcconfig
+echo "CURRENT_PROJECT_VERSION = $2\n" >> Configuration/Version.xcconfig
 /usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:0:DefaultValue $1" DuckDuckGo/Settings.bundle/Root.plist

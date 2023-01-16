ce4648f553d8df4c267b1fdec7d0fe4ab36b9bfb
Jacek Łyp <jlyp@duckduckgo.com>
2023-01-16 17:33:10 +0100

    Add evals to support verbose/non-verbose execution

--- prepare_release.sh
+++ prepare_release.sh
@@ -3,8 +3,7 @@
 set -e
 
 mute=">/dev/null 2>&1"
-
-if [ "$1" = "-v" ]; then
+if [ "$1" == "-v" ]; then
 	mute=
 fi
 
@@ -34,18 +33,18 @@ echo "✅"
 # Update version and build numbers
 
 printf '%s' "Setting app version ... "
-eval ./set_version.sh "$1" "$mute"
+./set_version.sh "$1"
 eval git add Configuration/Version.xcconfig "$mute"
 eval git add DuckDuckGo/Settings.bundle/Root.plist "$mute"
-eval git commit -m "Update version number" "$mute"
+eval git commit -m \""Update version number\"" "$mute"
 echo "✅"
 
-printf '%s' "Setting build version ... "
+echo "Setting build version ..."
 USERNAME=$(git config user.email 2>&1)
-eval fastlane increment_build_number_for_version version:"$1" username:"$USERNAME" "$mute"
+fastlane increment_build_number_for_version version:"$1" username:"$USERNAME"
 eval git add DuckDuckGo.xcodeproj/project.pbxproj "$mute"
-eval git commit -m "Update build number" "$mute"
-echo "✅"
+eval git commit -m \""Update build number\"" "$mute"
+echo "✅ Build version has been set"
 
 # Commit updated embedded files
 
@@ -55,7 +54,7 @@ eval git add Core/AppTrackerDataSetProvider.swift "$mute"
 eval git add Core/trackerData.json "$mute"
 eval git add Core/AppPrivacyConfigurationDataProvider.swift "$mute"
 eval git add Core/ios-config.json "$mute"
-eval git commit -m "Update embedded files" "$mute" || echo "\n✅ No changes to embedded files"
+eval git commit -m \""Update embedded files\"" "$mute" || echo "\n✅ No changes to embedded files"
 echo "✅"
 
 # Create a PR against release branch
@@ -63,8 +62,8 @@ echo "✅"
 printf '%s' "Creating PR ... "
 eval git push origin "release/$1" "$mute"
 eval git push origin "release/$1-changes" "$mute"
-eval gh pr create --title "Release $1 [TEST]" --base "release/$1" --body "" --assignee @me "$mute"
-eval gh pr comment --body "Make sure to update release notes, metadata and commit the changes." "$mute"
-eval gh pr comment --body "Once you validate the diff, go ahead and merge this PR." "$mute"
+eval gh pr create --title \""Release $1 [TEST]\"" --base "release/$1" --body "" --assignee @me "$mute"
+eval gh pr comment --body \""Make sure to update release notes, metadata and commit the changes.\"" "$mute"
+eval gh pr comment --body \""Once you validate the diff, go ahead and merge this PR.\"" "$mute"
 eval gh pr view --web "$mute"
 echo "✅"

c1ca2a42d68c3a46418baf1e56e672b85b0e6ca8
Diego Rey Mendez <dreymendez@duckduckgo.com>
2023-03-02 17:19:01 +0100

    Revert "Fixes a crasher due to UI code running in the main thread"
    This reverts commit e645c4472cc30291d9480a45b0801b0533034127.

--- DuckDuckGo/TabViewControllerBrowsingMenuExtension.swift
+++ DuckDuckGo/TabViewControllerBrowsingMenuExtension.swift
@@ -298,15 +298,13 @@ extension TabViewController {
             Pixel.fire(pixel: .emailUserCreatedAlias, withAdditionalParameters: pixelParameters, includedParameters: [])
 
             emailManager.getAliasIfNeededAndConsume { alias, _ in
-                Task { @MainActor in
-                    guard let alias = alias else {
-                        // we may want to communicate this failure to the user in the future
-                        return
-                    }
-                    let pasteBoard = UIPasteboard.general
-                    pasteBoard.string = emailManager.emailAddressFor(alias)
-                    ActionMessageView.present(message: UserText.emailBrowsingMenuAlert)
+                guard let alias = alias else {
+                    // we may want to communicate this failure to the user in the future
+                    return
                 }
+                let pasteBoard = UIPasteboard.general
+                pasteBoard.string = emailManager.emailAddressFor(alias)
+                ActionMessageView.present(message: UserText.emailBrowsingMenuAlert)
             }
         }
     }

6c2979a46f441a279e60b2026b9894d268e8c7c7
Michal Smaga <miasma13@gmail.com>
2023-03-17 13:24:49 +0100

    Add tests

--- Core/DailyPixel.swift
+++ Core/DailyPixel.swift
@@ -19,30 +19,44 @@
 
 import Foundation
 
+public enum DailyPixelError: Error {
+    case storageError
+    case alreadyFired
+}
+
 /// A variant of pixel that is fired at most once per day.
 ///
-/// The 'fire' method first checks if the pixel was already fired on a given day:
+/// When fired it checks if the pixel was already fired on a given day:
 /// - if not - a request is made as with standard Pixel
 /// - if it was - nothing happens
+///
+/// The 'fire' method mimics standard Pixel API.
+/// The 'onComplete' closure is always called - even when no pixel is fired.
+/// In those scenarios a 'DailyPixelError' is returned denoting the reason.
+/// 
 public class DailyPixel {
     
-    struct Constants {
+    private struct Constants {
         static let dailyPixelStorageIdentifier = "com.duckduckgo.daily.pixel.storage"
     }
-    
+
     private static var storage: UserDefaults? = UserDefaults(suiteName: Constants.dailyPixelStorageIdentifier)
     
     public static func fire(pixel: Pixel.Event,
-                            withAdditionalParameters params: [String: String] = [:]) {
+                            withAdditionalParameters params: [String: String] = [:],
+                            onComplete: @escaping (Error?) -> Void = { _ in }) {
         
         guard let storage = storage else {
             Pixel.fire(pixel: .debugDailyPixelStorageError)
+            onComplete(DailyPixelError.storageError)
             return
         }
         
         if !hasPixelBeenFiredToday(pixel: pixel, dailyPixelStorage: storage) {
-            Pixel.fire(pixel: pixel, withAdditionalParameters: params)
+            Pixel.fire(pixel: pixel, withAdditionalParameters: params, onComplete: onComplete)
             updatePixelLastFireDate(pixel: pixel, dailyPixelStorage: storage)
+        } else {
+            onComplete(DailyPixelError.alreadyFired)
         }
     }
     

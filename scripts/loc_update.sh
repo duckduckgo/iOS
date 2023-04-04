e0f157176841a6b13a7f66f12ea3a98f3735204d
Jacek Łyp <jlyp@duckduckgo.com>
2023-04-05 01:26:48 +0200

    Update loc_update

--- scripts/loc_update.sh
+++ scripts/loc_update.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-# Add target sub-directories here when needed
-declare -a targets=("DuckDuckGo" "Widgets")
+# Get the directory where the script is stored
+script_dir=$(dirname "$(readlink -f "$0")")
+base_dir="${script_dir}/.."
 
-for dir in "${targets[@]}"; do
-  echo Processing $dir
-  find $dir/ -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o $dir/en.lproj
-  iconv -f UTF-16 -t UTF8 $dir/en.lproj/Localizable.strings > $dir/en.lproj/Localizable-UTF8.strings
-  mv $dir/en.lproj/Localizable-UTF8.strings $dir/en.lproj/Localizable.strings
+# Add target sub-directories here when needed
+set -- "${base_dir}/DuckDuckGo" "${base_dir}/Widgets"
+
+for dir in "$@"; do
+  echo "Processing ${dir}"
+  find "${dir}/" -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${dir}/en.lproj"
+  iconv -f UTF-16 -t UTF8 "${dir}/en.lproj/Localizable.strings" > "${dir}/en.lproj/Localizable-UTF8.strings"
+  mv "${dir}/en.lproj/Localizable-UTF8.strings" "${dir}/en.lproj/Localizable.strings"
 done

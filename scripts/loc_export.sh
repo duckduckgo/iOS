264d1be251f37434c8e4e2256c4df2a70c6b38b0
Jacek Łyp <jlyp@duckduckgo.com>
2023-04-05 01:24:56 +0200

    Update loc_export

--- scripts/loc_export.sh
+++ scripts/loc_export.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+# Get the directory where the script is stored
+script_dir=$(dirname "$(readlink -f "$0")")
+base_dir="${script_dir}/.."
+
 echo "Updating..."
-./loc_update.sh
+"${script_dir}/loc_update.sh"
 
 echo "Exporting..."
-rm -r loc
-xcodebuild -exportLocalizations -project DuckDuckGo.xcodeproj -localizationPath loc -exportLanguage en
-open "loc/en.xcloc/Localized Contents"
+loc_path="${script_dir}/assets/loc"
+rm -r "$loc_path"
+xcodebuild -exportLocalizations -project "${base_dir}/DuckDuckGo.xcodeproj" -localizationPath "$loc_path" -exportLanguage en
+open "${loc_path}/en.xcloc/Localized Contents"

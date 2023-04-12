01032fd365b6de52c06bac49bcbce2b3948cd88b
Jacek Łyp <jlyp@duckduckgo.com>
2023-04-12 14:10:37 +0200

    Fix script warnings

--- scripts/loc_update.sh
+++ scripts/loc_update.sh
@@ -8,8 +8,8 @@ base_dir="${script_dir}/.."
 set -- "${base_dir}/DuckDuckGo" "${base_dir}/Widgets"
 
 for dir in "$@"; do
-  echo "Processing ${dir}"
-  find "${dir}/" -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${dir}/en.lproj"
-  iconv -f UTF-16 -t UTF8 "${dir}/en.lproj/Localizable.strings" > "${dir}/en.lproj/Localizable-UTF8.strings"
-  mv "${dir}/en.lproj/Localizable-UTF8.strings" "${dir}/en.lproj/Localizable.strings"
+	echo "Processing ${dir}"
+	find "${dir}/" -name "*.swift" -print0 | xargs -0 xcrun extractLocStrings -o "${dir}/en.lproj"
+	iconv -f UTF-16 -t UTF8 "${dir}/en.lproj/Localizable.strings" > "${dir}/en.lproj/Localizable-UTF8.strings"
+	mv "${dir}/en.lproj/Localizable-UTF8.strings" "${dir}/en.lproj/Localizable.strings"
 done
--- scripts/set_version.sh
+++ scripts/set_version.sh
@@ -5,10 +5,10 @@ script_dir=$(dirname "$(readlink -f "$0")")
 base_dir="${script_dir}/.."
 
 if [ -z "$1" ]; then
-   echo 'Usage: ./set_version.sh <version>'
-   echo 'Example: ./set_version.sh 7.100.1'
-   echo Current version: "$(cut -d' ' -f3 < "${base_dir}"/Configuration/Version.xcconfig)"
-   exit 1
+	echo 'Usage: ./set_version.sh <version>'
+	echo 'Example: ./set_version.sh 7.100.1'
+	echo Current version: "$(cut -d' ' -f3 < "${base_dir}"/Configuration/Version.xcconfig)"
+	exit 1
 fi
 
 printf "MARKETING_VERSION = %s\n" "$1" > "${base_dir}/Configuration/Version.xcconfig"

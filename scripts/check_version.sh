a4431613953f0c560ed2bffd634c603c40006141
Jacek Łyp <jlyp@duckduckgo.com>
2023-04-13 16:11:52 +0200

    Setup ShellCheck (#1636)

--- scripts/check_version.sh
+++ scripts/check_version.sh
@@ -4,8 +4,7 @@
 script_dir=$(dirname "$(readlink -f "$0")")
 base_dir="${script_dir}/.."
 
-if grep -q MARKETING_VERSION "${base_dir}/DuckDuckGo.xcodeproj/project.pbxproj"
-then
-    echo "Error: 'MARKETING_VERSION' is present in project file."
-    exit 1
+if grep -q MARKETING_VERSION "${base_dir}/DuckDuckGo.xcodeproj/project.pbxproj"; then
+	echo "Error: 'MARKETING_VERSION' is present in project file."
+	exit 1
 fi

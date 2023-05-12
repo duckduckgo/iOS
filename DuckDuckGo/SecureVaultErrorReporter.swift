44ec1eb761aa15bf43cedc9bfa18721bd34b6d84
amddg44 <amallon@duckduckgo.com>
2023-05-12 16:53:53 +0200

    Application background and lock status checks for SecureVault access (#1716)
    Task/Issue URL: https://app.asana.com/0/30173902528854/1204557908133145/f
    Tech Design URL:
    CC:
    
    Description:
    Added feature flag + autofill status check around whether secureVault should accessed + silenced pixel that was firing when app is in background

--- DuckDuckGo/SecureVaultErrorReporter.swift
+++ DuckDuckGo/SecureVaultErrorReporter.swift
@@ -36,6 +36,18 @@ final class SecureVaultErrorReporter: SecureVaultErrorReporting {
                            PixelParameters.appVersion: AppVersion.shared.versionAndBuildNumber]
         switch error {
         case .initFailed(let error):
+            // Silencing pixel reporting for error -25308 (attempt to access keychain while the device was locked)
+            // as per https://app.asana.com/0/30173902528854/1204557908133145/f, at least temporarily
+            if isBackgrounded,
+               let secureVaultError = error as? SecureVaultError,
+               let userInfo = secureVaultError.errorUserInfo["NSUnderlyingError"] as? NSError,
+               userInfo.code == -25308 {
+                os_log("SecureVault attempt to access keystore while device is locked: %@",
+                       log: .generalLog,
+                       type: .debug,
+                       error.localizedDescription)
+                return
+            }
             Pixel.fire(pixel: .secureVaultInitFailedError, error: error, withAdditionalParameters: pixelParams)
         case .failedToOpenDatabase(let error):
             Pixel.fire(pixel: .secureVaultFailedToOpenDatabaseError, error: error, withAdditionalParameters: pixelParams)

46b1b1beedd4bdcbeb990f38f828d6db05f8fd91
Dominik Kapusta <dkapusta@duckduckgo.com>
2023-05-18 15:58:30 +0200

    Update WIP message in Sync Settings

--- LocalPackages/SyncUI/Sources/SyncUI/Views/SyncSettingsView.swift
+++ LocalPackages/SyncUI/Sources/SyncUI/Views/SyncSettingsView.swift
@@ -174,7 +174,7 @@ public struct SyncSettingsView: View {
                     .foregroundColor(.black)
 
                 // swiftlint:disable line_length
-                Text("This feature is viewable to internal users only and is still being developed and tested. Currently you can create and connect accounts. Management and data syncing will be available soon. **[More Info](https://app.asana.com/0/1201493110486074/1203756800930481/f)**")
+                Text("This feature is viewable to internal users only and is still being developed and tested. Currently you can create accounts, connect and manage devices, and sync bookmarks and favorites. **[More Info](https://app.asana.com/0/1201493110486074/1203756800930481/f)**")
                     .foregroundColor(.black)
                     .font(.system(size: 11, weight: .regular))
                 // swiftlint:enable line_length

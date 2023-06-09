0c074a93221a5c9272f7431c8f20027e41e01dfc
Alistair Brown <github@alistairjcbrown.com>
2023-06-09 15:10:26 +0100

    Update BSK which has macOS support for in-context signup (#1756)
    Task/Issue URL: https://app.asana.com/0/0/1204013645775701/f
    Tech Design URL:
    CC: https://app.asana.com/0/72649045549333/1202940260106129/f
    
    Description:
    Updates BSK and autofill -- feature toggle is hard coded false, there should be no change on iOS

--- DuckDuckGo/AutofillContentScopeFeatureToggles.swift
+++ DuckDuckGo/AutofillContentScopeFeatureToggles.swift
@@ -28,6 +28,7 @@ extension ContentScopeFeatureToggles {
     static var supportedFeaturesOniOS: ContentScopeFeatureToggles {
         let isAutofillEnabledInSettings = AutofillSettingStatus.isAutofillEnabledInSettings
         return ContentScopeFeatureToggles(emailProtection: true,
+                                   emailProtectionIncontextSignup: false,
                                    credentialsAutofill: featureFlagger.isFeatureOn(.autofillCredentialInjecting) && isAutofillEnabledInSettings,
                                    identitiesAutofill: false,
                                    creditCardsAutofill: false,

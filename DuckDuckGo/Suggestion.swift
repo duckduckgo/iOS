f52cbfbd45229836b582fbea4db17bac54a76486
Christopher Brind <brindy@duckduckgo.com>
2023-06-27 11:22:22 +0100

    Show QRCode when state is not inactive, ie enabled. (#1793)

--- DuckDuckGo/Suggestion.swift
+++ DuckDuckGo/Suggestion.swift
@@ -31,11 +31,6 @@ struct Suggestion {
     let suggestion: String
     let url: URL?
     
-    init(source: Source, suggestion: String, url: URL?) {
-        self.source = source
-        self.suggestion = suggestion
-        self.url = url
-    }
 }
 
 struct AutocompleteEntry: Decodable {

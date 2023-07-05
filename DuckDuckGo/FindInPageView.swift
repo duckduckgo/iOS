f6ef45be93d7d5691fb3eefa27c8d9772ba244c2
Christopher Brind <brindy@duckduckgo.com>
2023-07-05 15:05:12 +0100

    use the accent color for text caret

--- DuckDuckGo/FindInPageView.swift
+++ DuckDuckGo/FindInPageView.swift
@@ -130,7 +130,7 @@ extension FindInPageView: Themable {
         counterLabel.textColor = theme.barTintColor
         searchBackground.backgroundColor = theme.searchBarBackgroundColor
         inputText.textColor = theme.searchBarTextColor
-        inputText.tintColor = theme.searchBarTextColor
+        inputText.tintColor = UIColor(designSystemColor: .accent)
         inputText.keyboardAppearance = theme.keyboardAppearance
         searchLoupe.tintColor = theme.barTintColor.withAlphaComponent(0.5)
         doneButton.setTitleColor(theme.barTintColor, for: .normal)

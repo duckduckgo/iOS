2c293eb7d7fd153cbaf4868aed149985cf071303
Christopher Brind <brindy@duckduckgo.com>
2023-07-16 22:17:04 +0100

    update images

--- Widgets/LockScreenWidgets.swift
+++ Widgets/LockScreenWidgets.swift
@@ -113,6 +113,7 @@ struct LockScreenWidgetView: View {
                 .resizable()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Rectangle().foregroundColor(.white.opacity(0.3)))
     }
 
 }

559076e6a150debe057a476047742cedce6ca8e5
Elle Sullivan <esullivan@duckduckgo.com>
2023-07-27 13:10:31 +0100

    Suppress shell check warning

--- scripts/prepare_release.sh
+++ scripts/prepare_release.sh
@@ -108,6 +108,7 @@ read_command_line_arguments() {
 	if [[ $is_hotfix ]]; then
 		version_to_hotfix=${version}
 		if ! [[ $is_coldfix ]]; then
+			# shellcheck disable=SC2206
 			arrIN=(${version//./ })
 			patch_number=$((arrIN[2]+1))
 			version="${arrIN[0]}.${arrIN[1]}.${patch_number}"

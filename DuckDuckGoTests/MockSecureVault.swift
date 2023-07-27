fc63aa30d3eac5e77a3f63425cd79495231a194a
Sam Symons <sam@samsymons.com>
2023-07-26 21:14:46 -0700

    Update resolved BSK version

--- DuckDuckGoTests/MockSecureVault.swift
+++ DuckDuckGoTests/MockSecureVault.swift
@@ -20,6 +20,7 @@
 import Foundation
 import BrowserServicesKit
 import SecureStorage
+import GRDB
 
 typealias MockVaultFactory = SecureVaultFactory<MockSecureVault<MockDatabaseProvider>>
 
@@ -154,9 +155,12 @@ class MockDatabaseProvider: AutofillDatabaseProvider {
     var _forDomain = [String]()
     var _credentialsDict = [Int64: SecureVaultModels.WebsiteCredentials]()
     var _note: SecureVaultModels.Note?
+    var db: GRDB.DatabaseWriter
     // swiftlint:enable identifier_name
 
-    required init(file: URL = URL(string: "https://duckduckgo.com/")!, key: Data = Data()) throws {}
+    required init(file: URL = URL(string: "https://duckduckgo.com/")!, key: Data = Data()) throws {
+        db = (try? DatabaseQueue(named: "Test"))!
+    }
 
     static func recreateDatabase(withKey key: Data) throws -> Self {
         // swiftlint:disable:next force_cast

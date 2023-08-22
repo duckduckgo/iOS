8cacc39d1df9262206457e58e149d8b27dc8a1b6
Daniel Bernal <dbernal@duckduckgo.com>
2023-08-22 15:08:02 +0200

    Pre-commit

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,32 +1,4 @@
 #!/bin/bash
 
-# URL where the installation/uninstallation script is hosted
 SCRIPT_URL="https://raw.githubusercontent.com/duckduckgo/BrowserServicesKit/daniel/swiftlint-hook/scripts/pre-commit.sh"
-LOCAL_SCRIPT="local-install-pre-commit-hook.sh"
-
-# Check the argument for install or uninstall
-if [ "$1" != "--install" ] && [ "$1" != "--uninstall" ]; then
-  echo "Usage: $0 --install | --uninstall"
-  exit 1
-fi
-
-# Download the script
-if command -v curl >/dev/null; then
-  curl -s -o "$LOCAL_SCRIPT" "$SCRIPT_URL"
-elif command -v wget >/dev/null; then
-  wget -O "$LOCAL_SCRIPT" "$SCRIPT_URL"
-else
-  echo "error: Neither curl nor wget are available on your system. Please install one of them to proceed."
-  exit 1
-fi
-
-# Make the script executable
-chmod +x "$LOCAL_SCRIPT"
-
-# Execute the script with the given option
-./"$LOCAL_SCRIPT" "$1"
-
-# Optionally, remove the downloaded script
-rm "$LOCAL_SCRIPT"
-
-echo "Operation completed!"
+curl -s "${SCRIPT_URL}" | bash -s -- "$@"

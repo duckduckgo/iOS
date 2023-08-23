d410c59b5e4c8acf2c901bea42fb85e3bca85eac
Daniel Bernal <dbernal@duckduckgo.com>
2023-08-23 14:57:40 +0200

    Script path

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-SCRIPT_URL="https://raw.githubusercontent.com/duckduckgo/BrowserServicesKit/daniel/swiftlint-hook/scripts/pre-commit.sh"
+SCRIPT_URL="https://raw.githubusercontent.com/duckduckgo/BrowserServicesKit/main/scripts/pre-commit.sh"
 curl -s "${SCRIPT_URL}" | bash -s -- "$@"

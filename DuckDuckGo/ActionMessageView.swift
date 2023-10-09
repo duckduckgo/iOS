6db2f7eff64ac6ce011c54f386471d4887063420
Chris Brind <brindy@duckduckgo.com>
2023-10-09 12:11:53 +0100

    revert toast change

--- DuckDuckGo/ActionMessageView.swift
+++ DuckDuckGo/ActionMessageView.swift
@@ -39,16 +39,11 @@ class ActionMessageView: UIView {
         static var duration: TimeInterval = 3.0
         
         static var windowBottomPaddingWithBottomBar: CGFloat {
-            // Tricky to inject, unfortuantely.  
-            //  Ideally we'd query the postion of the address bar directly.
-            let addressBarPosition = AppDependencyProvider.shared.appSettings.currentAddressBarPosition
-            let bottomAddressBarModifier: CGFloat = addressBarPosition == .bottom ? 52 : 0
-
             if UIDevice.current.userInterfaceIdiom == .phone && !isPortrait {
-                return 40 + bottomAddressBarModifier
+                return 40
             }
             
-            return 70 + bottomAddressBarModifier
+            return 70
         }
         static var windowBottomPaddingWithoutBottomBar: CGFloat {
             return 0

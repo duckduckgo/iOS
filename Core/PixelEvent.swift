c73d7deac9f4e66f32a1482e52c8b7f1c9f1acf1
Christopher Brind <brindy@duckduckgo.com>
2023-12-01 10:24:28 +0000

    new pixels for toolbars and share sheet  (#2208)
    Task/Issue URL: https://app.asana.com/0/414235014887631/1205953072928854/f
    Tech Design URL:
    CC:
    
    Description:
    
    New pixels for iOS toolbars project.
    
    Address Bar
    
    AddressBarSettingsPressed: New pixel m_addressbar_settings
    AddressBarRefreshPressed: This is the m_r pixel and is only called from the address bar so already exists
    AddressBarShieldPressed: This is the mp pixel so already exists
    AddressBarSharePressed: This was one pixel with a parameter, but I'm splitting into two new ones for clarity:
    new pixel: m_addressbar_share from the address bar
    new pixel: m_browsingmenu_share from the menu
    pixel removed: mb_sh has been removed
    Share Sheet
    
    Share result
    New pixel m_sharesheet_result_success when success is true
    New pixel m_sharesheet_result_fail when success is false
    ShareSheetCopy: new pixel m_sharesheet_activity_copy
    ShareSheetAddBookmark: new pixel m_sharesheet_activity_addbookmark
    ShareSheetAddFavorite: new pixel m_sharesheet_activity_addfavorite
    ShareSheetFindinPage: new pixel m_sharesheet_activity_findinpage
    ShareSheetPrint: new pixel m_sharesheet_activity_print
    ShareSheetAddToReadingList: new pixel m_sharesheet_activity_addtoreadinglist
    Also includes adding app version by default to the pixels as part of privacy triage: https://app.asana.com/0/69071770703008/1206042861558143/f

--- Core/PixelEvent.swift
+++ Core/PixelEvent.swift
@@ -74,6 +74,19 @@ extension Pixel {
         case browsingMenuFireproof
         case browsingMenuAutofill
         
+        case addressBarShare
+        case addressBarSettings
+
+        case shareSheetResultSuccess
+        case shareSheetResultFail
+        case shareSheetActivityCopy
+        case shareSheetActivityAddBookmark
+        case shareSheetActivityAddFavorite
+        case shareSheetActivityFindInPage
+        case shareSheetActivityPrint
+        case shareSheetActivityAddToReadingList
+        case shareSheetActivityOther
+
         case tabBarBackPressed
         case tabBarForwardPressed
         case bookmarksButtonPressed
@@ -562,7 +575,6 @@ extension Pixel.Event {
         case .browsingMenuRemoveFromFavorites: return "mb_df"
         case .browsingMenuAddToFavoritesAddFavoriteFlow: return "mb_aff"
         case .browsingMenuToggleBrowsingMode: return "mb_dm"
-        case .browsingMenuShare: return "mb_sh"
         case .browsingMenuCopy: return "mb_cp"
         case .browsingMenuPrint: return "mb_pr"
         case .browsingMenuSettings: return "mb_st"
@@ -572,7 +584,21 @@ extension Pixel.Event {
         case .browsingMenuReportBrokenSite: return "mb_rb"
         case .browsingMenuFireproof: return "mb_f"
         case .browsingMenuAutofill: return "m_nav_autofill_menu_item_pressed"
-            
+
+        case .browsingMenuShare: return "m_browsingmenu_share"
+
+        case .addressBarShare: return "m_addressbar_share"
+        case .addressBarSettings: return "m_addressbar_settings"
+        case .shareSheetResultSuccess: return "m_sharesheet_result_success"
+        case .shareSheetResultFail: return "m_sharesheet_result_fail"
+        case .shareSheetActivityCopy: return "m_sharesheet_activity_copy"
+        case .shareSheetActivityAddBookmark: return "m_sharesheet_activity_addbookmark"
+        case .shareSheetActivityAddFavorite: return "m_sharesheet_activity_addfavorite"
+        case .shareSheetActivityFindInPage: return "m_sharesheet_activity_findinpage"
+        case .shareSheetActivityPrint: return "m_sharesheet_activity_print"
+        case .shareSheetActivityAddToReadingList: return "m_sharesheet_activity_addtoreadinglist"
+        case .shareSheetActivityOther: return "m_sharesheet_activity_other"
+
         case .tabBarBackPressed: return "mt_bk"
         case .tabBarForwardPressed: return "mt_fw"
         case .bookmarksButtonPressed: return "mt_bm"

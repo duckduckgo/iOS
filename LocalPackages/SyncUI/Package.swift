092c8b4b1da5511df366a67729af37e749056af7
Alexey Martemyanov <mallexxx@gmail.com>
2023-12-18 13:29:01 +0600

    set BSK to 94.0.3

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DuckUI"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "91.0.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "94.0.3"),
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "2.0.0")
     ],
     targets: [

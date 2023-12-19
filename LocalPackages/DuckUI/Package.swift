fb3b89860aa9c28198a007e6ad8185ef7af1cc7d
Sam Symons <sam@samsymons.com>
2023-12-19 02:36:23 -0800

    Send WG invalid state reason (#2275)
    Task/Issue URL: https://app.asana.com/0/0/1206201299599597/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR updates the WG invalid state pixel to send the reason.

--- LocalPackages/DuckUI/Package.swift
+++ LocalPackages/DuckUI/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["DuckUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "94.0.3"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "95.0.0"),
     ],
     targets: [
         .target(

7f0fad6fe06466f387d6d437d66d55286b0eba7a
Anh Do <ado@duckduckgo.com>
2023-12-22 18:20:33 -0500

    Update BSK

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -32,7 +32,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DuckUI"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "99.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "99.0.2"),
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "2.0.0")
     ],
     targets: [

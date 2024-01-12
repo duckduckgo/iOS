0eb447cd5525ea0271e481a3b643c73693b0ac0f
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-01-12 19:15:53 +0100

    Use GRDB.swift 2.23.0 (upstream 6.32.0, SQLCipher 4.5.5) (#2305)
    Task/Issue URL: https://app.asana.com/0/414235014887631/1206287402354150/f
    
    Description:
    Use the latest version of GRDB.

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DuckUI"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.0"),
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "2.0.0")
     ],
     targets: [

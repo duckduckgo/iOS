cb35a1a0f2f53f2e9c5cba6fde4ae229007f7346
Daniel Bernal <dbernal@duckduckgo.com>
2024-01-16 17:32:45 +0100

    Update preloader

--- DuckDuckGo/PrivacyPro/Views/PurchaseInProgressView.swift
+++ DuckDuckGo/PrivacyPro/Views/PurchaseInProgressView.swift
@@ -17,6 +17,7 @@
 //  limitations under the License.
 //
 import SwiftUI
+import DesignResourcesKit
 
 struct PurchaseInProgressView: View {
     @Environment(\.colorScheme) var colorScheme
@@ -25,24 +26,24 @@ struct PurchaseInProgressView: View {
         
     var body: some View {
         ZStack {
-            Color(colorScheme == .dark ? .black : .white)
+            Color(designSystemColor: .background)
                 .opacity(0.6)
                 .edgesIgnoringSafeArea(.all)
                 .disabled(true)
                         
             ZStack {
                 RoundedRectangle(cornerRadius: 12)
-                    .fill(colorScheme == .dark ? .black : .white)
+                    .fill(Color(designSystemColor: .background))
                     .frame(width: 220, height: 120)
-                    .shadow(color: colorScheme == .dark ? .black : .gray30, radius: 10)
+                    .shadow(color: colorScheme == .dark ? .black : .gray50, radius: 10)
                 
                 VStack {
                     SwiftUI.ProgressView()
                         .scaleEffect(2)
-                        .progressViewStyle(CircularProgressViewStyle(tint: Color(colorScheme == .dark ? .gray30 : .gray70)))
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color(designSystemColor: DesignSystemColor.icons)))
                         .padding(.bottom, 18)
                         .padding(.top, 10)
-                    Text(status).daxSubheadRegular()
+                    Text(status).daxSubheadRegular().foregroundColor(Color(designSystemColor: .textPrimary))
                         .frame(width: 200, height: 20)
                 }
             }
@@ -54,7 +55,7 @@ struct PurchaseInProgressView: View {
 struct PurchaseInProgressView_Previews: PreviewProvider {
     static var previews: some View {
         PurchaseInProgressView(status: "Completing Purchase...")
-            .preferredColorScheme(.light) // Preview in light mode
+            .preferredColorScheme(.dark)
             .previewLayout(.sizeThatFits)
             .padding()
     }

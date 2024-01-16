7c00dccb3f6de894d1939ae21d52889bb0d3d496
Brad Slayter <SlayterDev@users.noreply.github.com>
2024-01-16 08:45:39 -0600

    Better detection of AMP pages (#2284)

--- LocalPackages/DuckUI/Package.swift
+++ LocalPackages/DuckUI/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["DuckUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.1.2"),
     ],
     targets: [
         .target(

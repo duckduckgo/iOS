e19ea97f7f3ca13bef8b93d86db75364f2b62755
Sabrina Tardio <stardio@duckduckgo.com>
2024-01-25 12:33:46 +0100

    update BSK dependency

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -33,7 +33,7 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../DuckUI"),
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.2.2"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "101.2.3"),
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "2.0.0")
     ],
     targets: [

7c96384ef799b6226f5892122f393e284b9ef765
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-01-26 14:57:44 +0100

    Don't report CancellationError from BookmarksFaviconsFetcher (#2385)
    Task/Issue URL: https://app.asana.com/0/1201493110486074/1206407973669779/f
    
    Description:
    Keep cancelling operations when requested, but without reporting the cancellation
    to errorEvents, which could have caused them being reported as Pixels.

--- LocalPackages/DuckUI/Package.swift
+++ LocalPackages/DuckUI/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["DuckUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "102.0.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "102.0.2"),
     ],
     targets: [
         .target(

143e272fd7136236cd40538f24a924c87e631810
Jonathan Kingston <jkingston@duckduckgo.com>
2024-02-02 23:15:25 +0000

    Force UA to be updated for all loads

--- DuckDuckGo/TabViewController.swift
+++ DuckDuckGo/TabViewController.swift
@@ -562,6 +562,7 @@ class TabViewController: UIViewController {
         loadViewIfNeeded()
         
         if let url = urlRequest.url, !shouldReissueSearch(for: url) {
+            userAgentManager.update(webView: webView, isDesktop: tabModel.isDesktop, url: url)
             requeryLogic.onNewNavigation(url: url)
         }
 

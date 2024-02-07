ae30bdcdfe09a813717e8d3b510adc7bf4b92c69
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2024-02-07 09:31:28 +0100

    Set BSK version to 104.2.1

--- LocalPackages/DuckUI/Package.swift
+++ LocalPackages/DuckUI/Package.swift
@@ -31,7 +31,7 @@ let package = Package(
             targets: ["DuckUI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "104.1.1"),
+        .package(url: "https://github.com/duckduckgo/BrowserServicesKit", exact: "104.2.1"),
     ],
     targets: [
         .target(

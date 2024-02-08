7095d3dc97c7599dc677480154c39f85035ceeb8
Chris Brind <brindy@duckduckgo.com>
2024-02-08 11:34:00 +0000

    Update the documentation for the CookieStorage class

--- Core/CookieStorage.swift
+++ Core/CookieStorage.swift
@@ -20,9 +20,12 @@
 import Common
 import Foundation
 
-/// The Fire Button does not delete the user's DuckDuckGo search settings, which are saved as cookies. Removing these cookies would reset them and have undesired consequences, i.e. changing the theme, default language, etc.
-/// The Fire Button also does not delete temporary cookies associated with 'surveys.duckduckgo.com'. When we launch surveys to help us understand issues that impact users over time, we use this cookie to temporarily store anonymous survey answers, before deleting the cookie. Cookie storage duration is communicated to users before they opt to submit survey answers.
-/// These cookies are not stored in a personally identifiable way. For example, the large size setting is stored as 's=l.' More info in https://duckduckgo.com/privacy
+/// Class for persisting cookies for fire proofed sites to work around a WKWebView / DataStore bug which does not let data get persisted until the webview has loaded.
+///
+/// Privacy information:
+/// * The Fire Button does not delete the user's DuckDuckGo search settings, which are saved as cookies. Removing these cookies would reset them and have undesired consequences, i.e. changing the theme, default language, etc.
+/// * The Fire Button also does not delete temporary cookies associated with 'surveys.duckduckgo.com'. When we launch surveys to help us understand issues that impact users over time, we use this cookie to temporarily store anonymous survey answers, before deleting the cookie. Cookie storage duration is communicated to users before they opt to submit survey answers.
+/// * These cookies are not stored in a personally identifiable way. For example, the large size setting is stored as 's=l.' More info in https://duckduckgo.com/privacy
 public class CookieStorage {
 
     struct Keys {
@@ -80,6 +83,7 @@ public class CookieStorage {
         self.userDefaults = userDefaults
     }
 
+    /// Used when debugging (e.g. on the simulator).
     enum CookieDomainsOnUpdateDiagnostic {
         case empty
         case match
@@ -88,6 +92,7 @@ public class CookieStorage {
         case notConsumed
     }
     
+    /// Update ALL cookies. The absence of cookie domains here indicateds they have been removed by the website, so be sure to call this with all cookies that might need to be persisted even if those websites have not been visited yet.
     @discardableResult
     func updateCookies(_ cookies: [HTTPCookie], keepingPreservedLogins preservedLogins: PreserveLogins) -> CookieDomainsOnUpdateDiagnostic {
         guard isConsumed else { return .notConsumed }

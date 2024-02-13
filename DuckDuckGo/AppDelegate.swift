bd240272cfc8b45abdf9e8ec5e5c1e81346d700a
Federico Cappelli <fcappelli@duckduckgo.com>
2024-02-13 12:38:19 +0000

    skad 4 support

--- DuckDuckGo/AppDelegate.swift
+++ DuckDuckGo/AppDelegate.swift
@@ -90,6 +90,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // swiftlint:disable:next function_body_length cyclomatic_complexity
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        // SKAD4 support
+        update(conversionValue: 1)
+
 #if targetEnvironment(simulator)
         if ProcessInfo.processInfo.environment["UITESTING"] == "true" {
             // Disable hardware keyboards.

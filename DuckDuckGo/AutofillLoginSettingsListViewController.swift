18f697360b5c45b6cee11e43ae6f10dba176c865
amddg44 <amallon@duckduckgo.com>
2024-02-22 17:15:21 +0100

    Removed unnecessary code

--- DuckDuckGo/AutofillLoginSettingsListViewController.swift
+++ DuckDuckGo/AutofillLoginSettingsListViewController.swift
@@ -105,8 +105,6 @@ final class AutofillLoginSettingsListViewController: UIViewController {
         tableView.registerCell(ofType: AutofillListItemTableViewCell.self)
         tableView.registerCell(ofType: EnableAutofillSettingsTableViewCell.self)
         tableView.registerCell(ofType: AutofillNeverSavedTableViewCell.self)
-        // Have to set tableHeaderView height otherwise tableView content will jump when adding / removing searchController due to tableView insetGrouped style
-        tableView.tableHeaderView = UIView(frame: CGRect(x: 0, y: 0, width: 0, height: 24))
         return tableView
     }()
 

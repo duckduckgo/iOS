bbe86c6a85e75128f5a16cf31963b307f719666f
Chris Brind <brindy@duckduckgo.com>
2024-03-13 14:26:11 +0000

    enable history by default for adhoc builds

--- Core/HistoryManager.swift
+++ Core/HistoryManager.swift
@@ -74,7 +74,9 @@ public class HistoryManager: HistoryManaging {
     }
 
     func isHistoryFeatureEnabled() -> Bool {
-        return privacyConfigManager.privacyConfig.isEnabled(featureKey: .history) && variantManager.isSupported(feature: .history)
+        #warning("DO NOT MERGE THIS")
+        return true
+//        return privacyConfigManager.privacyConfig.isEnabled(featureKey: .history) && variantManager.isSupported(feature: .history)
     }
 
     public func removeAllHistory() async {

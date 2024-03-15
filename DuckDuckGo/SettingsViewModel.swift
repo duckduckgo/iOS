801836e811b4211b77709a1645cf1597538336d4
Daniel Bernal <dbernal@duckduckgo.com>
2024-03-15 21:24:23 +0100

    Disable file_lenght

--- DuckDuckGo/SettingsViewModel.swift
+++ DuckDuckGo/SettingsViewModel.swift
@@ -16,7 +16,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 //
-
 // swiftlint:disable file_length
 import Core
 import BrowserServicesKit

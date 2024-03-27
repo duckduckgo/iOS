87ee3b9a756085f73d6ad8d8fd011e52c4543e7f
Daniel Bernal <dbernal@duckduckgo.com>
2024-03-27 15:40:18 +0100

    Subscriptions: 25 - Navigation Fix + State.cleanup (#2648)
    Task/Issue URL: https://app.asana.com/0/1204099484721401/1206905872297213/f
    
    Description:
    
    Ignores WKWebView 999 errors which triggered a false positive
    Improve memory management of UserScripts/Subfeatures

--- DuckDuckGo/Subscription/Views/SubscriptionRestoreView.swift
+++ DuckDuckGo/Subscription/Views/SubscriptionRestoreView.swift
@@ -147,7 +147,6 @@ struct SubscriptionRestoreView: View {
         }
         
         .onAppear {
-            viewModel.initializeView()
             viewModel.onAppear()
             setUpAppearances()
         }

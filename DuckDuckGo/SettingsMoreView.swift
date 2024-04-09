298cc2e317d4498d4c0b3d642cc17c1db568f9ef
Tomas Strba <tom@duckduckgo.com>
2024-04-09 11:08:39 +0200

    Missing email pixel added

--- DuckDuckGo/SettingsMoreView.swift
+++ DuckDuckGo/SettingsMoreView.swift
@@ -19,6 +19,7 @@
 
 import SwiftUI
 import UIKit
+import Core
 
 struct SettingsMoreView: View {
         
@@ -30,7 +31,11 @@ struct SettingsMoreView: View {
             
             SettingsCellView(label: UserText.emailProtection,
                              subtitle: UserText.settingsEmailProtectionDescription,
-                             action: { viewModel.openEmailProtection() },
+                             action: {
+                viewModel.openEmailProtection()
+                Pixel.fire(pixel: .settingsEmailProtectionOpen,
+                           withAdditionalParameters: PixelExperiment.parameters)
+            },
                              disclosureIndicator: true,
                              isButton: true)
             

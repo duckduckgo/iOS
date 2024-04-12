9261f84faa0b1b37e9a762a3a3b369fc22e85299
Daniel Bernal <dbernal@duckduckgo.com>
2024-04-12 14:04:48 +0200

    Small UI Fixes for subscriptions (#2690)
    Task/Issue URL: https://app.asana.com/0/1204099484721401/1207003662391345/f
    
    Description:
    A few minor UI changes.
    - Added a title to the VPN navigation bar
    - Added a loader to Restore View

--- DuckDuckGo/Subscription/ViewModel/SubscriptionRestoreViewModel.swift
+++ DuckDuckGo/Subscription/ViewModel/SubscriptionRestoreViewModel.swift
@@ -107,11 +107,8 @@ final class SubscriptionRestoreViewModel: ObservableObject {
                     self.state.viewTitle = UserText.subscriptionAddDeviceTitle
                 }
             default:
-                state.isLoading = false
-            }
-        } else {
-            DispatchQueue.main.async {
                 self.state.viewTitle = UserText.subscriptionActivate
+                state.isLoading = false
             }
         }
     }

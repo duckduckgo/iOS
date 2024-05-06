fb82e9088b25a739566111a5f4a421beb3091fd8
Graeme Arthur <garthur@duckduckgo.com>
2024-05-06 18:48:40 +0200

    Set placeholder widget to connected to LA

--- Widgets/VPNWidget.swift
+++ Widgets/VPNWidget.swift
@@ -51,11 +51,11 @@ class VPNStatusTimelineProvider: TimelineProvider {
     typealias Entry = VPNStatusTimelineEntry
 
     func placeholder(in context: Context) -> VPNStatusTimelineEntry {
-        return VPNStatusTimelineEntry(date: Date(), status: .status(.disconnected), location: "Los Angeles, CA")
+        return VPNStatusTimelineEntry(date: Date(), status: .status(.connected), location: "Los Angeles")
     }
 
     func getSnapshot(in context: Context, completion: @escaping (VPNStatusTimelineEntry) -> Void) {
-        let entry = VPNStatusTimelineEntry(date: Date(), status: .status(.disconnected), location: "Los Angeles, CA")
+        let entry = VPNStatusTimelineEntry(date: Date(), status: .status(.connected), location: "Los Angeles")
         completion(entry)
     }
 

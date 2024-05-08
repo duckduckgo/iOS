b58e5134511009ecaeb7ce4aa9d5ca264ef8f0d5
amddg44 <amallon@duckduckgo.com>
2024-05-08 15:12:52 +0200

    Updated mocks

--- DuckDuckGoTests/MockSecureVault.swift
+++ DuckDuckGoTests/MockSecureVault.swift
@@ -77,6 +77,14 @@ final class MockSecureVault<T: AutofillDatabaseProvider>: AutofillSecureVault {
         return storedAccounts
     }
 
+    func accountsCount() throws -> Int {
+        return storedAccounts.count
+    }
+
+    func accountsCountBucket() throws -> String {
+        return ""
+    }
+
     func accountsFor(domain: String) throws -> [SecureVaultModels.WebsiteAccount] {
         return storedAccounts.filter { $0.domain == domain }
     }
@@ -301,6 +309,10 @@ class MockDatabaseProvider: AutofillDatabaseProvider {
 
     func accounts() throws -> [SecureVaultModels.WebsiteAccount] {
         return _accounts
+    }
+
+    func accountsCount() throws -> Int {
+        return _accounts.count
     }
 
     func neverPromptWebsites() throws -> [SecureVaultModels.NeverPromptWebsites] {

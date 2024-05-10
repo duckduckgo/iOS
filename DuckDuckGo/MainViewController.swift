5f6131c326acb6f5b7117623b94fe39a554c5cf3
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-05-10 06:42:08 +0200

    registerForSynFeatureFlags -> registerForSyncFeatureFlagsUpdates

--- DuckDuckGo/MainViewController.swift
+++ DuckDuckGo/MainViewController.swift
@@ -259,7 +259,7 @@ class MainViewController: UIViewController {
         findInPageView.delegate = self
         findInPageBottomLayoutConstraint.constant = 0
         registerForKeyboardNotifications()
-        registerForSynFeatureFlags()
+        registerForSyncFeatureFlagsUpdates()
 
         decorate()
 
@@ -445,7 +445,7 @@ class MainViewController: UIViewController {
         keyboardShowing = false
     }
 
-    private func registerForSynFeatureFlags() {
+    private func registerForSyncFeatureFlagsUpdates() {
         syncFeatureFlagsCancellable = syncService.featureFlagsPublisher
             .dropFirst()
             .map { $0.contains(.dataSyncing) }

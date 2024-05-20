1ed77dbe771f4ce7e6cd443997fd4fc101c14577
Chris Brind <brindy@duckduckgo.com>
2024-05-20 09:55:02 +0100

    Revert "speculative fix to main vc related crash  (#2864)"
    This reverts commit f972912267efd7991f0559e715e4851f73f5984b.

--- DuckDuckGo/MainViewController.swift
+++ DuckDuckGo/MainViewController.swift
@@ -1601,11 +1601,12 @@ extension MainViewController: BrowserChromeDelegate {
         let updateBlock = {
             self.updateToolbarConstant(percent)
             self.updateNavBarConstant(percent)
-            self.view.needsUpdateConstraints()
 
             self.viewCoordinator.navigationBarContainer.alpha = percent
             self.viewCoordinator.tabBarContainer.alpha = percent
             self.viewCoordinator.toolbar.alpha = percent
+
+            self.view.layoutIfNeeded()
         }
            
         if animated {

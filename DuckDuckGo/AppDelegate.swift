078ee2af5a89fd475377aefdd8a5031cac61e9a6
Sam Symons <sam@samsymons.com>
2024-05-22 08:29:12 -0700

    Check entitlement return value when managing shortcut.

--- DuckDuckGo/AppDelegate.swift
+++ DuckDuckGo/AppDelegate.swift
@@ -917,7 +917,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             return
         }
 
-        if case .success = await AccountManager().hasEntitlement(for: .networkProtection, cachePolicy: .returnCacheDataDontLoad) {
+        if case .success(true) = await AccountManager().hasEntitlement(for: .networkProtection, cachePolicy: .returnCacheDataDontLoad) {
             let items = [
                 UIApplicationShortcutItem(type: ShortcutKey.openVPNSettings,
                                           localizedTitle: UserText.netPOpenVPNQuickAction,

919b602e41092838c930805d8249561477670658
amddg44 <amallon@duckduckgo.com>
2024-05-23 21:44:12 +0200

    Autofill engagement KPIs for pixel reporting (BSK update) (#2885)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1207357107981852/f
    Tech Design URL:
    CC:
    
    Description:
    Update to use BSK for Autofill engagement KPIs for pixel reporting

--- DuckDuckGo/AppDelegate.swift
+++ DuckDuckGo/AppDelegate.swift
@@ -81,7 +81,7 @@ import WebKit
     private let crashCollection = CrashCollection(platform: .iOS, log: .generalLog)
     private var crashReportUploaderOnboarding: CrashCollectionOnboarding?
 
-    private let autofillPixelReporter = AutofillPixelReporter()
+    private var autofillPixelReporter: AutofillPixelReporter?
 
     // MARK: lifecycle
 
@@ -339,6 +339,8 @@ import WebKit
         AppDependencyProvider.shared.toggleProtectionsCounter.sendEventsIfNeeded()
 
         AppDependencyProvider.shared.userBehaviorMonitor.handleAction(.reopenApp)
+
+        setUpAutofillPixelReporter()
 
         return true
     }
@@ -681,8 +683,6 @@ import WebKit
         }
 
         AppDependencyProvider.shared.userBehaviorMonitor.handleAction(.reopenApp)
-
-        autofillPixelReporter.checkIfOnboardedUser()
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -909,6 +909,26 @@ import WebKit
 
     private var mainViewController: MainViewController? {
         return window?.rootViewController as? MainViewController
+    }
+
+    private func setUpAutofillPixelReporter() {
+        autofillPixelReporter = AutofillPixelReporter(
+            userDefaults: .standard,
+            eventMapping: EventMapping<AutofillPixelEvent> {event, _, params, _ in
+                switch event {
+                case .autofillActiveUser:
+                    Pixel.fire(pixel: .autofillActiveUser)
+                case .autofillEnabledUser:
+                    Pixel.fire(pixel: .autofillEnabledUser)
+                case .autofillOnboardedUser:
+                    Pixel.fire(pixel: .autofillOnboardedUser)
+                case .autofillLoginsStacked:
+                    Pixel.fire(pixel: .autofillLoginsStacked, withAdditionalParameters: params ?? [:])
+                default:
+                    break
+                }
+            },
+            installDate: StatisticsUserDefaults().installDate ?? Date())
     }
 
     @MainActor

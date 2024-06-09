ce613ddac37772fce48c7ee79721fe183da4b606
Sam Symons <sam@samsymons.com>
2024-06-09 13:34:46 -0700

    Add Privacy Pro survey parameters (#2920)
    Task/Issue URL: https://app.asana.com/0/414235014887631/1207500645737162/f
    Tech Design URL:
    CC:
    
    Description:
    
    This PR adds new survey parameters for Privacy Pro.

--- DuckDuckGo/RemoteMessagingSurveyURLBuilder.swift
+++ DuckDuckGo/RemoteMessagingSurveyURLBuilder.swift
@@ -22,19 +22,23 @@ import BrowserServicesKit
 import RemoteMessaging
 import Core
 import Common
+import Subscription
 
 struct DefaultRemoteMessagingSurveyURLBuilder: RemoteMessagingSurveyActionMapping {
 
     private let statisticsStore: StatisticsStore
-    private let activationDateStore: VPNActivationDateStore
+    private let vpnActivationDateStore: VPNActivationDateStore
+    private let subscription: Subscription?
 
     init(statisticsStore: StatisticsStore = StatisticsUserDefaults(),
-         activationDateStore: VPNActivationDateStore = DefaultVPNActivationDateStore()) {
+         vpnActivationDateStore: VPNActivationDateStore = DefaultVPNActivationDateStore(),
+         subscription: Subscription?) {
         self.statisticsStore = statisticsStore
-        self.activationDateStore = activationDateStore
+        self.vpnActivationDateStore = vpnActivationDateStore
+        self.subscription = subscription
     }
 
-    // swiftlint:disable:next cyclomatic_complexity
+    // swiftlint:disable:next cyclomatic_complexity function_body_length
     func add(parameters: [RemoteMessagingSurveyActionParameter], to surveyURL: URL) -> URL {
         guard var components = URLComponents(string: surveyURL.absoluteString) else {
             assertionFailure("Could not build URL components from survey URL")
@@ -60,14 +64,55 @@ struct DefaultRemoteMessagingSurveyURLBuilder: RemoteMessagingSurveyActionMappin
             case .hardwareModel:
                 let model = hardwareModel().addingPercentEncoding(withAllowedCharacters: .alphanumerics)
                 queryItems.append(URLQueryItem(name: parameter.rawValue, value: model))
-            case .lastActiveDate:
-                if let daysSinceLastActive = activationDateStore.daysSinceLastActive() {
-                    queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: daysSinceLastActive)))
-                }
             case .daysInstalled:
                 if let installDate = statisticsStore.installDate,
-                      let daysSinceInstall = Calendar.current.numberOfDaysBetween(installDate, and: Date()) {
+                   let daysSinceInstall = Calendar.current.numberOfDaysBetween(installDate, and: Date()) {
                     queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: daysSinceInstall)))
+                }
+            case .privacyProStatus:
+                switch subscription?.status {
+                case .autoRenewable: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "auto_renewable"))
+                case .notAutoRenewable: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "not_auto_renewable"))
+                case .gracePeriod: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "grace_period"))
+                case .inactive: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "inactive"))
+                case .expired: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "expired"))
+                case .unknown: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "unknown"))
+                case nil: break
+                }
+            case .privacyProPlatform:
+
+                switch subscription?.platform {
+                case .apple: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "apple"))
+                case .google: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "google"))
+                case .stripe: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "stripe"))
+                case .unknown: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "unknown"))
+                case nil: break
+                }
+            case .privacyProBilling:
+                switch subscription?.billingPeriod {
+                case .monthly: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "monthly"))
+                case .yearly: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "yearly"))
+                case .unknown: queryItems.append(URLQueryItem(name: parameter.rawValue, value: "unknown"))
+                case nil: break
+                }
+
+            case .privacyProDaysSincePurchase:
+                if let startDate = subscription?.startedAt,
+                   let daysSincePurchase = Calendar.current.numberOfDaysBetween(startDate, and: Date()) {
+                    queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: daysSincePurchase)))
+                }
+            case .privacyProDaysUntilExpiry:
+                if let expiryDate = subscription?.expiresOrRenewsAt,
+                   let daysUntilExpiry = Calendar.current.numberOfDaysBetween(Date(), and: expiryDate) {
+                    queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: daysUntilExpiry)))
+                }
+            case .vpnFirstUsed:
+                if let vpnFirstUsed = vpnActivationDateStore.daysSinceActivation() {
+                    queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: vpnFirstUsed)))
+                }
+            case .vpnLastUsed:
+                if let vpnLastUsed = vpnActivationDateStore.daysSinceLastActive() {
+                    queryItems.append(URLQueryItem(name: parameter.rawValue, value: String(describing: vpnLastUsed)))
                 }
             }
         }

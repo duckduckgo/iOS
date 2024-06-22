eb828053a4f5baa9b5eb3f7fbaf588afc4ddf33a
Alessandro Boron <aboron@duckduckgo.com>
2024-06-22 17:45:43 +1000

    Fix Layout

--- DuckDuckGo/OnboardingExperiment/DaxDialogs/BrowsersComparisonChartView.swift
+++ DuckDuckGo/OnboardingExperiment/DaxDialogs/BrowsersComparisonChartView.swift
@@ -19,185 +19,147 @@
 
 import SwiftUI
 
-enum BrowserFeature: CaseIterable, Identifiable {
-    case privateSearch
-    case blockThirdPartyTrackers
-    case blockCookiePopups
-    case blockCreepyAds
-    case eraseBrowsingData
+struct BrowsersComparisonModel {
 
-    var id: Self {
-        self
-    }
-
-    var title: String {
-        switch self {
-        case .privateSearch:
-            UserText.DaxOnboardingExperiment.BrowsersComparison.Features.privateSearch
-        case .blockThirdPartyTrackers:
-            UserText.DaxOnboardingExperiment.BrowsersComparison.Features.trackerBlockers
-        case .blockCookiePopups:
-            UserText.DaxOnboardingExperiment.BrowsersComparison.Features.cookiePopups
-        case .blockCreepyAds:
-            UserText.DaxOnboardingExperiment.BrowsersComparison.Features.creepyAds
-        case .eraseBrowsingData:
-            UserText.DaxOnboardingExperiment.BrowsersComparison.Features.eraseBrowsingData
-        }
-    }
-}
-
-enum BrowserFeatureAvailability: Identifiable {
-    case available
-    case partiallyAvailable
-    case unavailable
-
-    var id: Self {
-        self
-    }
-
-    var image: String {
-        switch self {
-        case .available:
-            "CheckGreen"
-        case .partiallyAvailable:
-            "Stop"
-        case .unavailable:
-            "Cross"
-        }
-    }
-}
-
-enum Browser: CaseIterable, Identifiable {
-    case safari
-    case ddg
-
-    var id: Self {
-        self
-    }
-
-    var image: String {
-        switch self {
-        case .safari:
-            "SafariLogo"
-        case .ddg:
-            "SafariLogo"
-        }
-    }
-}
-
-struct BrowserComparisonChartModel {
-
-    static let privacyFeatures: [PrivacyFeature] = [
+    static let browsers: [BrowserModel] = [
         .init(
-            type: .privateSearch,
-            browsersAvailability: [
-                .init(browser: .safari, availability: .unavailable),
-                .init(browser: .ddg, availability: .available),
+            browser: Browser(type: .safari),
+            privacyFeatures: [
+                .init(
+                    feature: .privateSearch,
+                    availability: .unavailable
+                ),
+                .init(
+                    feature: .blockThirdPartyTrackers,
+                    availability: .partiallyAvailable
+                ),
+                .init(
+                    feature: .blockCookiePopups,
+                    availability: .unavailable
+                ),
+                .init(
+                    feature: .blockCreepyAds,
+                    availability: .unavailable
+                ),
+                .init(
+                    feature: .eraseBrowsingData,
+                    availability: .unavailable
+                ),
             ]
         ),
         .init(
-            type: .blockThirdPartyTrackers,
-            browsersAvailability: [
-                .init(browser: .safari, availability: .partiallyAvailable),
-                .init(browser: .ddg, availability: .available),
+            browser: Browser(type: .ddg),
+            privacyFeatures: [
+                .init(
+                    feature: .privateSearch,
+                    availability: .available
+                ),
+                .init(
+                    feature: .blockThirdPartyTrackers,
+                    availability: .available
+                ),
+                .init(
+                    feature: .blockCookiePopups,
+                    availability: .available
+                ),
+                .init(
+                    feature: .blockCreepyAds,
+                    availability: .available
+                ),
+                .init(
+                    feature: .eraseBrowsingData,
+                    availability: .available
+                ),
             ]
-        ),
-        .init(
-            type: .blockCookiePopups,
-            browsersAvailability: [
-                .init(browser: .safari, availability: .unavailable),
-                .init(browser: .ddg, availability: .available),
-            ]
-        ),
-        .init(
-            type: .blockCreepyAds,
-            browsersAvailability: [
-                .init(browser: .safari, availability: .unavailable),
-                .init(browser: .ddg, availability: .available),
-            ]
-        ),
-        .init(
-            type: .eraseBrowsingData,
-            browsersAvailability: [
-                .init(browser: .safari, availability: .unavailable),
-                .init(browser: .ddg, availability: .available),
-            ]
-        ),
+        )
     ]
 
 }
 
-struct BrowserModel {
-    struct Feature {
-        let feature: BrowserFeature
-        let availability: BrowserFeatureAvailability
-    }
-    let browser: Browser
-    let features: [Feature]
-}
+extension BrowsersComparisonModel {
 
-extension BrowserComparisonChartModel {
-    
-    struct PrivacyFeature {
-        struct Availability {
-            let browser: Browser
-            let availability: BrowserFeatureAvailability
-        }
+    struct Browser {
+        enum `Type`: CaseIterable, Identifiable {
+            case safari
+            case ddg
 
-        let type: BrowserFeature
-        let browsersAvailability: [Availability]
-    }
+            var id: Self {
+                self
+            }
 
-}
-
-struct BrowsersComparisonChartView: View {
-
-    let features: [BrowserComparisonChartModel.PrivacyFeature]
-
-    var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
-            BrowserComparisonChartHeader(browsers: Browser.allCases)
-                .frame(height: 50.0)
-
-//            ForEach(features, id: \.type) { feature in
-//                HStack {
-//                    BrowserComparisonChartItemView(feature: feature).frame(height: 50.0)
-//                }
-//            }
-
-            HStack {
-                VStack(alignment: .leading) {
-                    ForEach(features, id: \.type) { feature in
-                        BrowserFeatureTitleView(title: feature.type.title)
-                            .frame(height: 50)
-                    }
-                }
-
-                Spacer(minLength: 170)
-
-                VStack {
-                    ForEach(features, id: \.type) { feature in
-                        HStack(spacing: 0) {
-                            ForEach(feature.browsersAvailability, id: \.browser) { browser in
-                                BrowserFeatureAvailabilityView(availability: browser.availability)
-                                    .frame(height: 50)
-                            }
-                        }
-
-                    }
+            var image: ImageResource {
+                switch self {
+                case .safari: .safariBrowserIcon
+                case .ddg: .ddgBrowserIcon
                 }
             }
         }
-        .padding()
+        let type: `Type`
     }
+
+    enum PrivacyFeature: CaseIterable, Identifiable {
+        case privateSearch
+        case blockThirdPartyTrackers
+        case blockCookiePopups
+        case blockCreepyAds
+        case eraseBrowsingData
+
+        var id: Self {
+            self
+        }
+
+        var title: String {
+            switch self {
+            case .privateSearch:
+                UserText.DaxOnboardingExperiment.BrowsersComparison.Features.privateSearch
+            case .blockThirdPartyTrackers:
+                UserText.DaxOnboardingExperiment.BrowsersComparison.Features.trackerBlockers
+            case .blockCookiePopups:
+                UserText.DaxOnboardingExperiment.BrowsersComparison.Features.cookiePopups
+            case .blockCreepyAds:
+                UserText.DaxOnboardingExperiment.BrowsersComparison.Features.creepyAds
+            case .eraseBrowsingData:
+                UserText.DaxOnboardingExperiment.BrowsersComparison.Features.eraseBrowsingData
+            }
+        }
+
+        enum Availability: Identifiable {
+            case available
+            case partiallyAvailable
+            case unavailable
+
+            var id: Self {
+                self
+            }
+
+            var image: ImageResource {
+                switch self {
+                case .available: .checkGreen
+                case .partiallyAvailable: .stop
+                case .unavailable: .cross
+                }
+            }
+        }
+    }
+
+    struct BrowserModel {
+        struct Feature {
+            let feature: BrowsersComparisonModel.PrivacyFeature
+            let availability: PrivacyFeature.Availability
+        }
+
+        let browser: Browser
+        let privacyFeatures: [Feature]
+    }
+
 }
 
 struct BrowsersComparisonAvailabilityColumn: View {
-    let browser: BrowserModel
+    let browser: BrowsersComparisonModel.BrowserModel
 
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
-            ForEach(browser.features, id: \.feature.id) { feature in
+            ForEach(browser.privacyFeatures, id: \.feature.id) { feature in
                 Image(feature.availability.image)
                     .frame(width: 50, height: 50)
 
@@ -208,19 +170,25 @@ struct BrowsersComparisonAvailabilityColumn: View {
 }
 
 struct BrowsersComparisonColumns: View {
-    let browsers: [BrowserModel]
+    let browsers: [BrowsersComparisonModel.BrowserModel]
 
     var body: some View {
         HStack(spacing: 0) {
-            ForEach(browsers, id: \.browser.id) { browser in
+            ForEach(Array(browsers.enumerated()), id: \.offset) { index, browser in
                 BrowsersComparisonAvailabilityColumn(browser: browser)
+                    .frame(width: 50)
+
+                if index < browsers.count - 1 {
+                    Divider()
+                }
             }
         }
+        .fixedSize(horizontal: true, vertical: /*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/) // Stops the Divider to expand vertically
     }
 }
 
 struct BrowsersComparisonTitleView: View {
-    let features: [BrowserFeature]
+    let features: [BrowsersComparisonModel.PrivacyFeature]
 
     var body: some View {
         VStack(spacing: 0) {
@@ -233,11 +201,13 @@ struct BrowsersComparisonTitleView: View {
 }
 
 struct BrowsersComparisonView: View {
-    let features: [BrowserFeature]
-    let browsers: [BrowserModel]
+    let features: [BrowsersComparisonModel.PrivacyFeature]
+    let browsers: [BrowsersComparisonModel.BrowserModel]
 
     var body: some View {
-        VStack {
+        VStack(spacing: 0) {
+            BrowserComparisonChartHeader(browsers: browsers.map(\.browser))
+                .frame(height: 60)
             HStack(spacing: 0) {
                 BrowsersComparisonTitleView(features: features)
                 BrowsersComparisonColumns(browsers: browsers)
@@ -249,18 +219,21 @@ struct BrowsersComparisonView: View {
 
 
 struct BrowserComparisonChartHeader: View {
-    
-    let browsers: [Browser]
+    let browsers: [BrowsersComparisonModel.Browser]
 
     var body: some View {
-        HStack {
+        HStack(alignment: .top, spacing: 0) {
             Spacer()
 
-            ForEach(Array(browsers.enumerated()), id: \.offset) { index, browser in
-                BrowserLogo(browser: .safari)
-                //if index < browser.count - 1 {
+            ForEach(Array(browsers.map(\.type).enumerated()), id: \.offset) { index, browser in
+                Image(browser.image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 50, height: 40)
+
+                if index < browsers.count - 1 {
                     Divider()
-                //}
+                }
             }
         }
     }
@@ -286,8 +259,7 @@ struct BrowserFeatureTitleView: View {
 }
 
 struct BrowserFeatureAvailabilityView: View {
-
-    let availability: BrowserFeatureAvailability
+    let availability: BrowsersComparisonModel.PrivacyFeature.Availability
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -305,132 +277,10 @@ struct BrowserFeatureAvailabilityView: View {
     }
 }
 
-struct BrowserComparisonChartItemView: View {
-    let feature: BrowserComparisonChartModel.PrivacyFeature
-
-    var body: some View {
-        VStack(alignment: .leading) {
-
-            Spacer()
-
-            HStack {
-                Text(verbatim: feature.type.title)
-                    .font(.system(size: 15.0))
-                    .foregroundColor(.primary)
-
-                ForEach(feature.browsersAvailability, id: \.browser) { availability in
-                    Image(availability.availability.image)
-
-                    Divider()
-                }
-            }
-
-            Spacer()
-
-            Divider()
-        }
-    }
-}
-
-struct BrowserLogo: View {
-    enum Browser {
-        case safari
-        case ddg
-
-        fileprivate var image: String {
-            switch self {
-            case .safari:
-                "SafariLogo"
-            case .ddg:
-                "SafariLogo"
-            }
-        }
-    }
-
-    private let browser: Browser
-
-    init(browser: Browser) {
-        self.browser = browser
-    }
-
-    var body: some View {
-        Rectangle()
-            .foregroundColor(.clear)
-            .frame(width: 40, height: 40)
-            .background(
-                Image(browser.image)
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 40, height: 40)
-                    .clipped()
-            )
-    }
-
-}
-
 #Preview {
-
-    let browsers: [BrowserModel] = [
-        .init(
-            browser: .safari,
-            features: [
-                .init(
-                    feature: .privateSearch,
-                    availability: .unavailable
-                ),
-                .init(
-                    feature: .blockThirdPartyTrackers,
-                    availability: .partiallyAvailable
-                ),
-                .init(
-                    feature: .blockCookiePopups,
-                    availability: .unavailable
-                ),
-                .init(
-                    feature: .blockCreepyAds,
-                    availability: .unavailable
-                ),
-                .init(
-                    feature: .eraseBrowsingData,
-                    availability: .unavailable
-                ),
-            ]
-        ),
-        .init(
-            browser: .ddg,
-            features: [
-                .init(
-                    feature: .privateSearch,
-                    availability: .available
-                ),
-                .init(
-                    feature: .blockThirdPartyTrackers,
-                    availability: .available
-                ),
-                .init(
-                    feature: .blockCookiePopups,
-                    availability: .available
-                ),
-                .init(
-                    feature: .blockCreepyAds,
-                    availability: .available
-                ),
-                .init(
-                    feature: .eraseBrowsingData,
-                    availability: .available
-                ),
-            ]
-        )
-    ]
-
-
-    return BrowsersComparisonView(
-        features: BrowserFeature.allCases,
-        browsers: browsers
+    BrowsersComparisonView(
+        features: BrowsersComparisonModel.PrivacyFeature.allCases,
+        browsers: BrowsersComparisonModel.browsers
     )
     .padding()
-}
-
-#Preview {
-    BrowsersComparisonChartView(features: BrowserComparisonChartModel.privacyFeatures)
 }

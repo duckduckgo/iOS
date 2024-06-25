127758b7b5681f9768e5c9a037f325b636bbe265
Christopher Brind <brindy@duckduckgo.com>
2024-06-25 18:32:51 +0100

    don't attach a new home screen if autoclear is in progress (#2988)

--- DuckDuckGo/MainViewController.swift
+++ DuckDuckGo/MainViewController.swift
@@ -732,6 +732,8 @@ class MainViewController: UIViewController {
     }
     
     fileprivate func attachHomeScreen() {
+        guard !autoClearInProgress else { return }
+        
         viewCoordinator.logoContainer.isHidden = false
         findInPageView.isHidden = true
         chromeManager.detach()

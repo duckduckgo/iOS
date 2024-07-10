af8aae0b449f23c0ed753056a78a18a0afb629a1
Sabrina Tardio <44158575+SabrinaTardio@users.noreply.github.com>
2024-07-10 17:03:43 +0200

    add sites providers (#3055)
    Task/Issue URL: https://app.asana.com/0/1204186595873227/1207569384066479/f
    
    Description: Implements the providers for search and site suggestions showed in the contextual onboarding according to https://app.asana.com/0/1202552961248957/1207407312592957/f

---
+++ DuckDuckGo/OnboardingHelpers/OnboardingSuggestedSitesProvider.swift
@@ -0,0 +1,112 @@
+//
+//  OnboardingSuggestedSitesProvider.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+protocol OnboardingSuggestedSitesProviding {
+    var sitesList: [ContextualOnboardingListItem] { get }
+}
+
+struct OnboardingSuggestedSitesProvider: OnboardingSuggestedSitesProviding {
+    private let countryProvider: OnboardingRegionAndLanguageProvider
+
+    init(countryProvider: OnboardingRegionAndLanguageProvider = Locale.current) {
+        self.countryProvider = countryProvider
+    }
+
+    enum Countries: String {
+        case indonesia = "ID"
+        case gb = "GB"
+        case germany = "DE"
+        case canada = "CA"
+        case netherlands = "NL"
+        case australia = "AU"
+        case sweden = "SE"
+        case ireland = "IE"
+    }
+
+    var sitesList: [ContextualOnboardingListItem] {
+        return [
+            option1,
+            option2,
+            option3,
+            surpriseMe
+        ]
+    }
+
+    private var country: String {
+        countryProvider.regionCode ?? ""
+    }
+
+    private var option1: ContextualOnboardingListItem {
+        let site: String
+        switch Countries(rawValue: country) {
+        case .indonesia: site = "bolasport.com"
+        case .gb: site = "skysports.com"
+        case .germany: site = "kicker.de"
+        case .canada: site = "tsn.ca"
+        case .netherlands: site = "voetbalprimeur.nl"
+        case .australia: site = "afl.com.au"
+        case .sweden: site = "svenskafans.com"
+        case .ireland: site = "skysports.com"
+        default: site = "ESPN.com"
+        }
+        return ContextualOnboardingListItem.site(title: site)
+    }
+
+    private var option2: ContextualOnboardingListItem {
+        let site: String
+        switch Countries(rawValue: country) {
+        case .indonesia: site = "kompas.com"
+        case .gb: site = "bbc.co.uk"
+        case .germany: site = "tagesschau.de"
+        case .canada: site = "cbc.ca"
+        case .netherlands: site = "nu.nl"
+        case .australia: site = "abc.net.au"
+        case .sweden: site = "dn.se"
+        case .ireland: site = "bbc.co.uk"
+        default: site = "yahoo.com"
+        }
+        return ContextualOnboardingListItem.site(title: site)
+    }
+
+    private var option3: ContextualOnboardingListItem {
+        let site: String
+        switch Countries(rawValue: country) {
+        case .indonesia: site = "tokopedia.com"
+        case .gb, .germany, .australia, .ireland: site = "eBay.com"
+        case .canada: site = "canadiantire.ca"
+        case .netherlands: site = "bol.com"
+        case .sweden: site = "tradera.com"
+        default: site = "eBay.com"
+        }
+        return ContextualOnboardingListItem.site(title: site)
+    }
+
+    private var surpriseMe: ContextualOnboardingListItem {
+        let site: String
+        switch Countries(rawValue: country) {
+        case .germany: site = "https://www.duden.de/rechtschreibung/Ente"
+        case .netherlands: site = "https://www.woorden.org/woord/eend"
+        case .sweden: site = "https://www.synonymer.se/sv-syn/anka"
+        default: site = "britannica.com/animal/duck"
+        }
+        return ContextualOnboardingListItem.surprise(title: site)
+    }
+}
---
+++ DuckDuckGoTests/OnboardingSuggestedSitesProviderTests.swift
@@ -0,0 +1,174 @@
+//
+//  OnboardingSuggestedSitesProviderTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import DuckDuckGo
+
+class OnboardingSuggestedSitesProviderTests: XCTestCase {
+
+    func testSuggestedSitesForIndonesia() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "ID", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "bolasport.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "kompas.com"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "tokopedia.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForGB() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "GB", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "skysports.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "bbc.co.uk"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForGermany() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "DE", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "kicker.de"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "tagesschau.de"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "https://www.duden.de/rechtschreibung/Ente"))
+    }
+
+    func testSuggestedSitesForCanada() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "CA", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "tsn.ca"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "cbc.ca"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "canadiantire.ca"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForNetherlands() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "NL", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "voetbalprimeur.nl"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "nu.nl"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "bol.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "https://www.woorden.org/woord/eend"))
+    }
+
+    func testSuggestedSitesForAustralia() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "AU", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "afl.com.au"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "abc.net.au"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForSweden() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "SE", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "svenskafans.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "dn.se"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "tradera.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "https://www.synonymer.se/sv-syn/anka"))
+    }
+
+    func testSuggestedSitesForIreland() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "IE", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "skysports.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "bbc.co.uk"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForUS() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "US", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "ESPN.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "yahoo.com"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+
+    func testSuggestedSitesForUnknownCountry() {
+        // GIVEN
+        let mockProvider = MockOnboardingRegionAndLanguageProvider(regionCode: "UNKNOWN", languageCode: "")
+        let sut = OnboardingSuggestedSitesProvider(countryProvider: mockProvider)
+
+        // WHEN
+        let sitesList = sut.sitesList
+
+        // THEN
+        XCTAssertEqual(sitesList[0], ContextualOnboardingListItem.site(title: "ESPN.com"))
+        XCTAssertEqual(sitesList[1], ContextualOnboardingListItem.site(title: "yahoo.com"))
+        XCTAssertEqual(sitesList[2], ContextualOnboardingListItem.site(title: "eBay.com"))
+        XCTAssertEqual(sitesList[3], ContextualOnboardingListItem.surprise(title: "britannica.com/animal/duck"))
+    }
+}

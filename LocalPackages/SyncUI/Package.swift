f0b8332a4677a30432fb9a359b3e5377cacdfe6a
Chris Brind <brindy@duckduckgo.com>
2024-07-15 12:06:05 +0100

    bump toolbox

--- LocalPackages/SyncUI/Package.swift
+++ LocalPackages/SyncUI/Package.swift
@@ -34,7 +34,7 @@ let package = Package(
     dependencies: [
         .package(path: "../DuckUI"),
         .package(url: "https://github.com/duckduckgo/DesignResourcesKit", exact: "3.0.0"),
-        .package(url: "https://github.com/duckduckgo/apple-toolbox.git", exact: "3.1.1"),
+        .package(url: "https://github.com/duckduckgo/apple-toolbox.git", exact: "3.1.2"),
     ],
     targets: [
         .target(

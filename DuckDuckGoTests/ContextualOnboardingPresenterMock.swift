fea847a85eea6a3a32c72e63b6f5a880915abfa6
Alessandro Boron <aboron@duckduckgo.com>
2024-07-17 17:52:04 +1000

    Fix tests

---
+++ DuckDuckGoTests/ContextualOnboardingPresenterMock.swift
@@ -0,0 +1,36 @@
+//
+//  ContextualOnboardingPresenterMock.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+@testable import DuckDuckGo
+
+final class ContextualOnboardingPresenterMock: ContextualOnboardingPresenting {
+    private(set) var didCallPresentContextualOnboarding = false
+    private(set) var capturedBrowsingSpec: DaxDialogs.BrowsingSpec?
+    private(set) var didCallDismissContextualOnboardingIfNeeded = false
+
+    func presentContextualOnboarding(for spec: DaxDialogs.BrowsingSpec, in vc: TabViewOnboardingDelegate) {
+        didCallPresentContextualOnboarding = true
+        capturedBrowsingSpec = spec
+    }
+    
+    func dismissContextualOnboardingIfNeeded(from vc: TabViewOnboardingDelegate) {
+        didCallDismissContextualOnboardingIfNeeded = true
+    }
+}

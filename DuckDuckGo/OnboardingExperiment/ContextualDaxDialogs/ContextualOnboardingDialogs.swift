0105402eafbdaaafc23d64a0467ef6b245dc9f8f
Sabrina Tardio <stardio@duckduckgo.com>
2024-07-17 17:02:20 +0200

    make dialogs scrollable and animatable

--- DuckDuckGo/OnboardingExperiment/ContextualDaxDialogs/ContextualOnboardingDialogs.swift
+++ DuckDuckGo/OnboardingExperiment/ContextualDaxDialogs/ContextualOnboardingDialogs.swift
@@ -26,25 +26,37 @@ struct OnboardingTrySearchDialog: View {
     let viewModel: OnboardingSearchSuggestionsViewModel
 
     var body: some View {
-        ContextualDaxDialog(
-            logoPosition: .top,
-            title: title,
-            message: message,
-            list: viewModel.itemsList,
-            listAction: viewModel.listItemPressed)
+        ScrollView(.vertical, showsIndicators: false) {            DaxDialogView(logoPosition: .top) {
+                ContextualDaxDialogContent(
+                    title: title,
+                    message: message,
+                    list: viewModel.itemsList,
+                    listAction: viewModel.listItemPressed)
+            }
+        }
     }
 }
 
 struct OnboardingTryVisitingSiteDialog: View {
-    let title = UserText.DaxOnboardingExperiment.ContextualOnboarding.onboardingTryASiteTitle
-    let message = NSAttributedString(string: UserText.DaxOnboardingExperiment.ContextualOnboarding.onboardingTryASiteMessage)
-
     let logoPosition: DaxDialogLogoPosition
     let viewModel: OnboardingSiteSuggestionsViewModel
 
     var body: some View {
-        ContextualDaxDialog(
-            logoPosition: logoPosition,
+        ScrollView(.vertical) {
+            DaxDialogView(logoPosition: logoPosition) {
+                OnboardingTryVisitingSiteDialogContent(viewModel: viewModel)
+            }
+        }
+    }
+}
+
+struct OnboardingTryVisitingSiteDialogContent: View {
+    let title = UserText.DaxOnboardingExperiment.ContextualOnboarding.onboardingTryASiteTitle
+    let message = NSAttributedString(string: UserText.DaxOnboardingExperiment.ContextualOnboarding.onboardingTryASiteMessage)
+    let viewModel: OnboardingSiteSuggestionsViewModel
+
+    var body: some View {
+        ContextualDaxDialogContent(
             title: title,
             message: message,
             list: viewModel.itemsList,
@@ -52,7 +64,7 @@ struct OnboardingTryVisitingSiteDialog: View {
     }
 }
 
-struct OnboardingFireButtonDialog: View {
+struct OnboardingFireButtonDialogContent: View {
     private let attributedMessage: NSAttributedString = {
         let firstString = UserText.DaxOnboardingExperiment.ContextualOnboarding.onboardingTryFireButtonMessage
         let boldString = "Fire Button."
@@ -69,7 +81,7 @@ struct OnboardingFireButtonDialog: View {
     }()
 
     var body: some View {
-        ContextualDaxDialog(
+        ContextualDaxDialogContent(
             message: attributedMessage)
     }
 }
@@ -85,17 +97,28 @@ struct OnboardingFirstSearchDoneDialog: View {
     let gotItAction: () -> Void
 
     var body: some View {
-        if showNextScreen {
-            OnboardingTryVisitingSiteDialog(logoPosition: .left, viewModel: viewModel)
-        } else {
-            ContextualDaxDialog(
-                message: message,
-                cta: cta) {
-                    if shouldFollowUp {
-                        showNextScreen = true
+        ScrollView(.vertical, showsIndicators: false) {            DaxDialogView(logoPosition: .left) {
+                VStack {
+                    if showNextScreen {
+                        OnboardingTryVisitingSiteDialogContent(viewModel: viewModel)
                     } else {
-                        gotItAction()
+                        ContextualDaxDialogContent(
+                            message: message,
+                            cta: cta) {
+                                buttonAction()
+                        }
                     }
+                }
+            }
+        }
+    }
+
+    private func buttonAction() {
+        withAnimation {
+            if shouldFollowUp {
+                showNextScreen = true
+            } else {
+                gotItAction()
             }
         }
     }
@@ -110,14 +133,21 @@ struct OnboardingTrackersDoneDialog: View {
     let blockedTrackersCTAAction: () -> Void
 
     var body: some View {
-        if showNextScreen {
-            OnboardingFireButtonDialog()
-        } else {
-            ContextualDaxDialog(
-                message: message,
-                cta: cta) {
-                    showNextScreen = true
-                    blockedTrackersCTAAction()
+        ScrollView(.vertical, showsIndicators: false) {            DaxDialogView(logoPosition: .left) {
+                VStack {
+                    if showNextScreen {
+                        OnboardingFireButtonDialogContent()
+                    } else {
+                        ContextualDaxDialogContent(
+                            message: message,
+                            cta: cta) {
+                                withAnimation {
+                                    showNextScreen = true
+                                    blockedTrackersCTAAction()
+                                }
+                        }
+                    }
+                }
             }
         }
     }
@@ -132,13 +162,17 @@ struct OnboardingFinalDialog: View {
     let highFiveAction: () -> Void
 
     var body: some View {
-        ContextualDaxDialog(
-            title: title,
-            message: message,
-            imageName: imageName,
-            cta: cta,
-            action: highFiveAction
-        )
+        ScrollView(.vertical, showsIndicators: false) {
+            DaxDialogView(logoPosition: .left) {
+                ContextualDaxDialogContent(
+                    title: title,
+                    message: message,
+                    imageName: imageName,
+                    cta: cta,
+                    action: highFiveAction
+                )
+            }
+        }
     }
 }
 
@@ -160,7 +194,9 @@ struct OnboardingFinalDialog: View {
 }
 
 #Preview("Try Fire Button") {
-    OnboardingFireButtonDialog()
+    DaxDialogView(logoPosition: .left) {
+        OnboardingFireButtonDialogContent()
+    }
         .padding()
 }
 

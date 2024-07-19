93ce53f7b19c74173219d503f8de81e94be7705b
Alessandro Boron <aboron@duckduckgo.com>
2024-07-19 13:38:53 +1000

    Show final dialog from NTP only if we haven't seen it already

--- DuckDuckGo/DaxDialogs.swift
+++ DuckDuckGo/DaxDialogs.swift
@@ -452,7 +452,9 @@ final class DaxDialogs: NewTabDialogSpecProvider, ContextualOnboardingLogic {
             return .subsequent
         }
 
-        if firstBrowsingMessageSeen {
+        if firstBrowsingMessageSeen && !finalDaxDialogSeen  {
+            // Ensure we don't show the final dialog again in context when the user sees it and vice-versa.
+            settings.browsingFinalDialogShown = true
             return .final
         }
 

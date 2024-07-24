e89f6743a05eb208e37f5a8924f2e8438c27c1eb
Dominik Kapusta <dkapusta@duckduckgo.com>
2024-07-24 17:51:24 +0200

    Fix onDidAppear in HomeMessageViewSectionRenderer

--- DuckDuckGo/HomeMessageViewSectionRenderer.swift
+++ DuckDuckGo/HomeMessageViewSectionRenderer.swift
@@ -122,11 +122,11 @@ class HomeMessageViewSectionRenderer: NSObject, HomeViewSectionRenderer {
             let onDidAppear = { [weak self] in
                 self?.homePageConfiguration.didAppear(message)
             }
-            if controller?.collectionView.window != nil {
-                // call didAppear here to support marking messages as shown when they appear on the new tab page
-                // as a result of refreshing a config while the user was on a new tab page already.
-                onDidAppear()
-            }
+
+            // call didAppear here to support marking messages as shown when they appear on the new tab page
+            // as a result of refreshing a config while the user was on a new tab page already.
+            onDidAppear()
+
             return HomeMessageViewModelBuilder.build(for: remoteMessage, with: privacyProDataReporter) { [weak self] action in
 
                 guard let action,

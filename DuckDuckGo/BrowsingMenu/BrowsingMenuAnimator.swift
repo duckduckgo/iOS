b95e963829e7bf4beb8a87cbcfd94bb8fc3b43c4
Mariusz Śpiewak <mspiewak@duckduckgo.com>
2024-08-02 16:30:48 +0200

    Fix dismissing menu state for New Tab Page

--- DuckDuckGo/BrowsingMenu/BrowsingMenuAnimator.swift
+++ DuckDuckGo/BrowsingMenu/BrowsingMenuAnimator.swift
@@ -106,7 +106,7 @@ final class BrowsingMenuAnimator: NSObject, UIViewControllerAnimatedTransitionin
 
         fromViewController.view.isHidden = true
 
-        if toViewController.homeController != nil {
+        if toViewController.homeViewController != nil {
             toViewController.presentedMenuButton.setState(.bookmarksImage, animated: true)
         } else {
             toViewController.presentedMenuButton.setState(.menuImage, animated: true)

5f3969e5e40790d6d5df68c551dbb228d36475ba
Graeme Arthur <garthur@duckduckgo.com>
2024-08-05 16:08:11 +0200

    Increase icon size

--- DuckDuckGo/AutofillViews.swift
+++ DuckDuckGo/AutofillViews.swift
@@ -58,8 +58,9 @@ struct AutofillViews {
 
     struct AppIconHeader: View {
         var body: some View {
-            Image.appIcon
-                .scaledToFit()
+            Image(.appDDG32)
+                .resizable()
+                .frame(width: 48, height: 48)
         }
     }
 

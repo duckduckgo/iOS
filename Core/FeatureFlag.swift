13b000616ae138ffc186f04f4556a270935a8ede
Jacek Łyp <jlyp@duckduckgo.com>
2024-08-29 22:33:49 +0200

    Handle SSL errors gracefully (#3271)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1207295365843956/f
    
    If users visit a page with certificate or other security issues, provide
    warning information and allow users to proceed (if they choose to).

--- Core/FeatureFlag.swift
+++ Core/FeatureFlag.swift
@@ -36,6 +36,7 @@ public enum FeatureFlag: String {
     case history
     case newTabPageSections
     case duckPlayer
+    case sslCertificatesBypass
 }
 
 extension FeatureFlag: FeatureFlagSourceProviding {
@@ -71,6 +72,8 @@ extension FeatureFlag: FeatureFlagSourceProviding {
             return .internalOnly
         case .duckPlayer:
             return .remoteReleasable(.feature(.duckPlayer))
+        case .sslCertificatesBypass:
+            return .remoteReleasable(.subfeature(sslCertificatesSubfeature.allowBypass))
         }
     }
 }

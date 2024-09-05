b85f4a8b2b2b30a483018e76dd2f16d30bc02b1f
amddg44 <amallon@duckduckgo.com>
2024-09-05 13:47:56 +0200

    AutofillHeaderViewFactoryTests added

--- DuckDuckGoTests/AutofillSurveyManagerTests.swift
+++ DuckDuckGoTests/AutofillSurveyManagerTests.swift
@@ -40,7 +40,6 @@ final class AutofillSurveyManagerTests: XCTestCase {
         try super.tearDownWithError()
     }
 
-
     func testSurveyToPresentReturnsCorrectSurvey() {
         let settings: [String: Any] = [
             "surveys": [

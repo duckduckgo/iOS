11b42cba07f5a26c3a0c02e19e257705bfaf3fbc
Sam Symons <sam@samsymons.com>
2024-09-11 16:11:32 -0700

    Add pixel persistence.

---
+++ Core/PersistentPixelStoring.swift
@@ -0,0 +1,102 @@
+//
+//  PersistentPixelStoring.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+
+struct PersistentPixelMetadata: Codable, Equatable {
+    let pixelName: String
+    let parameters: [String: String]
+}
+
+protocol PersistentPixelStoring {
+    func append(pixel: PersistentPixelMetadata) throws
+    func replaceStoredPixels(with pixels: [PersistentPixelMetadata]) throws
+    func storedPixels() throws -> [PersistentPixelMetadata]
+}
+
+final class DefaultPersistentPixelStorage: PersistentPixelStoring {
+
+    private enum Constants {
+        static let queuedPixelsFileName = "queued-pixels.json"
+    }
+
+    let fileManager: FileManager
+    let fileName: String
+    let storageDirectory: URL
+
+    private let fileAccessQueue = DispatchQueue(label: "Persistent Pixel File Access Queue", qos: .utility)
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+
+    private var fileURL: URL {
+        return storageDirectory.appendingPathComponent(fileName)
+    }
+
+    public init(fileManager: FileManager = .default,
+                fileName: String = Constants.queuedPixelsFileName,
+                storageDirectory: URL? = nil) {
+        self.fileManager = fileManager
+        self.fileName = fileName
+
+        if let storageDirectory = storageDirectory {
+            self.storageDirectory = storageDirectory
+        } else if let appSupportDirectory = FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask).first {
+            self.storageDirectory = appSupportDirectory
+        } else {
+            fatalError("Unable to locate application support directory")
+        }
+    }
+
+    func append(pixel: PersistentPixelMetadata) throws {
+        try fileAccessQueue.sync {
+            var pixels = try self.readStoredPixelDataFromFileSystem()
+            pixels.append(pixel)
+            try writePixelDataToFileSystem(pixels: pixels)
+        }
+    }
+
+    func replaceStoredPixels(with pixels: [PersistentPixelMetadata]) throws {
+        try fileAccessQueue.sync {
+            try writePixelDataToFileSystem(pixels: pixels)
+        }
+    }
+
+    func storedPixels() throws -> [PersistentPixelMetadata] {
+        try fileAccessQueue.sync {
+            return try readStoredPixelDataFromFileSystem()
+        }
+    }
+
+    // MARK: - Private
+
+    private func readStoredPixelDataFromFileSystem() throws -> [PersistentPixelMetadata] {
+        guard fileManager.fileExists(atPath: fileURL.path) else {
+            return []
+        }
+
+        let pixelFileData = try Data(contentsOf: fileURL)
+        return try decoder.decode([PersistentPixelMetadata].self, from: pixelFileData)
+    }
+
+    private func writePixelDataToFileSystem(pixels: [PersistentPixelMetadata]) throws {
+        let encodedPixelData = try encoder.encode(pixels)
+        try encodedPixelData.write(to: fileURL)
+    }
+
+}
---
+++ DuckDuckGoTests/DefaultPersistentPixelStorageTests.swift
@@ -0,0 +1,92 @@
+//
+//  DefaultPersistentPixelStorageTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import XCTest
+@testable import Core
+
+class DefaultPersistentPixelStorageTests: XCTestCase {
+
+    var currentStorageURL: URL!
+    var persistentStorage: DefaultPersistentPixelStorage!
+
+    override func setUp() {
+        super.setUp()
+        let (url, storage) = createPersistentStorage()
+        self.currentStorageURL = url
+        self.persistentStorage = storage
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        try? FileManager.default.removeItem(at: currentStorageURL)
+    }
+
+    func testWhenStoringPixel_ThenPixelCanBeSuccessfullyRead() throws {
+        let metadata = PersistentPixelMetadata(pixelName: "test", parameters: ["param": "value"])
+        try persistentStorage.append(pixel: metadata)
+        let readPixelMetadata = try persistentStorage.storedPixels()
+
+        XCTAssertEqual([metadata], readPixelMetadata)
+    }
+
+    func testWhenStoringMultiplePixels_ThenPixelsCanBeSuccessfullyRead() throws {
+        let metadata1 = PersistentPixelMetadata(pixelName: "test1", parameters: ["param1": "value1"])
+        let metadata2 = PersistentPixelMetadata(pixelName: "test2", parameters: ["param2": "value2"])
+        let metadata3 = PersistentPixelMetadata(pixelName: "test3", parameters: ["param3": "value3"])
+
+        try persistentStorage.append(pixel: metadata1)
+        try persistentStorage.append(pixel: metadata2)
+        try persistentStorage.append(pixel: metadata3)
+
+        let readPixelMetadata = try persistentStorage.storedPixels()
+
+        XCTAssertEqual([metadata1, metadata2, metadata3], readPixelMetadata)
+    }
+
+    func testWhenReplacingPixels_AndNoPixelsAreStored_ThenNewPixelsAreStored() throws {
+        let metadata = PersistentPixelMetadata(pixelName: "test", parameters: ["param": "value"])
+        try persistentStorage.replaceStoredPixels(with: [metadata])
+        let readPixelMetadata = try persistentStorage.storedPixels()
+
+        XCTAssertEqual([metadata], readPixelMetadata)
+    }
+
+    func testWhenReplacingPixels_AndExistingPixelsAreStored_ThenOldPixelsAreReplacedWithNewOnes() throws {
+        let initialMetadata = PersistentPixelMetadata(pixelName: "old", parameters: ["param1": "value1"])
+        try persistentStorage.replaceStoredPixels(with: [initialMetadata])
+
+        let newMetadata = PersistentPixelMetadata(pixelName: "new", parameters: ["param2": "value2"])
+        try persistentStorage.replaceStoredPixels(with: [newMetadata])
+
+        let readPixelMetadata = try persistentStorage.storedPixels()
+        XCTAssertEqual([newMetadata], readPixelMetadata)
+    }
+
+    private func createPersistentStorage() -> (URL, DefaultPersistentPixelStorage) {
+        let storageDirectory = FileManager.default.temporaryDirectory
+        let fileName = UUID().uuidString.appendingPathExtension("json")
+
+        return (
+            storageDirectory.appendingPathComponent(fileName),
+            DefaultPersistentPixelStorage(fileName: fileName, storageDirectory: storageDirectory)
+        )
+    }
+
+}

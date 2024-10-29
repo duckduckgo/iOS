fb6a53f6af64588b7696e82fed11837180da79c5
Alessandro Boron <alessandro.boron@gmail.com>
2024-10-29 11:34:08 +0100

    Add to Dock Onboarding feature flag (#3476)
    Task/Issue URL: https://app.asana.com/0/1206329551987282/1208577512136639/f
    
    **Description**:
    Add a feature flag used to show Add to Dock during the onboarding flow.
    Note that the feature will be available only for iPhone

--- DuckDuckGo/OnboardingExperiment/Manager/OnboardingManager.swift
+++ DuckDuckGo/OnboardingExperiment/Manager/OnboardingManager.swift
@@ -20,35 +20,44 @@
 import BrowserServicesKit
 import Core
 
+final class OnboardingManager {
+    private var appDefaults: AppDebugSettings
+    private let featureFlagger: FeatureFlagger
+    private let variantManager: VariantManager
+    private let isIphone: Bool
+
+    init(
+        appDefaults: AppDebugSettings = AppDependencyProvider.shared.appSettings,
+        featureFlagger: FeatureFlagger = AppDependencyProvider.shared.featureFlagger,
+        variantManager: VariantManager = DefaultVariantManager(),
+        isIphone: Bool = UIDevice.current.userInterfaceIdiom == .phone
+    ) {
+        self.appDefaults = appDefaults
+        self.featureFlagger = featureFlagger
+        self.variantManager = variantManager
+        self.isIphone = isIphone
+    }
+}
+
+// MARK: - Onboarding Highlights
+
 protocol OnboardingHighlightsManaging: AnyObject {
     var isOnboardingHighlightsEnabled: Bool { get }
 }
 
 protocol OnboardingHighlightsDebugging: OnboardingHighlightsManaging {
-    var isLocalFlagEnabled: Bool { get set }
-    var isFeatureFlagEnabled: Bool { get }
+    var isOnboardingHighlightsLocalFlagEnabled: Bool { get set }
+    var isOnboardingHighlightsFeatureFlagEnabled: Bool { get }
 }
 
-final class OnboardingManager: OnboardingHighlightsManaging, OnboardingHighlightsDebugging {
-    private var appDefaults: AppDebugSettings
-    private let featureFlagger: FeatureFlagger
-    private let variantManager: VariantManager
 
-    init(
-        appDefaults: AppDebugSettings = AppDependencyProvider.shared.appSettings,
-        featureFlagger: FeatureFlagger = AppDependencyProvider.shared.featureFlagger,
-        variantManager: VariantManager = DefaultVariantManager()
-    ) {
-        self.appDefaults = appDefaults
-        self.featureFlagger = featureFlagger
-        self.variantManager = variantManager
-    }
+extension OnboardingManager: OnboardingHighlightsManaging, OnboardingHighlightsDebugging {
 
     var isOnboardingHighlightsEnabled: Bool {
-        variantManager.isOnboardingHighlightsExperiment || (isLocalFlagEnabled && isFeatureFlagEnabled)
+        variantManager.isOnboardingHighlightsExperiment || (isOnboardingHighlightsLocalFlagEnabled && isOnboardingHighlightsFeatureFlagEnabled)
     }
 
-    var isLocalFlagEnabled: Bool {
+    var isOnboardingHighlightsLocalFlagEnabled: Bool {
         get {
             appDefaults.onboardingHighlightsEnabled
         }
@@ -57,7 +66,41 @@ final class OnboardingManager: OnboardingHighlightsManaging, OnboardingHighlight
         }
     }
 
-    var isFeatureFlagEnabled: Bool {
+    var isOnboardingHighlightsFeatureFlagEnabled: Bool {
         featureFlagger.isFeatureOn(.onboardingHighlights)
     }
+
+}
+
+// MARK: - Add to Dock
+
+protocol OnboardingAddToDockManaging: AnyObject {
+    var isAddToDockEnabled: Bool { get }
+}
+
+protocol OnboardingAddToDockDebugging: AnyObject {
+    var isAddToDockLocalFlagEnabled: Bool { get set }
+    var isAddToDockFeatureFlagEnabled: Bool { get }
+}
+
+extension OnboardingManager: OnboardingAddToDockManaging, OnboardingAddToDockDebugging {
+
+    var isAddToDockEnabled: Bool {
+        // TODO: Add variant condition once the experiment is setup
+        isIphone && isAddToDockLocalFlagEnabled && isAddToDockFeatureFlagEnabled
+    }
+
+    var isAddToDockLocalFlagEnabled: Bool {
+        get {
+            appDefaults.onboardingAddToDockEnabled
+        }
+        set {
+            appDefaults.onboardingAddToDockEnabled = newValue
+        }
+    }
+
+    var isAddToDockFeatureFlagEnabled: Bool {
+        featureFlagger.isFeatureOn(.onboardingAddToDock)
+    }
+
 }

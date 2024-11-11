16d3078e763706122e2e393f27835d1b5fcb5004
Chris Brind <brindy@duckduckgo.com>
2024-11-11 17:36:53 +0000

    fix problem where if item is disabled then footer text would still be visible for both items

--- DuckDuckGo/SettingsAccessibilityView.swift
+++ DuckDuckGo/SettingsAccessibilityView.swift
@@ -28,33 +28,33 @@ struct SettingsAccessibilityView: View {
 
     var body: some View {
         List {
-            Section(footer: Text(UserText.textSizeDescription)) {
-                // Text Size
-                if viewModel.state.textSize.enabled {
+            if viewModel.state.textSize.enabled {
+                Section(footer: Text(UserText.textSizeDescription)) {
+                    // Text Size
                     SettingsPickerCellView(label: UserText.settingsText,
                                            options: TextZoomLevel.allCases,
                                            selectedOption: viewModel.textZoomLevelBinding)
                 }
             }
 
-            Section(footer: Text(UserText.voiceSearchFooter)) {
-                // Private Voice Search
-                if viewModel.state.speechRecognitionAvailable {
+            if viewModel.state.speechRecognitionAvailable {
+                Section(footer: Text(UserText.voiceSearchFooter)) {
+                    // Private Voice Search
                     SettingsCellView(label: UserText.settingsVoiceSearch,
                                      accessory: .toggle(isOn: viewModel.voiceSearchEnabledBinding))
                 }
-            }
-            .alert(isPresented: $shouldShowNoMicrophonePermissionAlert) {
-                Alert(title: Text(UserText.noVoicePermissionAlertTitle),
-                      message: Text(UserText.noVoicePermissionAlertMessage),
-                      dismissButton: .default(Text(UserText.noVoicePermissionAlertOKbutton),
-                      action: {
-                        viewModel.shouldShowNoMicrophonePermissionAlert = false
-                    })
-                )
-            }
-            .onChange(of: viewModel.shouldShowNoMicrophonePermissionAlert) { value in
-                shouldShowNoMicrophonePermissionAlert = value
+                .alert(isPresented: $shouldShowNoMicrophonePermissionAlert) {
+                    Alert(title: Text(UserText.noVoicePermissionAlertTitle),
+                          message: Text(UserText.noVoicePermissionAlertMessage),
+                          dismissButton: .default(Text(UserText.noVoicePermissionAlertOKbutton),
+                          action: {
+                            viewModel.shouldShowNoMicrophonePermissionAlert = false
+                        })
+                    )
+                }
+                .onChange(of: viewModel.shouldShowNoMicrophonePermissionAlert) { value in
+                    shouldShowNoMicrophonePermissionAlert = value
+                }
             }
         }
         .applySettingsListModifiers(title: UserText.accessibility,

duckduckgo/iOS

commit 5879142d2e004cc71bc26781ef71778f24c38f93

Add possibility to stop page loading (#3553)
Task/Issue URL: https://app.asana.com/0/1206226850447395/1206267785501484/f
Tech Design URL:
CC:

**Description**:

Adds an additional OmniBar state variation when the website is loading,
allowing to stop the loading process.

**Steps to test this PR**:
💡 It's helpful to test on a physical device (with link conditioner
enabled to slow down loading) or using [link
conditioner](https://nshipster.com/network-link-conditioner/) for
simulator.

1. Start loading a website on 2 or more tabs.
2. Switch between them and observe if state is shown properly. (swiping
on address bar is helpful to do it quickly).
3. Stop loading. 
4. Verify omni bar state is valid (refresh is visible) and:
    a. if no valid resources were loaded error page should be presented
b. if some resources were loaded and can be displayed, partially loaded
website should be visible
    c. pixel is sent.

<!--
Before submitting a PR, please ensure you have tested the combinations
you expect the reviewer to test, then delete configurations you *know*
do not need explicit testing.

Using a simulator where a physical device is unavailable is acceptable.
-->

**Definition of Done (Internal Only)**:

* [x] Does this PR satisfy our [Definition of
Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?

**Orientation Testing**:

* [x] Portrait
* [x] Landscape

**Device Testing**:

* [ ] iPhone SE (1st Gen)
* [ ] iPhone 8
* [ ] iPhone X
* [x] iPhone 15 Pro
* [x] iPad

**OS Testing**:

* [x] iOS 15
* [ ] iOS 16
* [x] iOS 18

**Theme Testing**:

* [x] Light theme
* [x] Dark theme

---
###### Internal references:
[Software Engineering
Expectations](https://app.asana.com/0/59792373528535/199064865822552)
[Technical Design
Template](https://app.asana.com/0/59792373528535/184709971311943)

DuckDuckGo.xcodeproj/project.pbxproj
@@ -299,6 +299,7 @@
 		6F04224D2CD2A3AD00729FA6 /* StorageInconsistencyMonitor.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F98573F2CD2933B001BE9A0 /* StorageInconsistencyMonitor.swift */; };
 		6F0FEF6B2C516D540090CDE4 /* NewTabPageSettingsStorage.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F0FEF6A2C516D540090CDE4 /* NewTabPageSettingsStorage.swift */; };
 		6F0FEF6D2C52639E0090CDE4 /* ReorderableForEach.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F0FEF6C2C52639E0090CDE4 /* ReorderableForEach.swift */; };
+		6F3529FF2CDCEDFF00A59170 /* OmniBarLoadingStateBearerTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F3529FE2CDCEDF700A59170 /* OmniBarLoadingStateBearerTests.swift */; };
 		6F35379E2C4AAF2E009F8717 /* NewTabPageSettingsView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F35379D2C4AAF2E009F8717 /* NewTabPageSettingsView.swift */; };
 		6F3537A02C4AAFD2009F8717 /* NewTabPageSettingsSectionItemView.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F35379F2C4AAFD2009F8717 /* NewTabPageSettingsSectionItemView.swift */; };
 		6F3537A22C4AB97A009F8717 /* NewTabPageSettingsModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F3537A12C4AB97A009F8717 /* NewTabPageSettingsModel.swift */; };
@@ -1599,6 +1600,7 @@
 		6F03CB082C32F331004179A8 /* PixelFiringAsync.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = PixelFiringAsync.swift; sourceTree = "<group>"; };
 		6F0FEF6A2C516D540090CDE4 /* NewTabPageSettingsStorage.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageSettingsStorage.swift; sourceTree = "<group>"; };
 		6F0FEF6C2C52639E0090CDE4 /* ReorderableForEach.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ReorderableForEach.swift; sourceTree = "<group>"; };
+		6F3529FE2CDCEDF700A59170 /* OmniBarLoadingStateBearerTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OmniBarLoadingStateBearerTests.swift; sourceTree = "<group>"; };
 		6F35379D2C4AAF2E009F8717 /* NewTabPageSettingsView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageSettingsView.swift; sourceTree = "<group>"; };
 		6F35379F2C4AAFD2009F8717 /* NewTabPageSettingsSectionItemView.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageSettingsSectionItemView.swift; sourceTree = "<group>"; };
 		6F3537A12C4AB97A009F8717 /* NewTabPageSettingsModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageSettingsModel.swift; sourceTree = "<group>"; };
@@ -6309,6 +6311,7 @@
 		F1D477C71F2139210031ED49 /* OmniBar */ = {
 			isa = PBXGroup;
 			children = (
+				6F3529FE2CDCEDF700A59170 /* OmniBarLoadingStateBearerTests.swift */,
 				BBFF18B02C76448100C48D7D /* QuerySubmittedTests.swift */,
 				8588026424E4209900C24AB6 /* LargeOmniBarStateTests.swift */,
 				85F20005221702F7006BB258 /* AddressDisplayHelperTests.swift */,
@@ -8107,6 +8110,7 @@
 				9F4CC51D2C48D240006A96EB /* CoreDataDatabaseTestUtilities.swift in Sources */,
 				C185ED672BD43DA100BAE9DC /* ImportPasswordsStatusHandlerTests.swift in Sources */,
 				F198D7981E3A45D90088DA8A /* WKWebViewConfigurationExtensionTests.swift in Sources */,
+				6F3529FF2CDCEDFF00A59170 /* OmniBarLoadingStateBearerTests.swift in Sources */,
 				564DE45E2C45218500D23241 /* OnboardingNavigationDelegateTests.swift in Sources */,
 				C14E2F7729DE14EA002AC515 /* AutofillInterfaceUsernameTruncatorTests.swift in Sources */,
 				9F7CFF782C86E3E10012833E /* OnboardingManagerTests.swift in Sources */,

DuckDuckGo/OmniBar.swift
@@ -50,7 +50,8 @@ class OmniBar: UIView {
     @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var voiceSearchButton: UIButton!
-    
+    @IBOutlet weak var abortButton: UIButton!
+
     @IBOutlet weak var bookmarksButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var forwardButton: UIButton!
@@ -80,7 +81,7 @@ class OmniBar: UIView {
 
     static func loadFromXib(voiceSearchHelper: VoiceSearchHelperProtocol) -> OmniBar {
         let omniBar = OmniBar.load(nibName: "OmniBar")
-        omniBar.state = SmallOmniBarState.HomeNonEditingState(voiceSearchHelper: voiceSearchHelper)
+        omniBar.state = SmallOmniBarState.HomeNonEditingState(voiceSearchHelper: voiceSearchHelper, isLoading: false)
         omniBar.refreshState(omniBar.state)
 
         return omniBar
@@ -92,7 +93,7 @@ class OmniBar: UIView {
 
     // Tests require this
     init(voiceSearchHelper: VoiceSearchHelperProtocol, frame: CGRect) {
-        self.state = SmallOmniBarState.HomeNonEditingState(voiceSearchHelper: voiceSearchHelper)
+        self.state = SmallOmniBarState.HomeNonEditingState(voiceSearchHelper: voiceSearchHelper, isLoading: false)
         super.init(frame: frame)
     }
 
@@ -249,6 +250,14 @@ class OmniBar: UIView {
         refreshState(state.onBrowsingStoppedState)
     }
 
+    func startLoading() {
+        refreshState(state.withLoading())
+    }
+
+    func stopLoading() {
+        refreshState(state.withoutLoading())
+    }
+
     func removeTextSelection() {
         textField.selectedTextRange = nil
     }
@@ -350,9 +359,9 @@ class OmniBar: UIView {
         textField.selectedTextRange = textField.textRange(from: fromPosition, to: textField.endOfDocument)
     }
 
-    fileprivate func refreshState(_ newState: OmniBarState) {
-        if state.name != newState.name {
-            Logger.general.debug("OmniBar entering \(newState.name) from \(self.state.name)")
+    fileprivate func refreshState(_ newState: any OmniBarState) {
+        if !newState.isEquivalent(to: state) {
+            Logger.general.debug("OmniBar entering \(newState.description) from \(self.state.description)")
             if newState.clearTextOnStart {
                 clear()
             }
@@ -370,6 +379,7 @@ class OmniBar: UIView {
         setVisibility(cancelButton, hidden: !state.showCancel)
         setVisibility(refreshButton, hidden: !state.showRefresh)
         setVisibility(voiceSearchButton, hidden: !state.showVoiceSearch)
+        setVisibility(abortButton, hidden: !state.showAbort)
 
         setVisibility(backButton, hidden: !state.showBackButton)
         setVisibility(forwardButton, hidden: !state.showForwardButton)
@@ -462,7 +472,11 @@ class OmniBar: UIView {
     @IBAction func onVoiceSearchButtonPressed(_ sender: UIButton) {
         omniDelegate?.onVoiceSearchPressed()
     }
-    
+
+    @IBAction func onAbortButtonPressed(_ sender: Any) {
+        omniDelegate?.onAbortPressed()
+    }
+
     @IBAction func onClearButtonPressed(_ sender: Any) {
         omniDelegate?.onClearPressed()
         refreshState(state.onTextClearedState)

DuckDuckGo/OmniBarState.swift
@@ -20,8 +20,10 @@
 import Foundation
 import Core
 
-protocol OmniBarState {
-    
+protocol OmniBarState: CustomStringConvertible {
+
+    var name: String { get }
+
     var hasLargeWidth: Bool { get }
     var showBackButton: Bool { get }
     var showForwardButton: Bool { get }
@@ -39,7 +41,8 @@ protocol OmniBarState {
     var showMenu: Bool { get }
     var showSettings: Bool { get }
     var showVoiceSearch: Bool { get }
-    var name: String { get }
+    var showAbort: Bool { get }
+
     var onEditingStoppedState: OmniBarState { get }
     var onEditingSuspendedState: OmniBarState { get }
     var onEditingStartedState: OmniBarState { get }
@@ -50,10 +53,43 @@ protocol OmniBarState {
     var onEnterPhoneState: OmniBarState { get }
     var onEnterPadState: OmniBarState { get }
     var onReloadState: OmniBarState { get }
+
+    var voiceSearchHelper: VoiceSearchHelperProtocol { get }
+
+    var isLoading: Bool { get }
+
+    func withLoading() -> Self
+    func withoutLoading() -> Self
+
+    func isEquivalent(to other: OmniBarState) -> Bool
 }
 
 extension OmniBarState {
+    func isEquivalent(to other: OmniBarState) -> Bool {
+        name == other.name && isLoading == other.isLoading
+    }
+
+    var description: String {
+        "\(name)\(isLoading ? " (loading)" : "")"
+    }
+
     var onEditingSuspendedState: OmniBarState {
-        UniversalOmniBarState.EditingSuspendedState(baseState: self.onEditingStartedState)
+        UniversalOmniBarState.EditingSuspendedState(baseState: onEditingStartedState,
+                                                    voiceSearchHelper: voiceSearchHelper,
+                                                    isLoading: isLoading)
+    }
+}
+
+protocol OmniBarLoadingBearerStateCreating {
+    init(voiceSearchHelper: VoiceSearchHelperProtocol, isLoading: Bool)
+}
+
+extension OmniBarLoadingBearerStateCreating where Self: OmniBarState {
+    func withLoading() -> Self {
+        Self.init(voiceSearchHelper: voiceSearchHelper, isLoading: true)
+    }
+
+    func withoutLoading() -> Self {
+        Self.init(voiceSearchHelper: voiceSearchHelper, isLoading: false)
     }
 }

Core/PixelEvent.swift
@@ -852,6 +852,9 @@ extension Pixel {
         case protectedDataUnavailableWhenBecomeActive
         case statisticsLoaderATBStateMismatch
         case adAttributionReportStateMismatch
+
+        // MARK: Browsing
+        case stopPageLoad
         
         // MARK: - DuckPlayer Overlay Navigation
         case duckPlayerYouTubeOverlayNavigationBack
@@ -1702,6 +1705,9 @@ extension Pixel.Event {
         case .protectedDataUnavailableWhenBecomeActive: return "m_protected_data_unavailable_when_become_active"
         case .statisticsLoaderATBStateMismatch: return "m_statistics_loader_atb_state_mismatch"
         case .adAttributionReportStateMismatch: return "m_ad_attribution_report_state_mismatch"
+
+        // MARK: Browsing
+        case .stopPageLoad: return "m_stop-page-load"
                         
         // MARK: - DuckPlayer Overlay Navigation
         case .duckPlayerYouTubeOverlayNavigationBack: return "duckplayer.youtube.overlay.navigation.back"
@@ -1710,8 +1716,7 @@ extension Pixel.Event {
         case .duckPlayerYouTubeOverlayNavigationOutsideYoutube: return "duckplayer.youtube.overlay.navigation.outside-youtube"
         case .duckPlayerYouTubeOverlayNavigationClosed: return "duckplayer.youtube.overlay.navigation.closed"
         case .duckPlayerYouTubeNavigationIdle30: return "duckplayer.youtube.overlay.idle-30"
-            
-            
+
         }
     }
 }

commit f7be389502d8613c7b9dcf752202948cf472cf59

Fix invalid OmniBar state transition on loading  (#3599)
Task/Issue URL:
https://app.asana.com/0/1206226850447395/1208790263070149/f
Tech Design URL:
CC:

**Description**:

Caused by https://github.com/duckduckgo/iOS/pull/3553.
Unnecessary text field clean was performed when `OmniBarState` changed
the internal value of `isLoading` without changing the state kind. This
caused suggestion tray controller with favorites to appear.

**Steps to test this PR**:
1. Turn off automatic data clearing on app exit.
2. Add at least one favorite.
3. Open tab with some url address.
4. Relaunch the app.
5. Verify suggestions tray is not shown and favorites are not visible
while the tab is loading.

**Definition of Done (Internal Only)**:

* [ ] Does this PR satisfy our [Definition of
Done](https://app.asana.com/0/1202500774821704/1207634633537039/f)?

**Orientation Testing**:

* [ ] Portrait
* [ ] Landscape

**Device Testing**:

* [ ] iPhone SE (1st Gen)
* [ ] iPhone 8
* [ ] iPhone X
* [ ] iPhone 14 Pro
* [ ] iPad

---
###### Internal references:
[Software Engineering
Expectations](https://app.asana.com/0/59792373528535/199064865822552)
[Technical Design
Template](https://app.asana.com/0/59792373528535/184709971311943)

DuckDuckGo.xcodeproj/project.pbxproj
@@ -315,6 +315,7 @@
 		6F64AA5F2C49463C00CF4489 /* ShortcutsModel.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F64AA5E2C49463C00CF4489 /* ShortcutsModel.swift */; };
 		6F655BE22BAB289E00AC3597 /* DefaultTheme.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F655BE12BAB289E00AC3597 /* DefaultTheme.swift */; };
 		6F691CCA2C4979EC002E9553 /* FavoritesTooltip.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F691CC92C4979EC002E9553 /* FavoritesTooltip.swift */; };
+		6F7BACD42CEE084B00F561D8 /* OmniBarEqualityCheckTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F7BACD32CEE084100F561D8 /* OmniBarEqualityCheckTests.swift */; };
 		6F7FB8E12C660B3E00867DA7 /* NewTabPageFavoritesModelTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F7FB8DF2C660B1A00867DA7 /* NewTabPageFavoritesModelTests.swift */; };
 		6F7FB8E32C660BF300867DA7 /* DailyPixelFiring.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F7FB8E22C660BF300867DA7 /* DailyPixelFiring.swift */; };
 		6F7FB8E52C66158D00867DA7 /* NewTabPageShortcutsSettingsModelTests.swift in Sources */ = {isa = PBXBuildFile; fileRef = 6F7FB8E42C66158D00867DA7 /* NewTabPageShortcutsSettingsModelTests.swift */; };
@@ -1623,6 +1624,7 @@
 		6F64AA5E2C49463C00CF4489 /* ShortcutsModel.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = ShortcutsModel.swift; sourceTree = "<group>"; };
 		6F655BE12BAB289E00AC3597 /* DefaultTheme.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DefaultTheme.swift; sourceTree = "<group>"; };
 		6F691CC92C4979EC002E9553 /* FavoritesTooltip.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = FavoritesTooltip.swift; sourceTree = "<group>"; };
+		6F7BACD32CEE084100F561D8 /* OmniBarEqualityCheckTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = OmniBarEqualityCheckTests.swift; sourceTree = "<group>"; };
 		6F7FB8DF2C660B1A00867DA7 /* NewTabPageFavoritesModelTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageFavoritesModelTests.swift; sourceTree = "<group>"; };
 		6F7FB8E22C660BF300867DA7 /* DailyPixelFiring.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = DailyPixelFiring.swift; sourceTree = "<group>"; };
 		6F7FB8E42C66158D00867DA7 /* NewTabPageShortcutsSettingsModelTests.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = NewTabPageShortcutsSettingsModelTests.swift; sourceTree = "<group>"; };
@@ -6348,6 +6350,7 @@
 			isa = PBXGroup;
 			children = (
 				6F3529FE2CDCEDF700A59170 /* OmniBarLoadingStateBearerTests.swift */,
+				6F7BACD32CEE084100F561D8 /* OmniBarEqualityCheckTests.swift */,
 				BBFF18B02C76448100C48D7D /* QuerySubmittedTests.swift */,
 				8588026424E4209900C24AB6 /* LargeOmniBarStateTests.swift */,
 				85F20005221702F7006BB258 /* AddressDisplayHelperTests.swift */,
@@ -8090,6 +8093,7 @@
 				85D2187224BF24F2004373D2 /* NotFoundCachingDownloaderTests.swift in Sources */,
 				C1935A242C89CC6D001AD72D /* AutofillHeaderViewFactoryTests.swift in Sources */,
 				C111B26927F579EF006558B1 /* BookmarkOrFolderTests.swift in Sources */,
+				6F7BACD42CEE084B00F561D8 /* OmniBarEqualityCheckTests.swift in Sources */,
 				6F7FB8E72C66197E00867DA7 /* NewTabPageSectionsSettingsModelTests.swift in Sources */,
 				851CD674244D7E6000331B98 /* UserDefaultsExtension.swift in Sources */,
 				569437362BE5160600C0881B /* SyncSettingsViewControllerErrorTests.swift in Sources */,

DuckDuckGo/OmniBar.swift
@@ -360,15 +360,19 @@ class OmniBar: UIView {
     }
 
     fileprivate func refreshState(_ newState: any OmniBarState) {
-        if !newState.isEquivalent(to: state) {
+        if state.requiresUpdate(transitioningInto: newState) {
             Logger.general.debug("OmniBar entering \(newState.description) from \(self.state.description)")
-            if newState.clearTextOnStart {
-                clear()
+
+            if state.isDifferentState(than: newState) {
+                if newState.clearTextOnStart {
+                    clear()
+                }
+                cancelAllAnimations()
             }
+
             state = newState
-            cancelAllAnimations()
         }
-        
+
         searchFieldContainer.adjustTextFieldOffset(for: state)
         
         setVisibility(privacyInfoContainer, hidden: !state.showPrivacyIcon)

DuckDuckGo/OmniBarState.swift
@@ -29,7 +29,7 @@ protocol OmniBarState: CustomStringConvertible {
     var showForwardButton: Bool { get }
     var showBookmarksButton: Bool { get }
     var showShareButton: Bool { get }
-    
+
     var clearTextOnStart: Bool { get }
     var allowsTrackersAnimation: Bool { get }
     var showSearchLoupe: Bool { get }
@@ -61,12 +61,20 @@ protocol OmniBarState: CustomStringConvertible {
     func withLoading() -> Self
     func withoutLoading() -> Self
 
-    func isEquivalent(to other: OmniBarState) -> Bool
+    func requiresUpdate(transitioningInto other: OmniBarState) -> Bool
+    func isDifferentState(than other: OmniBarState) -> Bool
 }
 
 extension OmniBarState {
-    func isEquivalent(to other: OmniBarState) -> Bool {
-        name == other.name && isLoading == other.isLoading
+    /// Returns if new state requires UI update
+    func requiresUpdate(transitioningInto other: OmniBarState) -> Bool {
+        name != other.name || isLoading != other.isLoading
+    }
+
+    /// Checks whether the state type is different.
+    /// If `true` it may require transitioning to a different appearance and/or cancelling pending animations.
+    func isDifferentState(than other: OmniBarState) -> Bool {
+        name != other.name
     }
 
     var description: String {

DuckDuckGoTests/OmniBarEqualityCheckTests.swift
@@ -1,0 +1,113 @@
+//
+//  OmniBarEqualityCheckTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+@testable import DuckDuckGo
+
+final class OmniBarEqualityCheckTests: XCTestCase {
+    func testRequiresUpdateChecksForIsLoading() {
+        let loadingOmniBarState = DummyOmniBarState(isLoading: true)
+        let notLoadingOmniBarState = DummyOmniBarState(isLoading: false)
+
+        XCTAssertTrue(loadingOmniBarState.requiresUpdate(transitioningInto: notLoadingOmniBarState))
+    }
+
+    func testRequiresUpdateChecksForName() {
+        let fooOmniBarState = DummyOmniBarState(name: "foo")
+        let barOmniBarState = DummyOmniBarState(name: "bar")
+
+        XCTAssertTrue(fooOmniBarState.requiresUpdate(transitioningInto: barOmniBarState))
+    }
+
+    func testIsDifferentStateChecksForName() {
+        let fooOmniBarState = DummyOmniBarState(name: "foo")
+        let barOmniBarState = DummyOmniBarState(name: "bar")
+
+        XCTAssertTrue(fooOmniBarState.isDifferentState(than: barOmniBarState))
+    }
+
+    func testIsDifferentStateIgnoresOtherProperties() {
+        let fooOmniBarState = DummyOmniBarState()
+        var barOmniBarState = DummyOmniBarState()
+
+        barOmniBarState.hasLargeWidth = !fooOmniBarState.hasLargeWidth
+        barOmniBarState.showBackButton = !fooOmniBarState.showBackButton
+        barOmniBarState.showForwardButton = !fooOmniBarState.showForwardButton
+        barOmniBarState.showBookmarksButton = !fooOmniBarState.showBookmarksButton
+        barOmniBarState.showShareButton = !fooOmniBarState.showShareButton
+        barOmniBarState.clearTextOnStart = !fooOmniBarState.clearTextOnStart
+        barOmniBarState.allowsTrackersAnimation = !fooOmniBarState.allowsTrackersAnimation
+        barOmniBarState.showSearchLoupe = !fooOmniBarState.showSearchLoupe
+        barOmniBarState.showCancel = !fooOmniBarState.showCancel
+        barOmniBarState.showPrivacyIcon = !fooOmniBarState.showPrivacyIcon
+        barOmniBarState.showBackground = !fooOmniBarState.showBackground
+        barOmniBarState.showClear = !fooOmniBarState.showClear
+        barOmniBarState.showRefresh = !fooOmniBarState.showRefresh
+        barOmniBarState.showMenu = !fooOmniBarState.showMenu
+        barOmniBarState.showSettings = !fooOmniBarState.showSettings
+        barOmniBarState.showVoiceSearch = !fooOmniBarState.showVoiceSearch
+        barOmniBarState.showAbort = !fooOmniBarState.showAbort
+
+        XCTAssertFalse(fooOmniBarState.isDifferentState(than: barOmniBarState))
+    }
+}
+
+private struct DummyOmniBarState: OmniBarState, OmniBarLoadingBearerStateCreating {
+    var name: String
+    var isLoading: Bool
+    var voiceSearchHelper: VoiceSearchHelperProtocol
+
+    var hasLargeWidth = false
+    var showBackButton = false
+    var showForwardButton = false
+    var showBookmarksButton = false
+    var showShareButton = false
+    var clearTextOnStart = false
+    var allowsTrackersAnimation = false
+    var showSearchLoupe = false
+    var showCancel = false
+    var showPrivacyIcon = false
+    var showBackground = false
+    var showClear = false
+    var showRefresh = false
+    var showMenu = false
+    var showSettings = false
+    var showVoiceSearch = false
+    var showAbort = false
+
+    var onEditingStoppedState: OmniBarState { DummyOmniBarState() }
+    var onEditingStartedState: OmniBarState { DummyOmniBarState() }
+    var onTextClearedState: OmniBarState { DummyOmniBarState() }
+    var onTextEnteredState: OmniBarState { DummyOmniBarState() }
+    var onBrowsingStartedState: OmniBarState { DummyOmniBarState() }
+    var onBrowsingStoppedState: OmniBarState { DummyOmniBarState() }
+    var onEnterPhoneState: OmniBarState { DummyOmniBarState() }
+    var onEnterPadState: OmniBarState { DummyOmniBarState() }
+    var onReloadState: OmniBarState { DummyOmniBarState() }
+
+    init(voiceSearchHelper: VoiceSearchHelperProtocol, isLoading: Bool) {
+        self.init(isLoading: isLoading, voiceSearchHelper: voiceSearchHelper)
+    }
+
+    init(name: String = "DummyOmniBarState", isLoading: Bool = false, voiceSearchHelper: VoiceSearchHelperProtocol = MockVoiceSearchHelper()) {
+        self.name = name
+        self.isLoading = isLoading
+        self.voiceSearchHelper = voiceSearchHelper
+    }
+}
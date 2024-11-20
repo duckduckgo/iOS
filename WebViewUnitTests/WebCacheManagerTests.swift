89f1baddcad09fd5df8d46ebc24dce885059b446
Bartek Waresiak <bartek@duckduckgo.com>
2024-11-20 10:00:36 +0100

    Add separate target for tests using WebView

--- WebViewUnitTests/WebCacheManagerTests.swift
+++ WebViewUnitTests/WebCacheManagerTests.swift
@@ -22,6 +22,29 @@ import XCTest
 import WebKit
 import TestUtils
 
+extension HTTPCookie {
+
+    static func make(name: String = "name",
+                     value: String = "value",
+                     domain: String = "example.com",
+                     path: String = "/",
+                     policy: HTTPCookieStringPolicy? = nil) -> HTTPCookie {
+
+        var properties: [HTTPCookiePropertyKey: Any] = [
+            .name: name,
+            .value: value,
+            .domain: domain,
+            .path: path
+        ]
+
+        if policy != nil {
+            properties[HTTPCookiePropertyKey.sameSitePolicy] = policy
+        }
+
+        return HTTPCookie(properties: properties)!    }
+
+}
+
 class WebCacheManagerTests: XCTestCase {
 
     override func setUp() {

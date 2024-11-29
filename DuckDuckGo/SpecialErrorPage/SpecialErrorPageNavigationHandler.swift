9f8ebcf5b27546f883d808d9d1b15dbb24e9a9d6
Alessandro Boron <aboron@duckduckgo.com>
2024-11-29 16:57:22 +0100

    Add Detection Logic

--- DuckDuckGo/SpecialErrorPage/SpecialErrorPageNavigationHandler.swift
+++ DuckDuckGo/SpecialErrorPage/SpecialErrorPageNavigationHandler.swift
@@ -27,7 +27,6 @@ typealias SpecialErrorPageManaging = SpecialErrorPageContextHandling & WebViewNa
 final class SpecialErrorPageNavigationHandler: SpecialErrorPageContextHandling {
     private var webView: WKWebView?
     private(set) var errorData: SpecialErrorData?
-    private var errorPageType: SpecialErrorKind?
     private(set) var isSpecialErrorPageVisible = false
     private(set) var failedURL: URL?
     private weak var userScript: SpecialErrorPageUserScript?
@@ -63,13 +62,17 @@ extension SpecialErrorPageNavigationHandler: WebViewNavigationHandling {
 
         return await MainActor.run {
             switch result {
-            case let .navigationHandled(model):
+            case let .navigationHandled(.mainFrame(error)):
                 var request = navigationAction.request
-                request.url = model.url
-                failedURL = model.url
-                errorData = model.errorData
-                errorPageType = .phishing
+                request.url = error.url
+                failedURL = error.url
+                errorData = error
                 loadSpecialErrorPage(request: request)
+                return true
+            case let .navigationHandled(.iFrame(maliciousURL, error)):
+                failedURL = maliciousURL
+                errorData = error
+                loadSpecialErrorPage(url: maliciousURL)
                 return true
             case .navigationNotHandled:
                 return false
@@ -88,7 +91,6 @@ extension SpecialErrorPageNavigationHandler: WebViewNavigationHandling {
         failedURL = sslSpecialError.error.url
         sslErrorPageNavigationHandler.errorPageVisited(errorType: sslSpecialError.type)
         errorData = sslSpecialError.error.errorData
-        errorPageType = .ssl
         loadSpecialErrorPage(url: sslSpecialError.error.url)
     }
     
@@ -105,45 +107,52 @@ extension SpecialErrorPageNavigationHandler: WebViewNavigationHandling {
 
 extension SpecialErrorPageNavigationHandler: SpecialErrorPageUserScriptDelegate {
 
+    @MainActor
     func leaveSiteAction() {
-        switch errorPageType {
+        defer {
+            if webView?.canGoBack == true {
+                _ = webView?.goBack()
+            } else {
+                delegate?.closeSpecialErrorPageTab()
+            }
+        }
+
+        guard let errorData else { return }
+
+        switch errorData {
         case .ssl:
             sslErrorPageNavigationHandler.leaveSite()
-        case .phishing:
+        case .maliciousSite:
             maliciousSiteProtectionNavigationHandler.leaveSite()
-        default:
-            break
-        }
-
-        if webView?.canGoBack == true {
-            _ = webView?.goBack()
-        } else {
-            delegate?.closeSpecialErrorPageTab()
         }
     }
 
+    @MainActor
     func visitSiteAction() {
-        switch errorPageType {
-        case .ssl:
-            sslErrorPageNavigationHandler.visitSite()
-        case .phishing:
-            maliciousSiteProtectionNavigationHandler.visitSite()
-        default:
-            break
+        defer {
+            isSpecialErrorPageVisible = false
+            _ = webView?.reload()
         }
 
-        isSpecialErrorPageVisible = false
-        _ = webView?.reload()
+        guard let errorData, let url = webView?.url else { return }
+
+        switch errorData {
+        case .ssl:
+            sslErrorPageNavigationHandler.visitSite(url: url, errorData: errorData)
+        case .maliciousSite:
+            maliciousSiteProtectionNavigationHandler.visitSite(url: url, errorData: errorData)
+        }
     }
 
+    @MainActor
     func advancedInfoPresented() {
-        switch errorPageType {
+        guard let errorData else { return }
+
+        switch errorData {
         case .ssl:
             sslErrorPageNavigationHandler.advancedInfoPresented()
-        case .phishing:
+        case .maliciousSite:
             maliciousSiteProtectionNavigationHandler.advancedInfoPresented()
-        default:
-            break
         }
     }
 }
@@ -160,6 +169,28 @@ private extension SpecialErrorPageNavigationHandler {
         let html = SpecialErrorPageHTMLTemplate.htmlFromTemplate
         webView?.loadSimulatedRequest(request, responseHTML: html)
         isSpecialErrorPageVisible = true
+    }
+
+}
+
+extension SpecialErrorData {
+
+    var url: URL? {
+        switch self {
+        case .ssl:
+            return nil
+        case let .maliciousSite(_, url):
+            return url
+        }
+    }
+
+    var threatKind: ThreatKind? {
+        switch self {
+        case .ssl:
+            return nil
+        case let .maliciousSite(threatKind, _):
+            return threatKind
+        }
     }
 
 }

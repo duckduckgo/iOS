e2f654c2c998f2b992e029639b73ca9293e35d90
Alessandro Boron <aboron@duckduckgo.com>
2024-12-02 13:20:14 +0100

    Remove duplicated mock after merging

--- DuckDuckGoTests/MockTabDelegate.swift
+++ DuckDuckGoTests/MockTabDelegate.swift
@@ -117,14 +117,6 @@ final class MockTabDelegate: TabDelegate {
 
 }
 
-final class MockCredentialCreator: URLCredentialCreating {
-
-    func urlCredentialFrom(trust: SecTrust?) -> URLCredential? {
-        return URLCredential(user: "", password: "", persistence: .forSession)
-    }
-
-}
-
 extension TabViewController {
 
     static func fake(

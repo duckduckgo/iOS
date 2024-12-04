a0a5ff1d202d0099b6e770b259d879f793b45fde
Alessandro Boron <aboron@duckduckgo.com>
2024-12-04 20:25:06 +0100

    Setup Add To Dock features and variants

--- Core/DefaultVariantManager.swift
+++ Core/DefaultVariantManager.swift
@@ -27,6 +27,8 @@ extension FeatureName {
     // Define your feature e.g.:
     // public static let experimentalFeature = FeatureName(rawValue: "experimentalFeature")
 
+    public static let addToDockIntro = FeatureName(rawValue: "addToDockIntro")
+    public static let addToDockContextual = FeatureName(rawValue: "addToDockContextual")
 }
 
 public struct VariantIOS: Variant {
@@ -54,6 +56,10 @@ public struct VariantIOS: Variant {
         VariantIOS(name: "sc", weight: doNotAllocate, isIncluded: When.always, features: []),
         VariantIOS(name: "sd", weight: doNotAllocate, isIncluded: When.always, features: []),
         VariantIOS(name: "se", weight: doNotAllocate, isIncluded: When.always, features: []),
+
+        VariantIOS(name: "mh", weight: 1, isIncluded: When.notPadDevice, features: []),
+        VariantIOS(name: "mk", weight: 1, isIncluded: When.notPadDevice, features: [.addToDockIntro]),
+        VariantIOS(name: "mo", weight: 1, isIncluded: When.notPadDevice, features: [.addToDockContextual]),
 
         returningUser
     ]

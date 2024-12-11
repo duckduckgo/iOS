dee115678b66b51063da32b6e175de020476406c
Alessandro Boron <aboron@duckduckgo.com>
2024-12-11 15:13:30 +0100

    Add MaliciouSiteProtectionManager config and resources

---
+++ DuckDuckGo/MaliciousSiteProtection/MaliciousSiteProtectionManager+Config.swift
@@ -0,0 +1,73 @@
+//
+//  MaliciousSiteProtectionManager+Config.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import MaliciousSiteProtection
+
+extension MaliciousSiteProtectionManager {
+
+    static func fileName(for dataType: MaliciousSiteProtection.DataManager.StoredDataType) -> String {
+        switch (dataType, dataType.threatKind) {
+        case (.hashPrefixSet, .phishing): "phishingHashPrefixes.json"
+        case (.filterSet, .phishing): "phishingFilterSet.json"
+        case (.hashPrefixSet, .malware): "malwareHashPrefixes.json"
+        case (.filterSet, .malware): "malwareFilterSet.json"
+        }
+    }
+
+    static func updateInterval(for dataKind: MaliciousSiteProtection.DataManager.StoredDataType) -> TimeInterval? {
+        switch dataKind {
+        case .hashPrefixSet: .minutes(20)
+        case .filterSet: .hours(12)
+        }
+    }
+
+    struct EmbeddedDataProvider: MaliciousSiteProtection.EmbeddedDataProviding {
+
+        private enum Constants {
+            static let embeddedDataRevision = 1696473
+            static let phishingEmbeddedHashPrefixDataSHA = "cdb609c37e950b7d0dcdaa80ae4071cf2c87223cfdd189caafae723722bd3158"
+            static let phishingEmbeddedFilterSetDataSHA = "4e52518aba04b0fd360fada76c9899001d3137d4a745cc13c484a54115a0fcd8"
+            static let malwareEmbeddedHashPrefixDataSHA = "6b5eb296e9e10ae9ea41c5b5356f532226d647e4f3b832c30ac670102446ea7a"
+            static let malwareEmbeddedFilterSetDataSHA = "4dc971fffaf244ee99267f28222a2c116743e35ef837dcbc0199693ed6a691cd"
+        }
+
+        func revision(for dataType: MaliciousSiteProtection.DataManager.StoredDataType) -> Int {
+            Constants.embeddedDataRevision
+        }
+
+        func url(for dataType: MaliciousSiteProtection.DataManager.StoredDataType) -> URL {
+            let fileName = fileName(for: dataType)
+            guard let url = Bundle.main.url(forResource: fileName, withExtension: nil) else {
+                fatalError("Could not find embedded data file \"\(fileName)\"")
+            }
+            return url
+        }
+
+        func hash(for dataType: MaliciousSiteProtection.DataManager.StoredDataType) -> String {
+            switch (dataType, dataType.threatKind) {
+            case (.hashPrefixSet, .phishing): Constants.phishingEmbeddedHashPrefixDataSHA
+            case (.filterSet, .phishing): Constants.phishingEmbeddedFilterSetDataSHA
+            case (.hashPrefixSet, .malware): Constants.malwareEmbeddedHashPrefixDataSHA
+            case (.filterSet, .malware): Constants.malwareEmbeddedFilterSetDataSHA
+            }
+        }
+        
+    }
+}

994213b5690f07f940b338519060589cb54870be
Daniel Bernal <dbernal@duckduckgo.com>
2024-12-16 16:17:25 +0100

    Allow Youtube’s internal navigation

--- DuckDuckGo/DuckPlayer/DuckPlayerNavigationHandler.swift
+++ DuckDuckGo/DuckPlayer/DuckPlayerNavigationHandler.swift
@@ -936,8 +936,19 @@ extension DuckPlayerNavigationHandler: DuckPlayerNavigationHandling {
             return false
         }
         
+        // Allow Youtube's internal navigation when DuckPlayer is enabled and user is watching on Youtube
+        // This is to prevent DuckPlayer from interfering with Youtube's internal navigation for search and settings
+        // https://app.asana.com/0/1204099484721401/1208930843675395/f
+        var isYoutubeInternalNavigation = false
+        if let (destinationVideoID, _) = url.youtubeVideoParams,
+           let (originVideoID, _) = webView.url?.youtubeVideoParams,
+            destinationVideoID == originVideoID,
+            duckPlayerMode == .enabled {
+                isYoutubeInternalNavigation = true
+        }
+        
         // Redirect to Duck Player if enabled
-        if url.isYoutubeWatch && duckPlayerMode == .enabled && !isDuckPlayerRedirect(url: url) {
+        if url.isYoutubeWatch && duckPlayerMode == .enabled && !isDuckPlayerRedirect(url: url) && !isYoutubeInternalNavigation {
             redirectToDuckPlayerVideo(url: url, webView: webView)
             return true
         }

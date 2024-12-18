7c1242e41ce5cb71ad9dda7b9a655c75307c0d6e
Pete <petes44@icloud.com>
2024-12-18 15:25:09 +0000

    Update FE <> Native API to return Free Trial subscriptions if applicable, add tests

---
+++ DuckDuckGoTests/Subscription/FreeTrialsExperiment/SubscriptionPagesUseSubscriptionFeatureFreeTrialsTests.swift
@@ -0,0 +1,274 @@
+//
+//  SubscriptionPagesUseSubscriptionFeatureFreeTrialsTests.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import XCTest
+import BrowserServicesKit
+import SubscriptionTestingUtilities
+@testable import Subscription
+@testable import DuckDuckGo
+
+
+final class SubscriptionPagesUseSubscriptionFeatureFreeTrialsTests: XCTestCase {
+
+    private var sut: SubscriptionPagesUseSubscriptionFeature!
+
+    private var mockSubscriptionManager: SubscriptionManagerMock!
+    private var mockAccountManager: AccountManagerMock!
+    private var mockFeatureFlagger: MockFeatureFlagger!
+    private var mockStorePurchaseManager: StorePurchaseManagerMock!
+    private var mockFreeTrialsFeatureFlagExperiment: MockFreeTrialsFeatureFlagExperiment!
+    private var mockAppStorePurchaseFlow: AppStorePurchaseFlowMock!
+
+    override func setUpWithError() throws {
+        mockAccountManager = AccountManagerMock()
+        mockStorePurchaseManager = StorePurchaseManagerMock()
+        mockSubscriptionManager = SubscriptionManagerMock(accountManager: mockAccountManager,
+                                                      subscriptionEndpointService: SubscriptionEndpointServiceMock(),
+                                                      authEndpointService: AuthEndpointServiceMock(),
+                                                          storePurchaseManager: mockStorePurchaseManager,
+                                                      currentEnvironment: SubscriptionEnvironment(serviceEnvironment: .production, purchasePlatform: .appStore),
+                                                      canPurchase: true,
+                                                      subscriptionFeatureMappingCache: SubscriptionFeatureMappingCacheMock())
+
+        mockAppStorePurchaseFlow = AppStorePurchaseFlowMock()
+        mockFeatureFlagger = MockFeatureFlagger()
+        mockFreeTrialsFeatureFlagExperiment = MockFreeTrialsFeatureFlagExperiment()
+
+        sut = SubscriptionPagesUseSubscriptionFeature(subscriptionManager: mockSubscriptionManager,
+                                                      subscriptionFeatureAvailability: SubscriptionFeatureAvailabilityMock.enabled,
+                                                      subscriptionAttributionOrigin: nil,
+                                                      appStorePurchaseFlow: mockAppStorePurchaseFlow,
+                                                      appStoreRestoreFlow: AppStoreRestoreFlowMock(),
+                                                      appStoreAccountManagementFlow: AppStoreAccountManagementFlowMock(),
+                                                      featureFlagger: mockFeatureFlagger,
+                                                      freeTrialsExperiment: mockFreeTrialsFeatureFlagExperiment)
+    }
+
+    func testWhenFreeTrialsCohortIsControl_thenStandardSubscriptionOptionsAreReturned() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.control
+        mockStorePurchaseManager.subscriptionOptionsResult = .mockStandard
+
+        // When
+        let result = await sut.getSubscriptionOptions(params: "", original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertEqual(result as? SubscriptionOptions, .mockStandard)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.incrementPaywallViewCountCalled)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.firePaywallImpressionPixelCalled)
+    }
+
+    func testWhenFreeTrialsCohortIsTreatment_thenFreeTrialSubscriptionOptionsAreReturned() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.treatment
+        mockStorePurchaseManager.freeTrialSubscriptionOptionsResult = .mockFreeTrial
+
+        // When
+        let result = await sut.getSubscriptionOptions(params: "", original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertEqual(result as? SubscriptionOptions, .mockFreeTrial)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.incrementPaywallViewCountCalled)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.firePaywallImpressionPixelCalled)
+    }
+
+    func testWhenUserIsAuthenticated_thenStandardSubscriptionOptionsAreReturned() async throws {
+        // Given
+        mockAccountManager.accessToken = "token"
+        mockSubscriptionManager.canPurchase = true
+        mockStorePurchaseManager.subscriptionOptionsResult = .mockStandard
+
+        // When
+        let result = await sut.getSubscriptionOptions(params: "", original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertEqual(result as? SubscriptionOptions, .mockStandard)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.incrementPaywallViewCountCalled)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.firePaywallImpressionPixelCalled)
+    }
+
+    func testWhenUserCannoPurchase_thenStandardSubscriptionOptionsAreReturned() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = false
+        mockStorePurchaseManager.subscriptionOptionsResult = .mockStandard
+
+        // When
+        let result = await sut.getSubscriptionOptions(params: "", original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertEqual(result as? SubscriptionOptions, .mockStandard)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.incrementPaywallViewCountCalled)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.firePaywallImpressionPixelCalled)
+    }
+
+    func testWhenFailedToFetchSubscriptionOptions_thenEmptyOptionsAreReturned() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.control
+        mockStorePurchaseManager.subscriptionOptionsResult = nil
+
+        // When
+        let result = await sut.getSubscriptionOptions(params: "", original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertEqual(result as? SubscriptionOptions, .empty)
+        XCTAssertEqual(sut.transactionError, .failedToGetSubscriptionOptions)
+    }
+
+    func testWhenMonthlySubscribeSucceedsForTreatment_thenSubscriptionPurchasedMonthlyPixelFired() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.treatment
+        mockAppStorePurchaseFlow.purchaseSubscriptionResult = .success("")
+        mockAppStorePurchaseFlow.completeSubscriptionPurchaseResult = .success(.completed)
+
+        let params: [String: Any] = ["id": "monthly-free-trial"]
+
+        // When
+        _ = await sut.subscriptionSelected(params: params, original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedMonthlyPixelCalled)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedYearlyPixelCalled)
+    }
+
+    func testWhenMonthlySubscribeSucceedsForTreatment_thenSubscriptionPurchasedYearlyPixelFired() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.treatment
+        mockAppStorePurchaseFlow.purchaseSubscriptionResult = .success("")
+        mockAppStorePurchaseFlow.completeSubscriptionPurchaseResult = .success(.completed)
+
+        let params: [String: Any] = ["id": "yearly-free-trial"]
+
+        // When
+        _ = await sut.subscriptionSelected(params: params, original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedMonthlyPixelCalled)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedYearlyPixelCalled)
+    }
+
+    func testWhenMonthlySubscribeSucceedsForControl_thenSubscriptionPurchasedMonthlyPixelFired() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.control
+        mockAppStorePurchaseFlow.purchaseSubscriptionResult = .success("")
+        mockAppStorePurchaseFlow.completeSubscriptionPurchaseResult = .success(.completed)
+
+        let params: [String: Any] = ["id": "monthly-free-trial"]
+
+        // When
+        _ = await sut.subscriptionSelected(params: params, original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedMonthlyPixelCalled)
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedYearlyPixelCalled)
+    }
+
+    func testWhenMonthlySubscribeSucceedsForControl_thenSubscriptionPurchasedYearlyPixelFired() async throws {
+        // Given
+        mockAccountManager.accessToken = nil
+        mockSubscriptionManager.canPurchase = true
+        mockFeatureFlagger.cohortToReturn = FreeTrialsFeatureFlagExperiment.Cohort.control
+        mockAppStorePurchaseFlow.purchaseSubscriptionResult = .success("")
+        mockAppStorePurchaseFlow.completeSubscriptionPurchaseResult = .success(.completed)
+
+        let params: [String: Any] = ["id": "yearly-free-trial"]
+
+        // When
+        _ = await sut.subscriptionSelected(params: params, original: MockWKScriptMessage(name: "", body: ""))
+
+        // Then
+        XCTAssertFalse(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedMonthlyPixelCalled)
+        XCTAssertTrue(mockFreeTrialsFeatureFlagExperiment.fireSubscriptionStartedYearlyPixelCalled)
+    }
+}
+
+private extension SubscriptionOptions {
+    static let mockStandard = SubscriptionOptions(platform: .ios,
+                                                   options: [
+                                                       SubscriptionOption(id: "1",
+                                                                          cost: SubscriptionOptionCost(displayPrice: "9", recurrence: "monthly")),
+                                                       SubscriptionOption(id: "2",
+                                                                          cost: SubscriptionOptionCost(displayPrice: "99", recurrence: "yearly"))
+                                                   ],
+                                                   features: [
+                                                       SubscriptionFeature(name: .networkProtection),
+                                                       SubscriptionFeature(name: .dataBrokerProtection),
+                                                       SubscriptionFeature(name: .identityTheftRestoration)
+                                                   ])
+
+    static let mockFreeTrial = SubscriptionOptions(platform: .ios,
+                                                    options: [
+                                                        SubscriptionOption(id: "3",
+                                                                           cost: SubscriptionOptionCost(displayPrice: "0", recurrence: "monthly-free-trial"), offer: .init(type: .freeTrial, id: "1", displayPrice: "0.00", durationInDays: 4, isUserEligible: true)),
+                                                        SubscriptionOption(id: "4",
+                                                                           cost: SubscriptionOptionCost(displayPrice: "0", recurrence: "yearly-free-trial"), offer: .init(type: .freeTrial, id: "1", displayPrice: "0.00", durationInDays: 4, isUserEligible: true))
+                                                    ],
+                                                    features: [
+                                                        SubscriptionFeature(name: .networkProtection)
+                                                    ])
+}
+
+private final class MockFreeTrialsFeatureFlagExperiment: FreeTrialsFeatureFlagExperimenting {
+    typealias CohortType = FreeTrialsFeatureFlagExperiment.Cohort
+    var rawValue: String = ""
+    var source: FeatureFlagSource = .remoteReleasable(.subfeature(PrivacyProSubfeature.privacyProFreeTrialJan25))
+
+    var incrementPaywallViewCountCalled = false
+    var firePaywallImpressionPixelCalled = false
+    var fireOfferSelectionMonthlyPixelCalled = false
+    var fireOfferSelectionYearlyPixelCalled = false
+    var fireSubscriptionStartedMonthlyPixelCalled = false
+    var fireSubscriptionStartedYearlyPixelCalled = false
+
+    func incrementPaywallViewCount() {
+        incrementPaywallViewCountCalled = true
+    }
+
+    func firePaywallImpressionPixel() {
+        firePaywallImpressionPixelCalled = true
+    }
+
+    func fireOfferSelectionMonthlyPixel() {
+        fireOfferSelectionMonthlyPixelCalled = true
+    }
+
+    func fireOfferSelectionYearlyPixel() {
+        fireOfferSelectionYearlyPixelCalled = true
+    }
+
+    func fireSubscriptionStartedMonthlyPixel() {
+        fireSubscriptionStartedMonthlyPixelCalled = true
+    }
+
+    func fireSubscriptionStartedYearlyPixel() {
+        fireSubscriptionStartedYearlyPixelCalled = true
+    }
+}

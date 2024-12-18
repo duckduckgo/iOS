c962d5af806babda3055f26fe8db90a125f420b0
Fernando Bunn <bunn@idevzilla.com>
2024-12-18 15:42:56 -0300

    Change titlebar UI

--- DuckDuckGo/RoundedPageContainer/RoundedPageSheetContainerViewController.swift
+++ DuckDuckGo/RoundedPageContainer/RoundedPageSheetContainerViewController.swift
@@ -21,24 +21,13 @@ import UIKit
 
 final class RoundedPageSheetContainerViewController: UIViewController {
     let contentViewController: UIViewController
-    private let logoImage: UIImage?
-    private let titleText: String
     private let allowedOrientation: UIInterfaceOrientationMask
 
     private var interactiveDismissalTransition: UIPercentDrivenInteractiveTransition?
     private var isInteractiveDismissal = false
 
-    private lazy var titleBarView: TitleBarView = {
-        let titleBarView = TitleBarView(logoImage: logoImage, title: titleText) { [weak self] in
-            self?.closeController()
-        }
-        return titleBarView
-    }()
-
-    init(contentViewController: UIViewController, logoImage: UIImage?, title: String, allowedOrientation: UIInterfaceOrientationMask = .all) {
+    init(contentViewController: UIViewController, allowedOrientation: UIInterfaceOrientationMask = .all) {
         self.contentViewController = contentViewController
-        self.logoImage = logoImage
-        self.titleText = title
         self.allowedOrientation = allowedOrientation
         super.init(nibName: nil, bundle: nil)
         modalPresentationStyle = .custom
@@ -65,23 +54,7 @@ final class RoundedPageSheetContainerViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .black
 
-        setupTitleBar()
         setupContentViewController()
-    }
-
-    private func setupTitleBar() {
-        view.addSubview(titleBarView)
-        titleBarView.translatesAutoresizingMaskIntoConstraints = false
-
-        NSLayoutConstraint.activate([
-            titleBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            titleBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            titleBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            titleBarView.heightAnchor.constraint(equalToConstant: 44)
-        ])
-
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
-        titleBarView.addGestureRecognizer(panGesture)
     }
 
     @objc private func handlePanGesture(_ gesture: UIPanGestureRecognizer) {
@@ -119,7 +92,7 @@ final class RoundedPageSheetContainerViewController: UIViewController {
         contentViewController.view.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            contentViewController.view.topAnchor.constraint(equalTo: titleBarView.bottomAnchor),
+            contentViewController.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             contentViewController.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             contentViewController.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             contentViewController.view.trailingAnchor.constraint(equalTo: view.trailingAnchor)
@@ -130,6 +103,9 @@ final class RoundedPageSheetContainerViewController: UIViewController {
         contentViewController.view.clipsToBounds = true
 
         contentViewController.didMove(toParent: self)
+
+        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePanGesture(_:)))
+        contentViewController.view.addGestureRecognizer(panGesture)
     }
 
     @objc func closeController() {
@@ -148,70 +124,5 @@ extension RoundedPageSheetContainerViewController: UIViewControllerTransitioning
 
     func interactionControllerForDismissal(using animator: UIViewControllerAnimatedTransitioning) -> UIViewControllerInteractiveTransitioning? {
         return isInteractiveDismissal ? interactiveDismissalTransition : nil
-    }
-}
-
-final private class TitleBarView: UIView {
-    private let imageView: UIImageView
-    private let titleLabel: UILabel
-    private let closeButton: UIButton
-
-    init(logoImage: UIImage?, title: String, closeAction: @escaping () -> Void) {
-        imageView = UIImageView(image: logoImage)
-        titleLabel = UILabel()
-        closeButton = UIButton(type: .system)
-
-        super.init(frame: .zero)
-
-        setupView(title: title, closeAction: closeAction)
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    private func setupView(title: String, closeAction: @escaping () -> Void) {
-        backgroundColor = .clear
-
-        imageView.contentMode = .scaleAspectFit
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-
-        let imageSize: CGFloat = 28
-        NSLayoutConstraint.activate([
-            imageView.widthAnchor.constraint(equalToConstant: imageSize),
-            imageView.heightAnchor.constraint(equalToConstant: imageSize)
-        ])
-
-        titleLabel.text = title
-        titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .semibold)
-        titleLabel.textColor = .white
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-
-        closeButton.setImage(UIImage(named: "Close-24"), for: .normal)
-        closeButton.tintColor = .white
-        closeButton.translatesAutoresizingMaskIntoConstraints = false
-        closeButton.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
-
-        addSubview(imageView)
-        addSubview(titleLabel)
-        addSubview(closeButton)
-
-        NSLayoutConstraint.activate([
-            imageView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            imageView.centerYAnchor.constraint(equalTo: centerYAnchor),
-
-            titleLabel.leadingAnchor.constraint(equalTo: imageView.trailingAnchor, constant: 8),
-            titleLabel.centerYAnchor.constraint(equalTo: centerYAnchor),
-
-            closeButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            closeButton.centerYAnchor.constraint(equalTo: centerYAnchor)
-        ])
-
-        self.closeAction = closeAction
-    }
-    private var closeAction: (() -> Void)?
-
-    @objc private func closeButtonTapped() {
-        closeAction?()
     }
 }

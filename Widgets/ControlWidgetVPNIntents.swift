8c886fc95f5938a12c59d546f7802556145b6891
Diego Rey Mendez <dreymendez@duckduckgo.com>
2024-12-20 10:48:16 -0300

    Fixed a header

--- Widgets/ControlWidgetVPNIntents.swift
+++ Widgets/ControlWidgetVPNIntents.swift
@@ -1,8 +1,8 @@
 //
-//  VPNIntents.swift
+//  ControlWidgetToggleVPNIntent.swift
 //  DuckDuckGo
 //
-//  Copyright © 2023 DuckDuckGo. All rights reserved.
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.

da156137cc6d1f74f748f462412befec17f1f15f
Alessandro Boron <alessandro.boron@gmail.com>
2025-01-14 14:53:49 +1100

    Malicious site protection feature flags (#3719)
    Task/Issue URL: https://app.asana.com/0/72649045549333/1207944134334659/f
    Tech Design URL: https://app.asana.com/0/1206329551987282/1207273224076495/f
    
    **Description**:
    Add Feature flag class as per tech design to check if malicious site protection feature is enabled and should check protection for domain based on the domain privacy preferences.

---
+++ DuckDuckGo/MaliciousSiteProtection/FeatureFlags/MaliciousSiteProtectionFeatureFlags.swift
@@ -0,0 +1,104 @@
+//
+//  MaliciousSiteProtectionFeatureFlags.swift
+//  DuckDuckGo
+//
+//  Copyright © 2024 DuckDuckGo. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import Foundation
+import BrowserServicesKit
+import Core
+
+protocol MaliciousSiteProtectionFeatureFlagger {
+    /// A Boolean value indicating whether malicious site protection is enabled.
+    /// - Returns: `true` if malicious site protection is enabled; otherwise, `false`.
+    var isMaliciousSiteProtectionEnabled: Bool { get }
+
+    /// Checks if should detect malicious threats for a specific domain.
+    /// - Parameter domain: The domain to check for malicious threat.
+    /// - Returns: `true` if should check for malicious threats for the specified domain; otherwise, `false`.
+    func shouldDetectMaliciousThreat(forDomain domain: String?) -> Bool
+}
+
+protocol MaliciousSiteProtectionFeatureFlagsSettingsProvider {
+    /// The frequency, in minutes, at which the hash prefix should be updated.
+    var hashPrefixUpdateFrequency: Int { get }
+    /// The frequency, in minutes, at which the filter set should be updated.
+    var filterSetUpdateFrequency: Int { get }
+}
+
+/// An enum representing the different settings for malicious site protection feature flags.
+enum MaliciousSiteProtectionFeatureSettings: String {
+    /// The setting for hash prefix update frequency.
+    case hashPrefixUpdateFrequency
+    /// The setting for filter set update frequency.
+    case filterSetUpdateFrequency
+
+    var defaultValue: Int {
+        switch self {
+        case .hashPrefixUpdateFrequency: return 20 // Default frequency for hash prefix updates is 20 minutes.
+        case .filterSetUpdateFrequency: return 720 // Default frequency for filter set updates is 720 minutes (12 hours).
+        }
+    }
+}
+
+final class MaliciousSiteProtectionFeatureFlags {
+    private let featureFlagger: FeatureFlagger
+    private let privacyConfigManager: PrivacyConfigurationManaging
+
+    private var remoteSettings: PrivacyConfigurationData.PrivacyFeature.FeatureSettings {
+        privacyConfigManager.privacyConfig.settings(for: .maliciousSiteProtection)
+    }
+
+    init(
+        featureFlagger: FeatureFlagger = AppDependencyProvider.shared.featureFlagger,
+        privacyConfigManager: PrivacyConfigurationManaging = ContentBlocking.shared.privacyConfigurationManager
+    ) {
+        self.featureFlagger = featureFlagger
+        self.privacyConfigManager = privacyConfigManager
+    }
+}
+
+// MARK: - MaliciousSiteProtectionFeatureFlagger
+
+extension MaliciousSiteProtectionFeatureFlags: MaliciousSiteProtectionFeatureFlagger {
+
+    var isMaliciousSiteProtectionEnabled: Bool {
+        featureFlagger.isFeatureOn(.maliciousSiteProtection)
+    }
+
+    func shouldDetectMaliciousThreat(forDomain domain: String?) -> Bool {
+        isMaliciousSiteProtectionEnabled && privacyConfigManager.privacyConfig.isFeature(.maliciousSiteProtection, enabledForDomain: domain)
+    }
+
+}
+
+// MARK: - MaliciousSiteProtectionFeatureFlagsSettingsProvider
+
+extension MaliciousSiteProtectionFeatureFlags: MaliciousSiteProtectionFeatureFlagsSettingsProvider {
+
+    var hashPrefixUpdateFrequency: Int {
+        getSettings(MaliciousSiteProtectionFeatureSettings.hashPrefixUpdateFrequency)
+    }
+    
+    var filterSetUpdateFrequency: Int {
+        getSettings(MaliciousSiteProtectionFeatureSettings.filterSetUpdateFrequency)
+    }
+
+    private func getSettings(_ value: MaliciousSiteProtectionFeatureSettings) -> Int {
+        remoteSettings[value.rawValue] as? Int ?? value.defaultValue
+    }
+
+}

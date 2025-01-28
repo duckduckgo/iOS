b689d57697e92fa6727d99a25d0cf0269c83df62
Daniel Bernal <dbernal@duckduckgo.com>
2025-01-28 15:55:37 +0100

    Lint issues

--- DuckDuckGo/DuckPlayer/DuckPlayerViewModel.swift
+++ DuckDuckGo/DuckPlayer/DuckPlayerViewModel.swift
@@ -2,7 +2,7 @@
 //  DuckPlayerViewModel.swift
 //  DuckDuckGo
 //
-//  Copyright 2025 DuckDuckGo. All rights reserved.
+//  Copyright © 2025 DuckDuckGo. All rights reserved.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.

7dd808cb783654eb4420cc9a8db0241c4840be02
Daniel Bernal <dbernal@duckduckgo.com>
2025-01-31 15:26:38 +0100

    Remove unused UserDefaults

--- Core/UserDefaultsPropertyWrapper.swift
+++ Core/UserDefaultsPropertyWrapper.swift
@@ -175,15 +175,6 @@ public struct UserDefaultsWrapper<T> {
         case debugOnboardingHighlightsEnabledKey = "com.duckduckgo.ios.debug.onboardingHighlightsEnabled"
         case debugWebViewStateRestorationEnabledKey = "com.duckduckgo.ios.debug.webViewStateRestorationEnabled"
 
-        // Duck Player Pixel Experiment
-        case duckPlayerPixelExperimentInstalled = "com.duckduckgo.ios.duckplayer.pixel.experiment.installed.v2"
-        case duckPlayerPixelExperimentCohort = "com.duckduckgo.ios.duckplayer.pixel.experiment.cohort.v2"
-        case duckPlayerPixelExperimentEnrollmentDate = "com.duckduckgo.ios.duckplayer.pixel.experiment.enrollment.date.v2"
-        case duckPlayerPixelExperimentLastWeekPixelFired = "com.duckduckgo.ios.duckplayer.pixel.experiment.last.week.pixel.fired.v2"
-        case duckPlayerPixelExperimentLastDayPixelFired = "com.duckduckgo.ios.duckplayer.pixel.experiment.last.day.pixel.fired.v2"
-        case duckPlayerPixelExperimentLastVideoIDRendered = "com.duckduckgo.ios.duckplayer.pixel.experiment.last.videoID.rendered.v2"
-        case duckPlayerPixelExperimentOverride = "com.duckduckgo.ios.duckplayer.pixel.experiment.override.v2"
-
         // Domain specific text zoom
         case domainTextZoomStorage = "com.duckduckgo.ios.domainTextZoomStorage"
 

db64182e9a68dc2602f129d769d25f6a2c80db95
Pete <petes44@icloud.com>
2025-02-06 11:11:57 +0000

    Fix typo!

--- DuckDuckGo/Subscription/UserScripts/SubscriptionPagesUseSubscriptionFeature.swift
+++ DuckDuckGo/Subscription/UserScripts/SubscriptionPagesUseSubscriptionFeature.swift
@@ -547,7 +547,7 @@ private extension SubscriptionPagesUseSubscriptionFeature {
     ///
     /// - Parameter freeTrialsCohort: The cohort the user belongs to (`control` or `treatment`).
     /// - Returns: A `SubscriptionOptions` object containing the relevant subscription options.
-    func se async -> SubscriptionOptions? {
+    func freeTrialSubscriptionOptions(for freeTrialsCohort: PrivacyProFreeTrialExperimentCohort) async -> SubscriptionOptions? {
         var subscriptionOptions: SubscriptionOptions?
 
         switch freeTrialsCohort {
